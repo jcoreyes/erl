f4e3f5c50627962739fa79c593170dd037f9bcbb
Vitchyr Pong <vitchyr@gmail.com>
2016-11-24 17:45:44 -0800

    make xavier_uniform_initializer() returna  function

--- core/tf_util.py
+++ core/tf_util.py
@@ -204,13 +204,16 @@ def he_uniform_initializer():
     return _initializer
 
 
-def xavier_uniform_initializer(shape, **kwargs):
-    if len(shape) == 2:
-        n_inputs, n_outputs = shape
-    else:
-        receptive_field_size = np.prod(shape[:2])
-        n_inputs = shape[-2] * receptive_field_size
-        n_outputs = shape[-1] * receptive_field_size
-    init_range = math.sqrt(6.0 / (n_inputs + n_outputs))
-    return tf.random_uniform(shape, minval=-init_range, maxval=init_range,
-                             **kwargs)
+def xavier_uniform_initializer():
+    def _initializer(shape, **kwargs):
+        if len(shape) == 2:
+            n_inputs, n_outputs = shape
+        else:
+            receptive_field_size = np.prod(shape[:2])
+            n_inputs = shape[-2] * receptive_field_size
+            n_outputs = shape[-1] * receptive_field_size
+        init_range = math.sqrt(6.0 / (n_inputs + n_outputs))
+        return tf.random_uniform(shape, minval=-init_range, maxval=init_range,
+                                 **kwargs)
+
+    return _initializer
