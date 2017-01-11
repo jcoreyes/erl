d1b75691047dd07e298a709fa38db7c80a144d7b
Vitchyr Pong <vitchyr@gmail.com>
2017-01-10 16:05:28 -0800

    add example hyperparameter sweep script

---
+++ scripts/ddpg_cart_reward_scale_sweep.py
@@ -0,0 +1,67 @@
+"""
+See how reward scale affects learning.
+
+TODO: DeterministicHyperparameterSweeper doesn't work in stub mode because
+**(stub object) just hangs.
+different.
+"""
+from algos.ddpg import DDPG
+from misc.hyperparameter import DeterministicHyperparameterSweeper
+from misc.scripts_util import timestamp
+from policies.nn_policy import FeedForwardPolicy
+from qfunctions.nn_qfunction import FeedForwardCritic
+from rllab.envs.box2d.cartpole_env import CartpoleEnv
+from rllab.exploration_strategies.ou_strategy import OUStrategy
+from rllab.misc.instrument import run_experiment_lite, stub
+from sandbox.rocky.tf.envs.base import TfEnv
+
+
+def main():
+    env = TfEnv(CartpoleEnv())
+    es = OUStrategy(env_spec=env.spec)
+    qf = FeedForwardCritic(
+        name_or_scope="critic",
+        env_spec=env.spec,
+    )
+    policy = FeedForwardPolicy(
+        name_or_scope="actor",
+        env_spec=env.spec,
+    )
+    default_ddpg_params = dict(
+        batch_size=32,
+        n_epochs=10,
+        epoch_length=1000,
+        eval_samples=1000,
+        max_path_length=100,
+        min_pool_size=1000,
+    )
+    sweeper = DeterministicHyperparameterSweeper(
+        {'scale_reward': [1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3, 1e4]},
+    )
+    for ddpg_params in sweeper.iterate_hyperparameters():
+        print(ddpg_params)
+        algorithm = DDPG(
+            env,
+            es,
+            policy,
+            qf,
+            scale_reward=ddpg_params['scale_reward'],
+            **default_ddpg_params,
+        )
+
+        exp_prefix = 'ddpg-cart-reward-scale-sweep-{0}'.format(timestamp())
+        for seed in range(3):
+            run_experiment_lite(
+                algorithm.train(),
+                n_parallel=1,
+                snapshot_mode="last",
+                exp_prefix=exp_prefix,
+                seed=seed,
+                # mode="local",
+                # use_cloudpickle=True,
+            )
+
+
+if __name__ == "__main__":
+    stub(globals())
+    main()
