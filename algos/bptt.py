f7de1a34e591de0b17fd89c8fe65fbec132d0cef
Vitchyr Pong <vitchyr@gmail.com>
2017-01-30 22:01:28 -0800

    rough version of bptt done

--- algos/bptt.py
+++ algos/bptt.py
@@ -1,49 +1,153 @@
+from collections import OrderedDict
+import numpy as np
+import tensorflow as tf
+import time
+
+from railrl.envs.supervised_learning_env import SupervisedLearningEnv
 from rllab.algos.base import RLAlgorithm
+from rllab.misc import logger
 from rllab.misc.overrides import overrides
 
 
 class Bptt(RLAlgorithm):
     """
-    Back propogation through time
+    Back propagation through time
     """
+
     def __init__(
             self,
+            env: SupervisedLearningEnv,
+            num_batches_per_epoch=32,
+            num_epochs=1000,
+            learning_rate=1e-3,
+            batch_size=32,
+            eval_batch_size=128,
+            lstm_state_size=10,
     ):
-        self.num_batches = None
-        self.episode_length = None
-        self.num_episodes_per_iteration = None
-        self.replay_buffer = None
-        self.env = None
-        self.policy = None
+        self._num_batches_per_epoch = num_batches_per_epoch
+        self._num_epochs = num_epochs
+        self._learning_rate = learning_rate
+        self._batch_size = batch_size
+        self._eval_batch_size = eval_batch_size
+        self._state_size = lstm_state_size
+        self._env = env
+        self._num_classes = env.feature_dim
+        self._num_steps = env.sequence_length
 
-    def _sample(self):
-        for k in range(self.num_episodes_per_iteration):
-            last_observation = self.env.reset()
-            done = False
-            for t in range(self.episode_length):
-                action = self.policy.act(last_observation)
-                observation, reward, done, _ = self.env.step(action)
-                self.replay_buffer.add_sample(observation, action, reward, done)
-                if t < self.episode_length - 1:
-                    assert not done
-                assert done
-            assert done
-            self.replay_buffer.finish_episode()
+        self._training_losses = []
 
     @overrides
     def train(self):
-        self._sample()
-        batches = self.replay_buffer.sample(self.num_batches)
-        self._bptt(batches)
+        self._init_ops()
+        for epoch in range(self._num_epochs):
+            logger.push_prefix('Epoch #%d | ' % epoch)
 
-    def _bptt(self, batches):
-        # A numpy array holding the state of LSTM after each batch of words.
-        total_loss = 0.0
-        for current_batch_of_words in words_in_dataset:
-            numpy_state, current_loss = session.run([final_state, loss],
-                                                    # Initialize the LSTM state from the previous iteration.
-                                                    feed_dict={
-                                                        initial_state: numpy_state,
-                                                        words: current_batch_of_words})
-            total_loss += current_loss
+            start_time = time.time()
+            for update in range(self._num_batches_per_epoch):
+                X, Y = self._env.get_batch(self._batch_size)
+                self._bptt_train(X, Y)
+            logger.log("Training time: {0}".format(time.time() - start_time))
 
+            start_time = time.time()
+            X, Y = self._env.get_batch(self._eval_batch_size)
+            self._eval(X, Y, epoch)
+            logger.pop_prefix()
+            logger.log("Eval time: {0}".format(time.time() - start_time))
+
+    def _init_ops(self):
+        self._sess = tf.get_default_session() or tf.Session()
+        self._init_network()
+
+    def _init_network(self):
+        """
+        Implementation based on
+        http://r2rt.com/recurrent-neural-networks-in-tensorflow-i.html
+        """
+
+        self._x = tf.placeholder(
+            tf.int32,
+            [self._batch_size, self._num_steps, self._num_classes],
+            name='input_placeholder',
+        )
+        self._y = tf.placeholder(
+            tf.int32,
+            [self._batch_size, self._num_steps, self._num_classes],
+            name='labels_placeholder',
+        )
+        self._init_state = tf.zeros([self._batch_size, self._state_size])
+
+        """
+        Inputs/outputs
+        """
+
+        rnn_inputs = tf.unpack(self._x, axis=1)
+        rnn_targets = tf.unpack(self._y, axis=1)
+
+        """
+        RNN
+        """
+
+        cell = tf.nn.rnn_cell.LSTMCell(self._state_size)
+        rnn_outputs, self._final_state = tf.nn.rnn(
+            cell,
+            rnn_inputs,
+            initial_state=self._init_state,
+        )
+
+        """
+        Predictions, loss, training step
+        """
+
+        with tf.variable_scope('softmax'):
+            W = tf.get_variable('W', [self._state_size, self._num_classes])
+            b = tf.get_variable('b', [self._num_classes],
+                                initializer=tf.constant_initializer(0.0))
+        logits = [tf.matmul(rnn_output, W) + b for rnn_output in rnn_outputs]
+        self._predictions = [tf.nn.softmax(logit) for logit in logits]
+
+        # weight all losses equally
+        loss_weights = [tf.ones([self._batch_size]) for _ in
+                        range(self._num_steps)]
+        losses = tf.nn.seq2seq.sequence_loss_by_example(logits,
+                                                        rnn_targets,
+                                                        loss_weights)
+        self._total_loss = tf.reduce_mean(losses)
+        self._train_step = tf.train.AdamOptimizer(
+            self._learning_rate).minimize(
+            self._total_loss)
+
+    def _bptt_train(self, X, Y):
+        init_state = np.zeros((self._batch_size, self._state_size))
+        training_loss_, _ = self._sess.run(
+            [
+                self._total_loss,
+                self._train_step,
+            ],
+            feed_dict={
+                self._x: X,
+                self._y: Y,
+                self._init_state: init_state,
+            },
+        )
+        self._training_losses.append(training_loss_)
+
+    def _eval(self, X, Y, epoch):
+        init_state = np.zeros((self._batch_size, self._state_size))
+        eval_loss, _ = self._sess.run(
+            self._total_loss,
+            feed_dict={
+                self._x: X,
+                self._y: Y,
+                self._init_state: init_state,
+            },
+        )
+        last_statistics = OrderedDict([
+            ('Epoch', epoch),
+            ('Training Loss', np.mean(self._training_losses)),
+            ('Eval Loss', eval_loss),
+        ])
+        self._training_losses = []
+
+        for key, value in last_statistics.items():
+            logger.record_tabular(key, value)
+        logger.dump_tabular(with_prefix=False)
--- launchers/rnn_launchers.py
+++ launchers/rnn_launchers.py
@@ -2,6 +2,7 @@
 Various launchers for recurrent algorithms.
 """
 
+
 def lstm_launcher(variant):
     """
     Run a simple LSTM on an environment.
@@ -13,29 +14,9 @@ def lstm_launcher(variant):
         - policy_params
     :return:
     """
-    from railrl.algos.ddpg import DDPG as MyDDPG
-    from railrl.policies.nn_policy import FeedForwardPolicy
-    from railrl.qfunctions.nn_qfunction import FeedForwardCritic
-    from rllab.exploration_strategies.ou_strategy import OUStrategy
+    from railrl.algos.bptt import Bptt
     from railrl.launchers.launcher_util import get_env_settings
     env_settings = get_env_settings(**variant['env_params'])
-    env = env_settings['env']
-    es = OUStrategy(env_spec=env.spec)
-    qf = FeedForwardCritic(
-        name_or_scope="critic",
-        env_spec=env.spec,
-        **variant.get('qf_params', {})
-    )
-    policy = FeedForwardPolicy(
-        name_or_scope="actor",
-        env_spec=env.spec,
-        **variant.get('policy_params', {})
-    )
-    algorithm = MyDDPG(
-        env,
-        es,
-        policy,
-        qf,
-        **variant['algo_params']
-    )
+    env = env_settings['_env']
+    algorithm = Bptt(env)
     algorithm.train()
