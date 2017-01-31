4562f572930165bfd0767c9ebcd9b664ebcafb1e
Vitchyr Pong <vitchyr@gmail.com>
2017-01-30 16:39:40 -0800

    finish testing rl mode

--- envs/memory/one_char_memory.py
+++ envs/memory/one_char_memory.py
@@ -11,19 +11,19 @@ class OneCharMemory(Env, SupervisedLearningEnv):
     A simple env whose output is a value `X` the first time step, followed by a
     fixed number of zeros.
 
-    There are `n` different values that `X` can take on (excluding 0).
-
-    The reward is zero for all steps except the end, where the agent gets a
-    reward of 1 if `X` is returned correctly.
-
+    The reward is 1 for all steps if the agent outputs zero except at the end,
+    where the agent gets a large reward if `X` is returned correctly.
 
     Both the actions and observations are represented as one-hot vectors.
+    There are `n` different values that `X` can take on (excluding 0),
+    so the one-hot vector's dimension is n+1.
     """
 
-    def __init__(self, n=4, num_steps=1000):
+    def __init__(self, n=4, num_steps=100, reward_for_remembering=1000):
         """
         :param n: Number of different values that could be returned
         :param num_steps: How many steps the agent needs to remember.
+        :param reward_for_remembering: The reward for remembering the number.
         """
         super().__init__()
         self.num_steps = num_steps
@@ -31,24 +31,27 @@ class OneCharMemory(Env, SupervisedLearningEnv):
         self._action_space = Discrete(self.n + 1)
         self._observation_space = Discrete(self.n + 1)
         self._t = 1
+        self._reward_for_remembering = reward_for_remembering
 
         self._target = None
         self._next_obs = None
 
     def step(self, action):
         # flatten = to one hot...not sure why it was given that name.
-        observation = self._observation_space.flatten(self._next_obs)
+        observation = self._get_next_observation()
         self._next_obs = 0
 
         done = self._t == self.num_steps
         self._t += 1
 
         if done:
-            reward = int(
+            reward = self._reward_for_remembering * int(
                 self._observation_space.unflatten(action) == self._target
             )
         else:
-            reward = 0
+            reward = int(
+                self._observation_space.unflatten(action) == 0
+            )
         info = {'target': self.n}
         return observation, reward, done, info
 
@@ -63,7 +66,11 @@ class OneCharMemory(Env, SupervisedLearningEnv):
     def reset(self):
         self._target = randint(1, self.n)
         self._next_obs = self._target
-        self._t = 0
+        self._t = 1
+        return self._get_next_observation()
+
+    def _get_next_observation(self):
+        return self._observation_space.flatten(self._next_obs)
 
     @property
     def observation_space(self):
@@ -77,8 +84,10 @@ class OneCharMemory(Env, SupervisedLearningEnv):
         )
         onehot_targets = special.to_onehot_n(targets, self.feature_dim)
         X = np.zeros((batch_size, self.sequence_length, self.feature_dim))
+        X[:, :, 0] = 1  # make the target 0
         X[:, 0, :] = onehot_targets
         Y = np.zeros((batch_size, self.sequence_length, self.target_dim))
+        Y[:, :, 0] = 1  # make the target 0
         Y[:, -1, :] = onehot_targets
         return X, Y
 
