450cc6ead7450a2fe18176c2ffe43138bab846d5
Vitchyr Pong <vitchyr@gmail.com>
2017-02-09 11:37:11 -0800

    consolidate env_utils/env_util

--- envs/env_util.py
+++
@@ -1,8 +0,0 @@
-from rllab.envs.gym_env import GymEnv
-
-
-def gym_env(name):
-    return GymEnv(name,
-                  record_video=False,
-                  log_dir='/tmp/gym-test',  # Ignore gym log.
-                  record_log=False)
