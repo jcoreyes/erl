cc6b2b4176f215642932d6475128b00f1cbaa856
Vitchyr Pong <vitchyr@gmail.com>
2017-02-11 21:41:35 -0800

    Add log config utils. alsoreprocess more stuff and add TODO

--- launchers/launcher_util.py
+++ launchers/launcher_util.py
@@ -1,3 +1,8 @@
+import datetime
+import dateutil.tz
+import os
+import os.path as osp
+
 from railrl.envs.env_utils import gym_env
 from railrl.envs.memory.one_char_memory import OneCharMemory
 from rllab.envs.box2d.cartpole_env import CartpoleEnv
@@ -9,6 +14,8 @@ from rllab.envs.normalized_env import normalize
 
 # Although this import looks like it does nothing, but this is needed to use
 # the envs in this package, because this call will register the environments.
+from rllab import config
+from rllab.misc import logger
 
 
 def get_env_settings(env_id="", normalize_env=True, gym_name="",
@@ -54,3 +61,80 @@ def get_env_settings(env_id="", normalize_env=True, gym_name="",
     )
 
 
+now = datetime.datetime.now(dateutil.tz.tzlocal())
+timestamp = now.strftime('%Y_%m_%d_%H_%M_%S')
+
+
+def create_exp_name(exp_prefix="default", exp_count=0):
+    """
+    Create a semi-unique experiment name that has a timestamp
+    :param exp_prefix:
+    :param exp_count:
+    :return:
+    """
+    return "%s_%s_%04d" % (exp_prefix, timestamp, exp_count)
+
+
+def create_log_dir(exp_prefix="default", exp_count=0):
+    """
+    Creates and returns a unique log directory.
+
+    :param exp_prefix: All experiments with this prefix will have log
+    directories be under this directory.
+    :param exp_count: Different exp_counts will be in different directories.
+    :return:
+    """
+    exp_name = create_exp_name(exp_prefix=exp_prefix, exp_count=exp_count)
+    log_dir = osp.join(
+        config.LOG_DIR,
+        'local',
+        exp_prefix.replace("_", "-"),
+        exp_name,
+    )
+    os.makedirs(log_dir, exist_ok=True)
+    return log_dir
+
+
+def setup_logger(
+        exp_prefix=None,
+        exp_count=0,
+        variant_data=None,
+        log_dir=None,
+        text_log_file="debug.log",
+        variant_log_file="variant.json",
+        tabular_log_file="progress.csv",
+        snapshot_mode="last",
+        log_tabular_only=False,
+        snapshot_gap=1,
+):
+    """
+    Set up logger to have some reasonable default settings.
+
+    :param exp_prefix:
+    :param exp_count:
+    :param variant_data:
+    :param log_dir:
+    :param text_log_file:
+    :param variant_log_file:
+    :param tabular_log_file:
+    :param snapshot_mode:
+    :param log_tabular_only:
+    :param snapshot_gap:
+    :return:
+    """
+    if log_dir is None:
+        assert exp_prefix is not None
+        log_dir = create_log_dir(exp_prefix, exp_count=exp_count)
+    tabular_log_path = osp.join(log_dir, tabular_log_file)
+    text_log_path = osp.join(log_dir, text_log_file)
+
+    if variant_data is not None:
+        variant_log_path = osp.join(log_dir, variant_log_file)
+        logger.log_variant(variant_log_path, variant_data)
+
+    logger.add_text_output(text_log_path)
+    logger.add_tabular_output(tabular_log_path)
+    logger.set_snapshot_dir(log_dir)
+    logger.set_snapshot_mode(snapshot_mode)
+    logger.set_snapshot_gap(snapshot_gap)
+    logger.set_log_tabular_only(log_tabular_only)
--- notes.md
+++ notes.md
@@ -5,6 +5,7 @@
  - Make a load_policy method
  - Organize the misc folder
  - See TODO in test_tensorflow.py
+ - Create my own spaces rather than editting rllab's spaces
 
 # Notes
 These notes are really for myself (vpong), so they're probably meaningless to anyone else.
