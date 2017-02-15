d0495012020d2c8d14a6cc6a7abfa715b625ac61
Vitchyr Pong <vitchyr@gmail.com>
2017-02-14 22:33:03 -0800

    delete old scripts and only have one to try new things

--- scripts/icml2017/linear_ocm.py
+++ scripts/icml2017/linear_ocm.py
@@ -1,5 +1,5 @@
 """
-Check affine_softmax_policy on OneCharMemoryEndOnly task.
+Check various policies on OneCharMemory task.
 """
 from railrl.launchers.launcher_util import (
     run_experiment,
@@ -9,13 +9,15 @@ from railrl.launchers.launcher_util import (
 
 def run_linear_ocm_exp(variant):
     from railrl.algos.ddpg_ocm import DdpgOcm
-    from railrl.qfunctions.memory_qfunction import MemoryQFunction
+    from railrl.qfunctions.memory.mlp_memory_qfunction import MlpMemoryQFunction
     from railrl.exploration_strategies.noop import NoopStrategy
+    from railrl.exploration_strategies.onehot_sampler import OneHotSampler
+    from railrl.exploration_strategies.product_strategy import ProductStrategy
     from railrl.envs.memory.continuous_memory_augmented import (
         ContinuousMemoryAugmented
     )
     from railrl.envs.memory.one_char_memory import OneCharMemoryEndOnly
-    from railrl.policies.memory.affine_softmax_policy import AffineSoftmaxPolicy
+    from railrl.policies.memory.linear_ocm_policy import LinearOcmPolicy
     from railrl.launchers.launcher_util import (
         set_seed,
     )
@@ -41,14 +43,14 @@ def run_linear_ocm_exp(variant):
         num_memory_states=onehot_dim,
     )
 
-    policy = AffineSoftmaxPolicy(
+    policy = LinearOcmPolicy(
         name_or_scope="policy",
         memory_and_action_dim=onehot_dim,
         env_spec=env.spec,
     )
 
-    es = NoopStrategy()
-    qf = MemoryQFunction(
+    es = ProductStrategy([OneHotSampler(), NoopStrategy()])
+    qf = MlpMemoryQFunction(
         name_or_scope="critic",
         env_spec=env.spec,
     )
@@ -65,21 +67,20 @@ def run_linear_ocm_exp(variant):
 
 if __name__ == '__main__':
     n_seeds = 1
-    exp_prefix = "2-12-dev-affine-softmax-policy-ocm"
-
+    exp_prefix = "2-14-dev-linear-ocm-sweep"
     """
     DDPG Params
     """
     n_batches_per_epoch = 100
     n_batches_per_eval = 100
     batch_size = 64
-    n_epochs = 1000
+    n_epochs = 100
     replay_pool_size = 100
 
     USE_EC2 = False
     exp_count = -1
-    for H in [1]:
-        for num_values in [2]:
+    for H in [2, 8, 16]:
+        for num_values in [2, 8, 16]:
             print("H", H)
             print("num_values", num_values)
             exp_count += 1
@@ -95,6 +96,8 @@ if __name__ == '__main__':
                 epoch_length=epoch_length,
                 eval_samples=eval_samples,
                 max_path_length=max_path_length,
+                # qf_learning_rate=1e-1,
+                # policy_learning_rate=1e-1,
             )
             variant = dict(
                 H=H,
