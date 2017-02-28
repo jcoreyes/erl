test and fix softmax function

railrl/misc/np_util.py
@@ -42,4 +42,5 @@ def to_onehot(x, num_values):
 def softmax(x, axis=-1):
     """Compute softmax values for each sets of scores in x."""
     e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum(axis=0)
+    denom = np.expand_dims(e_x.sum(axis=axis), axis=axis)
+    return e_x / denom