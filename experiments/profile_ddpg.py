1beb8b806afb8a93f5a539385b79f360a0b5aff4
Vitchyr Pong <vitchyr@gmail.com>
2017-03-05 15:30:10 -0800

    have faster settings for profiling

--- experiments/profile_ddpg.py
+++ experiments/profile_ddpg.py
@@ -1,14 +1,13 @@
 """
 Exampling of running DDPG on HalfCheetah.
 """
-
+from railrl.launchers.launcher_util import run_experiment_here
 from railrl.policies.nn_policy import FeedForwardPolicy
 from railrl.qfunctions.nn_qfunction import FeedForwardCritic
 from railrl.algos.ddpg import DDPG
 
 from rllab.envs.mujoco.half_cheetah_env import HalfCheetahEnv
 from rllab.exploration_strategies.ou_strategy import OUStrategy
-from rllab.misc.instrument import run_experiment_lite
 
 
 def example(*_):
@@ -27,20 +26,16 @@ def example(*_):
         es,
         policy,
         qf,
-        n_epochs=100,
+        n_epochs=25,
+        epoch_length=1000,
         batch_size=1024,
     )
     algorithm.train()
 
 
 if __name__ == "__main__":
-    run_experiment_lite(
+    run_experiment_here(
         example,
-        n_parallel=1,
-        snapshot_mode="last",
-        exp_prefix="ddpg-half-cheetah",
+        exp_prefix="3-5-profile-ddpg-half-cheetah",
         seed=2,
-        use_cloudpickle=True,
-        mode="ec2",
-        python_command="python -m cProfile -o 3-5-ddpg-test.prof",
     )
