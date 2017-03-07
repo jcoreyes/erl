bf8f6bc42630f7d1b568fc86961200dd0a06e6df
Vitchyr Pong <vitchyr@gmail.com>
2017-03-07 08:48:52 -0800

    set default params

--- experiments/naf_dev.py
+++ experiments/naf_dev.py
@@ -1,7 +1,12 @@
 """
 Fiddle with NAF
 """
-from railrl.launchers.launcher_util import run_experiment
+import random
+from railrl.launchers.launcher_util import (
+    run_experiment,
+    get_standard_env_ids,
+    get_env_settings,
+)
 from railrl.qfunctions.quadratic_naf_qfunction import QuadraticNAF
 from railrl.qfunctions.unbiased.unbiased_naf_qfunction import UnbiasedNAF
 from railrl.algos.naf import NAF
@@ -10,13 +15,16 @@ from rllab.envs.mujoco.half_cheetah_env import HalfCheetahEnv
 from rllab.exploration_strategies.ou_strategy import OUStrategy
 from rllab.envs.normalized_env import normalize
 
+from railrl.policies.nn_policy import FeedForwardPolicy
+from railrl.qfunctions.nn_qfunction import FeedForwardCritic
+from railrl.algos.ddpg import DDPG
 
-def example(*_):
-    env = HalfCheetahEnv()
-    env = normalize(env)
+
+def example(_):
+    env_settings = get_env_settings('random2d')
+    env = env_settings['env']
     es = OUStrategy(env_spec=env.spec)
-    # naf_qfunction = QuadraticNAF(
-    naf_qfunction = UnbiasedNAF(
+    naf_qfunction = QuadraticNAF(
         name_or_scope="naf_q",
         env_spec=env.spec,
     )
@@ -24,17 +32,38 @@ def example(*_):
         env,
         es,
         naf_qfunction,
-        n_epochs=20,
+        n_epochs=25,
         batch_size=1024,
-        epoch_length=1000,
-        eval_samples=500,
+        epoch_length=10000,
+        eval_samples=1000,
+        replay_pool_size=50000,
     )
+    # qf = FeedForwardCritic(
+    #     name_or_scope="critic",
+    #     env_spec=env.spec,
+    # )
+    # policy = FeedForwardPolicy(
+    #     name_or_scope="actor",
+    #     env_spec=env.spec,
+    # )
+    # algorithm = DDPG(
+    #     env,
+    #     es,
+    #     policy,
+    #     qf,
+    #     n_epochs=100,
+    #     batch_size=1024,
+    #     epoch_length=1000,
+    #     eval_samples=100,
+    #     min_pool_size=1000,
+    # )
     algorithm.train()
 
 
 if __name__ == "__main__":
+    seed = random.randint(0, 100000)
     run_experiment(
         example,
         exp_prefix="dev-naf",
-        seed=2,
+        seed=seed,
     )
