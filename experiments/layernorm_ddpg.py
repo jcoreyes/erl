197a541caa0f00fcb0db889173aab91e562c5940
Vitchyr Pong <vitchyr@gmail.com>
2017-03-16 19:55:33 -0700

    add parameter sweep

--- experiments/layernorm_ddpg.py
+++ experiments/layernorm_ddpg.py
@@ -6,46 +6,53 @@ from railrl.launchers.algo_launchers import (
     random_action_launcher,
 )
 from railrl.launchers.launcher_util import run_experiment
+from railrl.misc import hyperparameter as hp
 
 
 def main():
-    variant = dict(
-        algo_params=dict(
-            batch_size=128,
-            n_epochs=50,
-            epoch_length=100,
-            eval_samples=100,
-            discount=0.99,
-            qf_learning_rate=1e-2,
-            policy_learning_rate=1e-2,
-            soft_target_tau=0.01,
-            replay_pool_size=1000000,
-            min_pool_size=256,
-            scale_reward=1.0,
-            max_path_length=1000,
-            qf_weight_decay=0.00,
-            n_updates_per_time_step=5,
-        ),
-        env_params=dict(
-            env_id='cart',
-            normalize_env=True,
-            gym_name="",
-        ),
-        policy_params=dict(
-            layer_norm=True,
-        ),
-        qf_params=dict(
-            layer_norm=True,
-        ),
-    )
-    seed = 0
-    run_experiment(
-        my_ddpg_launcher,
-        exp_prefix="dev-layernorm-ddpg",
-        seed=seed,
-        variant=variant,
-        mode="here",
-    )
+    num_hyperparameters = 20
+    layer_norm = False
+    sweeper = hp.RandomHyperparameterSweeper([
+        hp.LogFloatParam("qf_learning_rate", 1e-5, 1e-1),
+        hp.LogFloatParam("policy_learning_rate", 1e-5, 1e-1),
+        hp.LogFloatParam("scale_reward", 10.0, 0.001),
+        hp.LogFloatParam("discount", 0.5, 0.99),
+    ])
+    for seed in range(num_hyperparameters):
+        params_dict = sweeper.generate_random_hyperparameters()
+        variant = dict(
+            algo_params=dict(
+                batch_size=128,
+                n_epochs=50,
+                epoch_length=1000,
+                eval_samples=1000,
+                replay_pool_size=1000000,
+                min_pool_size=256,
+                max_path_length=1000,
+                qf_weight_decay=0.00,
+                n_updates_per_time_step=5,
+                soft_target_tau=0.01,
+                **params_dict
+            ),
+            env_params=dict(
+                env_id='cart',
+                normalize_env=True,
+                gym_name="",
+            ),
+            policy_params=dict(
+                layer_norm=layer_norm,
+            ),
+            qf_params=dict(
+                layer_norm=layer_norm,
+            ),
+        )
+        run_experiment(
+            my_ddpg_launcher,
+            exp_prefix="3-16-cartpole-ddpg-sweep-test",
+            seed=seed,
+            variant=variant,
+            mode="ec2",
+        )
 
 
 if __name__ == "__main__":
