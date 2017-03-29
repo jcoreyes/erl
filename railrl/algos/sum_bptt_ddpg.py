22b398b847a28a2f6cd99090fcb27affc22dbd20
Vitchyr Pong <vitchyr@gmail.com>
2017-03-28 21:51:33 -0700

    attempt 1 at this sum bptt

--- railrl/algos/sum_bptt_ddpg.py
+++ railrl/algos/sum_bptt_ddpg.py
@@ -11,13 +11,14 @@ class SumBpttDDPG(BpttDDPG):
     """
     BpttDDPT but sum the Q values as you unroll the policy.
     """
+
     def _init_policy_ops(self):
         self._rnn_inputs_ph = tf.placeholder(
             tf.float32,
             [None, self._num_bptt_unrolls, self._env_obs_dim],
             name='rnn_time_inputs',
         )
-        rnn_inputs = tf.unstack(self._rnn_inputs_ph, axis=1)
+        all_env_obs = tf.unstack(self._rnn_inputs_ph, axis=1)
         self._rnn_init_state_ph = self.policy.get_init_state_placeholder()
 
         self._rnn_cell_scope.reuse_variables()
@@ -26,29 +27,77 @@ class SumBpttDDPG(BpttDDPG):
             self._rnn_outputs, self._rnn_final_state = (
                 tf.contrib.rnn.static_rnn(
                     wrapper_rnn_cell,
-                    rnn_inputs,
+                    all_env_obs,
                     initial_state=self._rnn_init_state_ph,
                     dtype=tf.float32,
                     scope=self._rnn_cell_scope,
                 )
             )
-        # self._final_rnn_action = self._rnn_outputs[-1]
         all_env_actions, all_write_actions = zip(*self._rnn_outputs)
+        """
+        Creating all_rnn_observations is a bit tricky. Let X be shorthand for
+        the memories in all_rnn_observations. We want
+        
+            X[0] = batch element 0, T = 0 <-- from self._rnn_init_state_ph
+            X[1] = batch element 0, T = 1 <-- from all_write_actions[0]
+            X[2] = batch element 0, T = 2 <-- from all_write_actions[1]
+            ...
+            X[N-1] = batch element 0, T = N-1 <-- from self._rnn_init_state_ph
+            X[N] = batch element 1, T = 0     <-- from all_write_actions[0]
+            X[N+1] = batch element 1, T = 1   <-- from all_write_actions[0]
+            ...
+            
+        Basically, for each element in the batch, the first element needs to 
+        come from `self._rnn_init_state_ph`, while the next N-1 elements need to
+        come from all_write_actions. 
+        
+        Let N be self._num_bptt_unrolls.
+        all_write_actions is a list of N tensors, each w/ shape [None x memory].
+        
+        Here's how I do this:
+          1. Convert the first N-1 all_write_actions into a tensor of shape
+              [None x N-1 x memory]
+          2. Convert `self._rnn_init_state_ph` into shape [None x 1 x memory]
+          3. Combine the above two into a tensor of shape [None x N x memory]
+          4. Convert this into a list of N tensors, each w/ shape [None x mem]
+        """
+        memory_dim = self.observation_dim - self._env_obs_dim
+
+        # Step 1
+        outputted_memories = tf.stack(all_write_actions[:-1], axis=1)
+        outputted_memories.get_shape().assert_is_compatible_with(
+            (0, self._num_bptt_unrolls - 1, memory_dim)
+        )
+
+        # Step 2
+        expanded_init = tf.expand_dims(input=self._rnn_init_state_ph, axis=1)
+        expanded_init.get_shape().assert_is_compatible_with(
+            (0, 1, memory_dim)
+        )
+
+        # Step 3
+        all_memory_obs = tf.concat(
+            axis=1,
+            values=[
+                expanded_init,
+                outputted_memories,
+            ],
+        )
+        all_memory_obs.get_shape().assert_is_compatible_with(
+            (0, self._num_bptt_unrolls, memory_dim)
+        )
+
+        # Step 4
+        all_memories = tf.unstack(all_memory_obs, axis=1)
+
+        all_rnn_observations = (
+            tf.concat(axis=0, values=all_env_obs),
+            tf.concat(axis=0, values=all_memories),
+        )
+
         all_rnn_actions = (
             tf.concat(axis=0, values=all_env_actions),
             tf.concat(axis=0, values=all_write_actions),
-        )
-
-        # I think this is wrong because it's appending the first obs to the
-        # front of everything, rather than in front of each individual
-        # subtrajectory
-        # TODO(vitchyr): fix this
-        all_memory_obs_list = (
-            [self._rnn_init_state_ph] + list(all_write_actions[:-1])
-        )
-        all_rnn_observations = (
-            tf.concat(axis=0, values=rnn_inputs),
-            tf.concat(axis=0, values=all_memory_obs_list),
         )
 
         self.qf_with_action_input = self.qf.get_weight_tied_copy(
@@ -67,9 +116,8 @@ class SumBpttDDPG(BpttDDPG):
         :param obs: See output of `self._split_flat_action`.
         :return: Feed dictionary for policy training TensorFlow ops.
         """
-        first_obs = self._get_time_step(obs, 0)
+        _, initial_memory_obs = self._get_time_step(obs, 0)
         env_obs, _ = obs
-        initial_memory_obs = first_obs[1]
         return {
             self._rnn_inputs_ph: env_obs,
             self._rnn_init_state_ph: initial_memory_obs,
