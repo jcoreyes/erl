b5b93bfe5488e9aaa9c5bb81f08750071adaeb27
Vitchyr Pong <vitchyr@gmail.com>
2017-05-02 19:23:53 -0700

    DELETE delete unused code

--- railrl/algos/regress_q_bptt_ddpg.py
+++ railrl/algos/regress_q_bptt_ddpg.py
@@ -76,9 +76,6 @@ class RegressQBpttDdpg(BpttDDPG):
         if self.oracle_memory_grad is None:
             logger.log("WARNING: Oracle memory gradients set to zero.")
             self.oracle_memory_grad = tf.zeros_like(final_memory_action)
-        if self.memory_grad is None:
-            logger.log("WARNING: Memory gradients set to zero.")
-            self.memory_grad = tf.zeros_like(final_memory_action)
 
         self.mem_grad_cosine_distance = tf_util.cosine(self.oracle_memory_grad,
                                                        self.memory_grad)
