45b81522ecc270ad1e0416b22038ca89c2ad601c
Vitchyr Pong <vitchyr@gmail.com>
2017-05-10 20:30:15 -0700

    delete unneeded code

--- railrl/algos/bptt_ddpg.py
+++ railrl/algos/bptt_ddpg.py
@@ -223,54 +223,17 @@ class BpttDDPG(DDPG):
     def _qf_feed_dict_from_batch(self, batch):
         flat_batch = self.subtraj_batch_to_flat_augmented_batch(batch)
         feed = self._qf_feed_dict(
-            flat_batch['rewards'],
-            flat_batch['terminals'],
-            flat_batch['obs'],
-            flat_batch['actions'],
-            flat_batch['next_obs'],
+            rewards=flat_batch['rewards'],
+            terminals=flat_batch['terminals'],
+            obs=flat_batch['obs'],
+            actions=flat_batch['actions'],
+            next_obs=flat_batch['next_obs'],
             target_numbers=flat_batch['target_numbers'],
             times=flat_batch['times'],
         )
         return feed
 
     def subtraj_batch_to_flat_augmented_batch(self, batch):
-        """
-        The batch is a bunch of subsequences. Flatten the subsequences so
-        that they just look like normal (s, a, s') tuples.
-        
-        Also, the actions/observations are split into their respective 
-        augmented parts.
-        """
-        rewards = batch['rewards']
-        terminals = batch['terminals']
-        obs = batch['observations']
-        actions = batch['actions']
-        next_obs = batch['next_observations']
-        target_numbers = batch['target_numbers']
-        times = batch['times']
-
-        flat_actions = actions.reshape(-1, actions.shape[-1])
-        flat_obs = obs.reshape(-1, obs.shape[-1])
-        flat_next_obs = next_obs.reshape(-1, next_obs.shape[-1])
-        flat_target_numbers = target_numbers.flatten()
-        flat_times = times.flatten()
-        flat_terminals = terminals.flatten()
-        flat_rewards = rewards.flatten()
-
-        split_flat_obs = self._split_flat_obs(flat_obs)
-        split_flat_actions = self._split_flat_actions(flat_actions)
-        split_flat_next_obs = self._split_flat_obs(flat_next_obs)
-        return dict(
-            rewards=flat_rewards,
-            terminals=flat_terminals,
-            obs=split_flat_obs,
-            actions=split_flat_actions,
-            next_obs=split_flat_next_obs,
-            target_numbers=flat_target_numbers,
-            times=flat_times,
-        )
-
-    def subtraj_batch_to_last_augmented_batch(self, batch):
         """
         The batch is a bunch of subsequences. Flatten the subsequences so
         that they just look like normal (s, a, s') tuples.
