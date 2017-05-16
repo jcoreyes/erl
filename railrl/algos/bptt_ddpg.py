bfe2e41151641f602a8714bf50e18db7a9f0adf6
Vitchyr Pong <vitchyr@gmail.com>
2017-05-16 12:41:23 -0700

    make write action only optimize bellman error

--- railrl/algos/bptt_ddpg.py
+++ railrl/algos/bptt_ddpg.py
@@ -543,15 +543,14 @@ class BpttDDPG(DDPG):
         self.dloss_dmems_subsequences = tf.stack(self.dloss_dmems, axis=1)
 
     def _init_policy_loss_and_train_ops(self):
-        self.policy_surrogate_loss = self._get_policy_train_loss()
-        self.train_policy_op = self._get_policy_train_op(
-            self.policy_surrogate_loss
-        )
-
-    def _get_policy_train_loss(self):
-        loss = - tf.reduce_mean(
+        self.policy_env_action_loss = - tf.reduce_mean(
             self.qf_with_action_input.output
         )
+        policy_env_params = self.policy.get_params(env_only=True)
+        self.train_policy_op_env = tf.train.AdamOptimizer(
+            self.policy_learning_rate
+        ).minimize(self.policy_env_action_loss, var_list=policy_env_params)
+
         self._saved_write_gradients = tf.placeholder(
             tf.float32,
             self.all_writes_subsequences.get_shape(),
@@ -568,49 +567,20 @@ class BpttDDPG(DDPG):
             tf.reduce_sum(self._saved_write_losses)
             * self.saved_write_loss_weight
         )
-        # self.dL_dtheta_actually_applied = tf.gradients(
-        #     self._saved_write_loss,
-        #     self.wparam,
-        # )[0]
-        # self.dL_dwrite_actually_applied = tf.gradients(
-        #     self._saved_write_loss,
-        #     self.all_writes_list
-        # )
-        if self.saved_write_loss_weight > 0:
-            loss += self._saved_write_loss
-        if self._bpt_bellman_error_weight > 0.:
-            loss += (
-                self.bellman_error_for_policy * self._bpt_bellman_error_weight
-            )
-        return loss
+        write_loss = self._saved_write_loss * self.saved_write_loss_weight
+        write_loss += (
+            self.bellman_error_for_policy * self._bpt_bellman_error_weight
+        )
+        self.policy_write_action_loss = write_loss
+        policy_write_params = self.policy.get_params(write_only=True)
+        self.train_policy_op_write = tf.train.AdamOptimizer(
+            self.write_policy_learning_rate
+        ).minimize(self.policy_write_action_loss, var_list=policy_write_params)
 
-    def _get_policy_train_op(self, loss):
-        if not self.train_policy:
-            return None
-
-        if self.write_policy_learning_rate is None:
-            trainable_policy_params = self.policy.get_params_internal()
-            return tf.train.AdamOptimizer(
-                self.policy_learning_rate
-            ).minimize(loss, var_list=trainable_policy_params)
-
-        policy_env_params = self.policy.get_params(env_only=True)
-        if self.write_policy_learning_rate == 0.:
-            return tf.train.AdamOptimizer(
-                self.policy_learning_rate
-            ).minimize(loss, var_list=policy_env_params)
-        else:
-            policy_write_params = self.policy.get_params(write_only=True)
-            self.train_policy_op_env = tf.train.AdamOptimizer(
-                self.policy_learning_rate
-            ).minimize(loss, var_list=policy_env_params)
-            self.train_policy_op_write = tf.train.AdamOptimizer(
-                self.write_policy_learning_rate
-            ).minimize(loss, var_list=policy_write_params)
-            return [
-                self.train_policy_op_env,
-                self.train_policy_op_write,
-            ]
+        return [
+            self.train_policy_op_env,
+            self.train_policy_op_write,
+        ]
 
     def _get_policy_training_ops(self, **kwargs):
         return self._get_network_training_ops(
