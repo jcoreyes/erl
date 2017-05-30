d211333f1b8941fec78ab77f3cd724c37b714c00
Vitchyr Pong <vitchyr@gmail.com>
2017-05-30 11:37:39 -0700

    print some useful information

--- experiments/run_water_maze.py
+++ experiments/run_water_maze.py
@@ -5,6 +5,16 @@ env = WaterMaze()
 
 while True:
     env.reset()
-    for _ in range(100):
-        env.step(np.random.rand(2))
+    action = np.zeros(2)
+    last_reward_t = 0
+    print("---------- RESET ----------")
+    for t in range(100):
+        obs, reward, done, info = env.step(action)
+        if reward > 0:
+            time_to_goal = t - last_reward_t
+            if time_to_goal > 1:
+                print("Time to goal", time_to_goal)
+                last_reward_t = t
+        delta = obs[:2] - info['target_position']
+        action = - delta * 10
         env.render()
