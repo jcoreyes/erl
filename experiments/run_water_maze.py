e1088831cf67fa188d41c89756a37e6a1cae9d4b
Vitchyr Pong <vitchyr@gmail.com>
2017-05-31 13:26:09 -0700

    also try random policy

--- experiments/run_water_maze.py
+++ experiments/run_water_maze.py
@@ -1,6 +1,9 @@
 from railrl.envs.water_maze import WaterMaze, WaterMazeMemory
 import numpy as np
 import argparse
+
+from railrl.exploration_strategies.ou_strategy import OUStrategy
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--small", action='store_true', help="Use a small maze.")
 args = parser.parse_args()
@@ -8,14 +11,17 @@ args = parser.parse_args()
 env = WaterMazeMemory(use_small_maze=args.small, include_velocity=True)
 
 all_returns = []
+es = OUStrategy(env)
 while True:
     obs = env.reset()
+    es.reset()
     # print("init obs", obs)
-    action = np.zeros(2)
+    zero_action = np.zeros(2)
     last_reward_t = 0
     print("---------- RESET ----------")
     returns = 0
-    for t in range(101):
+    for t in range(200):
+        action = es.get_action_from_raw_action(zero_action)
         obs, reward, done, info = env.step(action)
         returns += reward
         # print("obs", obs)
@@ -26,8 +32,8 @@ while True:
                 # print("Time to goal", time_to_goal)
                 last_reward_t = t
         delta = obs[:2] - info['target_position']
-        action = - delta * 10
-        env.render()
+        # action = - delta * 10
+        # env.render()
     print("Returns", returns)
     all_returns.append(returns)
     print("Returns Mean", np.mean(all_returns))
