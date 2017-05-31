5820fae13c9bd4c299390f3b3de60b72f99d395c
Vitchyr Pong <vitchyr@gmail.com>
2017-05-30 21:39:15 -0700

    make noop strat clip action

--- railrl/exploration_strategies/noop.py
+++ railrl/exploration_strategies/noop.py
@@ -3,14 +3,14 @@ from railrl.exploration_strategies.base import RawExplorationStrategy
 
 class NoopStrategy(RawExplorationStrategy):
     """
-    Exploration strategy that does nothing.
+    Exploration strategy that does nothing other than clip the action.
     """
 
-    def __init__(self, **kwargs):
-        pass
+    def __init__(self, env, **kwargs):
+        self.action_space = env.action_space
 
     def get_action(self, t, observation, policy, **kwargs):
         return policy.get_action(observation)
 
     def get_action_from_raw_action(self, action, **kwargs):
-        return action
+        return np.clip(action, self.action_space.low, self.action_space.high)
