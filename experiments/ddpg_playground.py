d302f4f8d48f66b7bd7ce4d8d5d90e8e1ab6a2d0
Vitchyr Pong <vitchyr@gmail.com>
2017-06-05 19:40:39 -0700

    check hard vs soft target updates on cartpole

--- experiments/ddpg_playground.py
+++ experiments/ddpg_playground.py
@@ -10,16 +10,19 @@ from railrl.exploration_strategies.ou_strategy import OUStrategy
 from railrl.launchers.launcher_util import run_experiment
 from railrl.policies.nn_policy import FeedForwardPolicy
 from railrl.qfunctions.nn_qfunction import FeedForwardCritic
-from railrl.algos.ddpg import DDPG
+from railrl.algos.ddpg import DDPG, TargetUpdateMode
 from rllab.envs.box2d.cartpole_env import CartpoleEnv
 from rllab.envs.gym_env import GymEnv
+from rllab.envs.mujoco.half_cheetah_env import HalfCheetahEnv
+from rllab.envs.normalized_env import normalize
 
 
-def example(*_):
+def example(variant):
     # env = HalfCheetahEnv()
-    pointenv = PointEnv()
+    # pointenv = PointEnv()
     # env = ProxyEnv(PendulumEnv())
     env = CartpoleEnv()
+    env = normalize(env)
     # env = gym_env("Pendulum-v0")
     es = OUStrategy(env_spec=env.spec)
     qf = FeedForwardCritic(
@@ -35,18 +38,29 @@ def example(*_):
         es,
         policy,
         qf,
-        n_epochs=30,
-        # eval_samples=100,
-        # epoch_length=1000,
-        batch_size=1024,
+        **variant['algo_params']
     )
     algorithm.train()
 
 
 if __name__ == "__main__":
-    run_experiment(
-        example,
-        exp_prefix="ddpg-half-cheetah",
-        seed=2,
-        mode='here',
+    variant = dict(
+        algo_params=dict(
+            n_epochs=100,
+            eval_samples=100,
+            epoch_length=1000,
+            batch_size=32,
+            # target_update_mode=TargetUpdateMode.SOFT,
+            # soft_target_tau=0.001,
+            target_update_mode=TargetUpdateMode.HARD,
+            hard_update_period=100,
+        ),
     )
+    for seed in range(8):
+        run_experiment(
+            example,
+            exp_prefix="6-5-ddpg-tf-hard-vs-soft",
+            seed=seed,
+            mode='ec2',
+            variant=variant,
+        )
