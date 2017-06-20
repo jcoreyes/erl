69cecb59d47f8a75ba06bb3620ed28c2b2d75586
Vitchyr Pong <vitchyr@gmail.com>
2017-06-20 15:32:03 -0700

    starting NAF

---
+++ experiments/pytorch/naf_test.py
@@ -0,0 +1,56 @@
+"""
+Experiment with NAF.
+"""
+from railrl.exploration_strategies.ou_strategy import OUStrategy
+from railrl.launchers.launcher_util import run_experiment
+from railrl.policies.torch import FeedForwardPolicy
+from railrl.qfunctions.torch import FeedForwardQFunction
+from railrl.torch.ddpg import DDPG
+from railrl.torch.naf import NAF, NafPolicy
+from rllab.envs.box2d.cartpole_env import CartpoleEnv
+
+from rllab.envs.normalized_env import normalize
+
+
+def example(variant):
+    # env = HalfCheetahEnv()
+    # env = PointEnv()
+    # env = gym_env("Pendulum-v0")
+    env = CartpoleEnv()
+    env = normalize(env)
+    es = OUStrategy(action_space=env.action_space)
+    qf = NafPolicy(
+        int(env.observation_space.flat_dim),
+        int(env.action_space.flat_dim),
+        400,
+    )
+    algorithm = NAF(
+        env,
+        exploration_strategy=es,
+        qf=qf,
+        **variant['algo_params']
+    )
+    algorithm.train()
+
+
+if __name__ == "__main__":
+    use_gpu = True
+    variant = dict(
+        algo_params=dict(
+            num_epochs=10,
+            num_steps_per_epoch=1000,
+            num_steps_per_eval=100,
+            target_hard_update_period=10000,
+            # use_soft_update=True,
+            batch_size=32,
+            max_path_length=100,
+        )
+    )
+    run_experiment(
+        example,
+        exp_prefix="dev-pytorch-ddpg",
+        seed=0,
+        mode='here',
+        variant=variant,
+        use_gpu=use_gpu,
+    )
