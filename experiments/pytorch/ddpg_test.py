6b874bdab122ac3da47f502e871122a5d698148e
Vitchyr Pong <vitchyr@gmail.com>
2017-06-21 21:01:16 -0700

    change to cheetah for now

--- experiments/pytorch/ddpg_test.py
+++ experiments/pytorch/ddpg_test.py
@@ -7,15 +7,17 @@ from railrl.policies.torch import FeedForwardPolicy
 from railrl.qfunctions.torch import FeedForwardQFunction
 from railrl.torch.ddpg import DDPG
 from rllab.envs.box2d.cartpole_env import CartpoleEnv
+from rllab.envs.mujoco.half_cheetah_env import HalfCheetahEnv
+from rllab.envs.mujoco.hopper_env import HopperEnv
 
 from rllab.envs.normalized_env import normalize
 
 
 def example(variant):
-    # env = HalfCheetahEnv()
+    env = HalfCheetahEnv()
     # env = PointEnv()
     # env = gym_env("Pendulum-v0")
-    env = CartpoleEnv()
+    # env = HopperEnv()
     env = normalize(env)
     es = OUStrategy(action_space=env.action_space)
     qf = FeedForwardQFunction(
@@ -43,18 +45,18 @@ def example(variant):
 if __name__ == "__main__":
     variant = dict(
         algo_params=dict(
-            num_epochs=10,
-            num_steps_per_epoch=1000,
-            num_steps_per_eval=100,
+            num_epochs=50,
+            num_steps_per_epoch=10000,
+            num_steps_per_eval=1000,
             target_hard_update_period=10000,
             # use_soft_update=True,
-            batch_size=32,
-            max_path_length=100,
+            batch_size=128,
+            max_path_length=1000,
         )
     )
     run_experiment(
         example,
-        exp_prefix="dev-pytorch-ddpg",
+        exp_prefix="6-21-pytorch-ddpg-no-bn-cheetah",
         seed=0,
         mode='here',
         variant=variant,
