7d90113c8863f4cacaa066db5310e99396f5168c
Vitchyr Pong <vitchyr@gmail.com>
2017-06-28 18:01:48 -0700

    autodetect if the params look like they are log distrubted

--- scripts/analyze_hyperparameter_sweep.py
+++ scripts/analyze_hyperparameter_sweep.py
@@ -8,6 +8,9 @@ $ python analyze_hyperparameter_sweep.py path/to/exp_directory
 """
 import argparse
 from os.path import join
+from string import Template
+
+import numpy as np
 
 from fanova import visualizer, CategoricalHyperparameter
 
@@ -22,7 +25,34 @@ def get_param_importance(f, param):
     return result[param_name_tuple]['total importance']
 
 
-def generate_report(fanova_info: FanovaInfo, base_dir):
+def max_min_mean_error(x):
+    delta = x.max() - x.min()
+    uniform_mean_estimate = (x.max() + x.min()) / 2
+    return np.abs((x.mean() - uniform_mean_estimate) / delta)
+
+
+def is_data_log_uniformly_distributed(x):
+    bin_sizes, *_ = np.histogram(x)
+    is_decreasing = [
+        bin_sizes[i] > bin_sizes[i+1] for i in range(len(bin_sizes) - 1)
+    ]
+    deltas = [
+        bin_sizes[i] - bin_sizes[i+1] for i in range(len(bin_sizes) - 1)
+    ]
+    uniform_error = max_min_mean_error(x)
+    log_uniform_error = max_min_mean_error(np.log(x))
+    heuristics = [
+        np.mean(is_decreasing) > 0.7,
+        log_uniform_error + 0.1 < uniform_error,
+        bin_sizes[0] > np.mean(bin_sizes) * 1.5,
+        np.mean(deltas) > 0,
+    ]
+    return np.mean(heuristics) >= 0.5
+
+
+def generate_report(fanova_info: FanovaInfo, base_dir, param_name_to_log=None):
+    if param_name_to_log is None:
+        param_name_to_log = {}
     f, config_space, X, Y = fanova_info
     report = HTMLReport(
         join(base_dir, 'report.html'), images_per_row=3,
@@ -42,20 +72,32 @@ def generate_report(fanova_info: FanovaInfo, base_dir):
     vis = visualizer.Visualizer(f, config_space)
     cs_params = config_space.get_hyperparameters()
     importances = [get_param_importance(f, param) for param in cs_params]
-    param_and_importance = sorted(
-        zip(cs_params, importances),
+    are_logs = [is_data_log_uniformly_distributed(X[:, i])
+              for i in range(len(cs_params))]
+    data = sorted(
+        zip(cs_params, importances, are_logs),
         key=lambda x: -x[1],
     )
+
+    print("Guesses for is_log")
+    print("{")
+    for param, _, is_log in data:
+        name = param.name
+        print("    '{}': {},".format(name, is_log))
+    print("}")
 
     """
     Plot individual marginals.
     """
-    for param, importance in param_and_importance:
+    print("Creating marginal plots")
+    for param, importance, is_log in data:
         param_name = param.name
+        if param_name in param_name_to_log:
+            is_log = param_name_to_log[param_name]
         if isinstance(param, CategoricalHyperparameter):
             vis.plot_categorical_marginal(param_name, show=False)
         else:
-            vis.plot_marginal(param_name, show=False)
+            vis.plot_marginal(param_name, show=False, log_scale=is_log)
         img = vu.save_image()
         report.add_image(
             img,
@@ -65,6 +107,7 @@ def generate_report(fanova_info: FanovaInfo, base_dir):
     """
     Plot pairwise marginals.
     """
+    print("Creating pairwise-marginal plots")
     num_params = len(cs_params)
     num_pairs = num_params * (num_params + 1) // 2
     pair_and_importance = (
@@ -83,6 +126,7 @@ def generate_report(fanova_info: FanovaInfo, base_dir):
                 importance,
             ),
         )
+    print("Report saved to: {}".format(report.path))
 
 
 def main():
@@ -90,7 +134,10 @@ def main():
     parser.add_argument("expdir", help="experiment dir, e.g., /tmp/experiments")
     args = parser.parse_args()
     fanova_info = get_fanova_info(args.expdir)
-    generate_report(fanova_info, args.expdir)
+    are_logs = {
+    }
+
+    generate_report(fanova_info, args.expdir, are_logs)
 
 
 if __name__ == '__main__':
