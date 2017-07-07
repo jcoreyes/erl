1b147a388e4e5ec022f980bb434db85d20ea6549
Vitchyr Pong <vitchyr@gmail.com>
2017-07-07 14:19:11 -0700

    Added gridcraft

---
+++ railrl/envs/gridcraft/test.py
@@ -0,0 +1,34 @@
+from guided_prop.envs.gridcraft import REW_ARENA_64
+from guided_prop.envs.gridcraft.grid_env import GridEnv
+from guided_prop.envs.gridcraft.grid_spec import *
+import gym.spaces.prng as prng
+import numpy as np
+
+if __name__ == "__main__":
+    prng.seed(2)
+
+    maze_spec = \
+        spec_from_string("SOOOO#R#OO\\"+
+                         "OSOOO#2##O\\" +
+                         "###OO#3O#O\\" +
+                         "OOOOO#OO#O\\" +
+                         "OOOOOOOOOO\\"
+                         )
+
+    #maze_spec = spec_from_sparse_locations(50, 50, {START: [(25,25)], REWARD: [(45,45)]})
+    maze_spec = REW_ARENA_64
+
+    env = GridEnv(maze_spec, one_hot=True, add_eyes=True, coordinate_wise=True)
+
+    s = env.reset()
+    #env.render()
+
+    obses = []
+    for t in range(10):
+        a = env.action_space.sample()
+        obs, r, done, infos = env.step(a, verbose=True)
+        obses.append(obs)
+    obses = np.array(obses)
+
+    paths = [{'observations': obses}]
+    env.plot_trajs(paths)
