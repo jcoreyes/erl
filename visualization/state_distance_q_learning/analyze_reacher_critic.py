c5d4b374951e048ff50d214569c912001f34caa8
Vitchyr Pong <vitchyr@gmail.com>
2017-07-09 16:24:23 -0700

    finish visualize learned critic

--- visualization/state_distance_q_learning/analyze_reacher_critic.py
+++ visualization/state_distance_q_learning/analyze_reacher_critic.py
@@ -3,157 +3,89 @@ For the heatmap, I index into the Q function with Q[action_1, action_2].
 The state is fixed
 """
 
+from math import pi
 import argparse
-import itertools
 import os
+import subprocess
 import re
 from operator import itemgetter
 from pathlib import Path
+import os.path as osp
 
 import joblib
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import torch
+from gym.envs.mujoco import ReacherEnv
 from torch.autograd import Variable
 
-from railrl.envs.pygame.water_maze import WaterMaze
 import railrl.misc.visualization_util as vu
+from railrl.misc.html_report import HTMLReport
+from rllab.misc.instrument import query_yes_no
 
 USE_TIME = True
 
 
 def create_figure(
         titles,
-        list_of_list_of_heatmaps,
-        target_poses,
-        iteration_number
+        heatmaps,
+        # target_poses,
+        # iteration_number
 ):
-    num_heatmaps = len(list_of_list_of_heatmaps)
-    series_length = len(target_poses)
-    width = 5 * series_length
-    height = 10 * num_heatmaps
-    fig, axes = plt.subplots(
-        2 * num_heatmaps, series_length, figsize=(width, height)
-    )
-    for i, (heatmaps, target_pos) in enumerate(
-            zip(zip(*list_of_list_of_heatmaps), target_poses)
-    ):
-        # `heatmaps` is now a list of heatmaps, such that
-        # heatmaps[k] = list_of_list_of_heatmaps[k][i]
-        min_pos = max(target_pos - WaterMaze.TARGET_RADIUS,
-                      -WaterMaze.BOUNDARY_DIST)
-        max_pos = min(target_pos + WaterMaze.TARGET_RADIUS,
-                      WaterMaze.BOUNDARY_DIST)
-        state_values = heatmaps[0].x_values
-
-        names_and_heatmaps = list(zip(titles, heatmaps))
-        num_heatmaps = len(names_and_heatmaps)
-
+    num_heatmaps = len(heatmaps)
+    width = 5 * num_heatmaps
+    height = 5
+    fig, axes = plt.subplots(1, num_heatmaps, figsize=(width, height))
+    for i, (title, heatmap) in enumerate(zip(titles, heatmaps)):
         """
         Plot Estimated & Optimal QF
         """
-        for j, (title, heatmap) in enumerate(names_and_heatmaps):
-            ax = axes[j][i]
-            vu.plot_heatmap(fig, ax, heatmap)
-            ax.vlines([min_pos, max_pos], *ax.get_ylim())
-            ax.set_xlabel("Position")
-            ax.set_ylabel("Velocity")
-            ax.set_title("{0} QF. Target Position = {1}".format(
-                title,
-                target_pos,
-            ))
+        ax = axes[i]
+        vu.plot_heatmap(fig, ax, heatmap)
+        ax.set_xlabel("X-action")
+        ax.set_ylabel("Y-action")
+        ax.set_title(title)
 
-        """
-        Plot Estimated & Optimal VF
-        """
-        for j, (title, heatmap) in enumerate(names_and_heatmaps):
-            ax = axes[num_heatmaps + j][i]
-            ax.plot(state_values, np.max(heatmap.values, axis=1))
-            ax.vlines([min_pos, max_pos], *ax.get_ylim())
-            ax.set_xlabel("Position")
-            ax.set_ylabel("Value Function")
-            ax.set_title("{0} VF. Target Position = {1}".format(
-                title,
-                target_pos,
-            ))
-
-    fig.suptitle("Iteration = {0}".format(iteration_number))
     return fig
 
 
-def create_qf_eval_fnct(qf, target_pos, pos):
-    state = get_state(target_pos, pos)
-    state = Variable(torch.from_numpy(state)).float().unsqueeze(0)
+env = ReacherEnv()
+
+
+def create_qf_eval_fnct(qf, target_pos, joint_angles):
     def evaluate(x, y):
+        set_state(target_pos, joint_angles)
+        obs = env._get_obs()
+
         action = np.array([x, y])
         action = Variable(torch.from_numpy(action)).float().unsqueeze(0)
+        new_obs = np.hstack((obs, target_pos))
+        state = Variable(torch.from_numpy(new_obs)).float().unsqueeze(0)
         out = qf(state, action)
         return out.data.numpy()
+
     return evaluate
 
 
-def clip(value, min_value, max_value):
-    return max(min(value, max_value), min_value)
+def set_state(target_pos, joint_angles):
+    qpos, qvel = np.concatenate([joint_angles, target_pos]), np.zeros(4)
+    env.set_state(qpos, qvel)
 
 
-def find_nearest_idx(array, value):
-    return (np.abs(array - value)).argmin()
-
-
-def get_state(target_pos, position):
-    return np.concatenate([
-        np.cos(position),
-        np.sin(position),
-        target_pos,
-        np.zeros(2),
-        position - target_pos,
-        [0],
-    ])
-
-
-def create_optimal_qf(target_pos, state_bounds, action_bounds, discount_factor,
-                      *, resolution=10):
+def create_optimal_qf(target_pos, joint_angles):
     """
-    Do Q-learning to find the optimal Q-values
-    :param target_pos:
-    :param state_bounds:
-    :param action_bounds:
-    :param resolution:
-    :return:
+    Return
+        Q(s, a, s_g, 0) = - ||f(s, a) - s_g||^2
     """
-    def get_reward(state):
-        return int(target_pos - WaterMaze.TARGET_RADIUS
-                   <= state
-                   <= target_pos + WaterMaze.TARGET_RADIUS)
+    def qfunction(a1, a2):
+        action = np.array([a1, a2])
+        set_state(target_pos, joint_angles)
+        env.do_simulation(action, env.frame_skip)
+        pos = env.get_body_com('fingertip')[:2]
+        return -np.linalg.norm(pos - target_pos)
 
-    qf = np.zeros((resolution, resolution))  # state, action
-    state_values = np.linspace(*state_bounds, num=resolution)
-    action_values = np.linspace(*action_bounds, num=resolution)
-    alpha = 0.1
-    for _ in range(1000):
-        vf = np.max(qf, axis=1)
-        for action_i, state_i in itertools.product(range(resolution),
-                                                   range(resolution)):
-            next_state = clip(
-                action_values[action_i] + state_values[state_i],
-                *state_bounds
-            )
-            next_state_i = find_nearest_idx(state_values, next_state)
-            reward = get_reward(state_values[next_state_i])
-            qf[state_i, action_i] = (
-                (1 - alpha) * qf[state_i, action_i]
-                + alpha * (
-                    reward + discount_factor * vf[next_state_i]
-                )
-            )
-
-    def qf_fnct(state, action):
-        state_i = find_nearest_idx(state_values, state)
-        action_i = find_nearest_idx(action_values, action)
-        return qf[state_i, action_i]
-
-    return qf_fnct
+    return qfunction
 
 
 def get_path_and_iters(dir_path):
@@ -176,61 +108,57 @@ def main():
     base = base / args.folder_path
 
     path_and_iter = get_path_and_iters(base)
+    path, _ = path_and_iter[0]
+    data = joblib.load(str(path))
+    qf = data['qf']
 
-    resolution = 10
-    discount_factor = 0.99
-    state_bounds = (-WaterMaze.BOUNDARY_DIST, WaterMaze.BOUNDARY_DIST)
-    action_bounds = (-1, 1)
+    resolution = 20
+    joint_angles = np.array([pi / 2, pi / 2])
+    x_bounds = (-1, 1)
+    y_bounds = (-1, 1)
 
-    for path, iter_number in path_and_iter:
-        data = joblib.load(str(path))
-        qf = data['qf']
-        print("QF loaded from iteration %d" % iter_number)
+    report = HTMLReport(
+        str(base / 'report.html'), images_per_row=1
+    )
+    report.add_text("Joint Angles = {}".format(joint_angles))
 
-        heatmaps_t0 = []
-        heatmaps_t24 = []
-        optimal_heatmaps = []
-        target_poses = np.linspace(-5, 5, num=5)
-        for target_pos in target_poses:
-            qf_eval_t0 = create_qf_eval_fnct(qf, target_pos, 0)
-            heatmaps_t0.append(vu.make_heat_map(
-                qf_eval_t0,
-                x_bounds=state_bounds,
-                y_bounds=action_bounds,
-                resolution=resolution,
-            ))
-            qf_eval_t24 = create_qf_eval_fnct(qf, target_pos, 24)
-            heatmaps_t24.append(vu.make_heat_map(
-                qf_eval_t24,
-                x_bounds=state_bounds,
-                y_bounds=action_bounds,
-                resolution=resolution,
-            ))
-            optimal_qf_eval = create_optimal_qf(
-                target_pos,
-                state_bounds=state_bounds,
-                action_bounds=action_bounds,
-                resolution=resolution,
-                discount_factor=discount_factor,
-            )
-            optimal_heatmaps.append(vu.make_heat_map(
-                optimal_qf_eval,
-                x_bounds=state_bounds,
-                y_bounds=action_bounds,
-                resolution=resolution,
-            ))
+    for target_pos in [
+        np.array([0, 0]),
+        np.array([.1, .1]),
+        np.array([.1, -.1]),
+        np.array([-.1, .1]),
+        np.array([-.1, -.1]),
+    ]:
+        qf_eval = create_qf_eval_fnct(qf, target_pos, joint_angles)
+        qf_heatmap = vu.make_heat_map(
+            qf_eval,
+            x_bounds=x_bounds,
+            y_bounds=y_bounds,
+            resolution=resolution,
+        )
+        optimal_qf_eval = create_optimal_qf(target_pos, joint_angles)
+        optimal_heatmap = vu.make_heat_map(
+            optimal_qf_eval,
+            x_bounds=x_bounds,
+            y_bounds=y_bounds,
+            resolution=resolution,
+        )
 
         fig = create_figure(
-            ['Estimated T=0', 'Estimated T=24', 'Optimal'],
-            [heatmaps_t0, heatmaps_t24, optimal_heatmaps],
-            target_poses,
-            iter_number,
+            ['Estimated', 'Optimal'],
+            [qf_heatmap, optimal_heatmap],
         )
-        save_dir = base / "images"
-        if not save_dir.exists():
-            save_dir.mkdir()
-        save_file = save_dir / 'iter_{}.png'.format(iter_number)
-        fig.savefig(str(save_file), bbox_inches='tight')
+        img = vu.save_image(fig)
+        report.add_image(img, "Target Position = {}".format(target_pos))
+
+    abs_path = osp.abspath(report.path)
+    print("Report saved to: {}".format(abs_path))
+    report.save()
+    open_report = query_yes_no("Open report?", default="yes")
+    if open_report:
+        cmd = "xdg-open {}".format(abs_path)
+        print(cmd)
+        subprocess.call(cmd, shell=True)
 
 if __name__ == '__main__':
     main()
