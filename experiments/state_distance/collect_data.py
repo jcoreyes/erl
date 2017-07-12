0d5c08f579b600ccf753ccf9f8764d209f76b14a
Vitchyr Pong <vitchyr@gmail.com>
2017-07-12 10:45:36 -0700

    make collect-data save to its own dir

--- experiments/state_distance/collect_data.py
+++ experiments/state_distance/collect_data.py
@@ -1,26 +1,22 @@
 from pathlib import Path
-import pickle
-import os
-import os.path as osp
 
 from railrl.algos.qlearning.state_distance_q_learning import (
-    StateDistanceQLearning,
-    MultitaskPathSampler, StateDistanceQLearningSimple)
+    MultitaskPathSampler)
 from railrl.data_management.env_replay_buffer import EnvReplayBuffer
 from railrl.data_management.split_buffer import SplitReplayBuffer
 from railrl.envs.multitask.reacher_env import MultitaskReacherEnv
 from railrl.envs.wrappers import convert_gym_space
-from railrl.exploration_strategies.ou_strategy import OUStrategy
-from railrl.policies.torch import FeedForwardPolicy
+from railrl.exploration_strategies.gaussian_strategy import GaussianStrategy
+from railrl.launchers.launcher_util import run_experiment
 from railrl.policies.zero_policy import ZeroPolicy
-from railrl.qfunctions.torch import FeedForwardQFunction
 from rllab.config import LOG_DIR
 
 
 def main(variant):
     env = MultitaskReacherEnv()
     action_space = convert_gym_space(env.action_space)
-    es = OUStrategy(action_space=action_space)
+    # es = OUStrategy(action_space=action_space)
+    es = GaussianStrategy(action_space=action_space)
     exploration_policy = ZeroPolicy(
         int(action_space.flat_dim),
     )
@@ -46,17 +42,14 @@ def main(variant):
         **variant['algo_params']
     )
     sampler.collect_data()
-    out_dir = variant['out_dir']
-    save_file = osp.join(out_dir, 'data.pkl')
-    sampler.save_pool(save_file)
-    print("Saved to {}".format(save_file))
+    sampler.save_pool()
 
 
 if __name__ == '__main__':
     out_dir = Path(LOG_DIR) / 'datasets/generated'
-    out_dir /= '7-11-reacher'
-    min_num_steps_to_collect = 1000000
-    max_path_length = 1000
+    out_dir /= '7-11-reacher-gaussian-dev'
+    min_num_steps_to_collect = 100000
+    max_path_length = 100
     pool_size = min_num_steps_to_collect + max_path_length
 
     # noinspection PyTypeChecker
@@ -68,4 +61,15 @@ if __name__ == '__main__':
         ),
         pool_size=pool_size,
     )
-    main(variant)
+    # main(variant)
+    run_experiment(
+        main,
+        exp_prefix='gaussian',
+        seed=0,
+        mode='here',
+        variant=variant,
+        exp_id=0,
+        use_gpu=True,
+        snapshot_mode='last',
+        base_log_dir=str(out_dir),
+    )
