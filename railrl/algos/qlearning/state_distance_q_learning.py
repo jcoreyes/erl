75dfac37e99b2ec6b3ec9e7a36d7160b68c4590b
Vitchyr Pong <vitchyr@gmail.com>
2017-07-12 10:46:42 -0700

    save pool to log dir

--- railrl/algos/qlearning/state_distance_q_learning.py
+++ railrl/algos/qlearning/state_distance_q_learning.py
@@ -150,12 +150,14 @@ class MultitaskPathSampler(object):
             else:
                 obs = next_ob
 
-    def save_pool(self, filename):
+    def save_pool(self):
         # train_file = os.path.join(dir_name, 'train.pkl')
         # validation_file = os.path.join(dir_name, 'validation.pkl')
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        out_dir = logger.get_snapshot_dir()
+        filename = os.path.join(out_dir, 'data.pkl')
         with open(filename, 'wb') as handle:
             pickle.dump(self.pool, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        print("Saved to {}".format(filename))
 
     def reset_env(self):
         self.exploration_strategy.reset()
