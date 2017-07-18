c0cb3bc394c618d743134c5dec55f7a8ba3e4e85
Vitchyr Pong <vitchyr@gmail.com>
2017-07-17 21:03:42 -0700

    update/fix tests

--- tests/test_reacher_env.py
+++ tests/test_reacher_env.py
@@ -4,6 +4,7 @@ import numpy as np
 from railrl.envs.multitask.reacher_env import (
     MultitaskReacherEnv,
     SimpleReacherEnv,
+    GoalStateReacherEnv,
 )
 from railrl.testing.np_test_case import NPTestCase
 
@@ -43,11 +44,29 @@ class TestSimpleReacherEnv(NPTestCase):
                     np.expand_dims(obs, 0),
                     np.expand_dims(env.goal, 0),
                 )
-                distance = np.array([np.linalg.norm(obs[-3:])])
                 true_reward = np.array([info_dict['reward_dist']])
-                self.assertNpAlmostEqual(-distance, true_reward)
                 self.assertNpAlmostEqual(estimated_reward, true_reward)
                 obs = next_obs
+
+
+class TestGoalStateReacherEnv(NPTestCase):
+    def test_compute_rewards(self):
+        env = GoalStateReacherEnv()
+        for _ in range(10):
+            obs = env.reset()
+            goal = np.expand_dims(obs, 0)
+            for _ in range(10):
+                action = np.random.uniform(-1, 1, 2)
+                next_obs, *_ = env.step(action)
+                next_obs = np.expand_dims(next_obs, 0)
+                estimated_reward = env.compute_rewards(
+                    None,
+                    None,
+                    next_obs,
+                    goal,
+                )
+                true_reward = -np.linalg.norm(next_obs - goal, axis=1)
+                self.assertNpAlmostEqual(estimated_reward, true_reward)
 
 
 if __name__ == '__main__':
