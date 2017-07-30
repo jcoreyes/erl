f19735823a8131d75d8225171ab9d42f6d26a39a
Vitchyr Pong <vitchyr@gmail.com>
2017-07-30 15:18:58 -0700

    add script to test ddpg when increasing gamma

---
+++ experiments/qlearning/ddpg_increase_gamma.py
@@ -0,0 +1,97 @@
+"""
+Try increasing gamma slowly to see how it affects DDPG.
+"""
+import random
+
+from railrl.exploration_strategies.ou_strategy import OUStrategy
+from railrl.launchers.launcher_util import run_experiment
+from railrl.misc.ml_util import RampUpSchedule
+from railrl.policies.torch import FeedForwardPolicy
+from railrl.qfunctions.torch import FeedForwardQFunction
+from railrl.torch.ddpg import DDPG
+import railrl.torch.pytorch_util as ptu
+
+from rllab.envs.mujoco.ant_env import AntEnv
+from rllab.envs.mujoco.hopper_env import HopperEnv
+from rllab.envs.mujoco.swimmer_env import SwimmerEnv
+from rllab.envs.mujoco.half_cheetah_env import HalfCheetahEnv
+from rllab.envs.normalized_env import normalize
+
+
+def experiment(variant):
+    env = variant['env_class']()
+    env = normalize(env)
+    es = OUStrategy(action_space=env.action_space)
+    qf = FeedForwardQFunction(
+        int(env.observation_space.flat_dim),
+        int(env.action_space.flat_dim),
+        400,
+        300,
+    )
+    policy = FeedForwardPolicy(
+        int(env.observation_space.flat_dim),
+        int(env.action_space.flat_dim),
+        400,
+        300,
+    )
+    epoch_discount_schedule_class = variant['epoch_discount_schedule_class']
+    epoch_discount_schedule = epoch_discount_schedule_class(
+        **variant['epoch_discount_schedule_params']
+    )
+    algorithm = DDPG(
+        env,
+        exploration_strategy=es,
+        qf=qf,
+        policy=policy,
+        epoch_discount_schedule=epoch_discount_schedule,
+        **variant['algo_params']
+    )
+    if ptu.gpu_enabled():
+        algorithm.cuda()
+    algorithm.train()
+
+
+if __name__ == "__main__":
+    # noinspection PyTypeChecker
+    variant = dict(
+        algo_params=dict(
+            num_epochs=100,
+            # num_steps_per_epoch=10000,
+            # num_steps_per_eval=1000,
+            num_steps_per_epoch=100,
+            num_steps_per_eval=100,
+            use_soft_update=True,
+            tau=1e-2,
+            batch_size=128,
+            # max_path_length=1000,
+            max_path_length=10,
+            discount=0.99,
+            qf_learning_rate=1e-3,
+            policy_learning_rate=1e-4,
+        ),
+        version="DDPG",
+        epoch_discount_schedule_class=RampUpSchedule,
+        epoch_discount_schedule_params=dict(
+            min_value=0.,
+            max_value=0.99,
+            ramp_duration=100,
+        ),
+    )
+    for env_class in [
+        SwimmerEnv,
+        HalfCheetahEnv,
+        AntEnv,
+        HopperEnv,
+    ]:
+        variant['env_class'] = env_class
+        variant['version'] = str(env_class)
+        for _ in range(5):
+            seed = random.randint(0, 999999)
+            run_experiment(
+                experiment,
+                exp_prefix="ddpg-increase-gamma",
+                seed=seed,
+                mode='here',
+                variant=variant,
+                use_gpu=True,
+            )
