95e829377ec33bd840cf66fbe07042049ce3720e
Vitchyr Pong <vitchyr@gmail.com>
2017-07-30 22:18:05 -0700

    added different run modes

--- experiments/state_distance/train_q.py
+++ experiments/state_distance/train_q.py
@@ -2,6 +2,10 @@ import argparse
 import pickle
 import random
 
+import numpy as np
+from hyperopt import hp
+
+import railrl.misc.hyperparameter as hyp
 import railrl.torch.pytorch_util as ptu
 from railrl.algos.state_distance.state_distance_q_learning import (
     StateDistanceQLearning,
@@ -9,22 +13,23 @@ from railrl.algos.state_distance.state_distance_q_learning import (
 from railrl.data_management.env_replay_buffer import EnvReplayBuffer
 from railrl.data_management.split_buffer import SplitReplayBuffer
 from railrl.envs.multitask.reacher_env import (
-    FullStateVaryingWeightReacherEnv,
-    XyMultitaskSimpleStateReacherEnv,
     GoalStateSimpleStateReacherEnv,
 )
 from railrl.envs.wrappers import convert_gym_space
 from railrl.exploration_strategies.gaussian_strategy import GaussianStrategy
+from railrl.launchers.launcher_util import (
+    create_log_dir,
+    create_run_experiment_multiple_seeds,
+)
 from railrl.launchers.launcher_util import run_experiment
+from railrl.misc.hypopt import optimize_and_save
 from railrl.misc.ml_util import RampUpSchedule
 from railrl.networks.state_distance import UniversalPolicy, UniversalQfunction
-from railrl.policies.torch import FeedForwardPolicy
 from railrl.policies.zero_policy import ZeroPolicy
-from railrl.predictors.torch import Mlp
 from railrl.samplers.path_sampler import MultitaskPathSampler
 
 
-def main(variant):
+def experiment(variant):
     env_class = variant['env_class']
     env = env_class(**variant['env_params'])
     if variant['generate_data']:
@@ -75,7 +80,7 @@ def main(variant):
         int(observation_space.flat_dim),
         int(action_space.flat_dim),
         env.goal_dim,
-        [400, 300, 200],
+        **variant['qf_params']
     )
     policy = UniversalPolicy(
         int(observation_space.flat_dim),
@@ -110,14 +115,22 @@ if __name__ == '__main__':
                         help='path to the snapshot file')
     args = parser.parse_args()
 
-    n_seeds = 1
+    num_configurations = 1  # for random mode
+    n_seeds = 3
     mode = "here"
-    use_gpu = True
-    exp_prefix = "dev-sdqlr-split-qf-input"
-    snapshot_mode = 'gap'
+    exp_prefix = "dev-sdqlr-sweep-lr"
+    version = "Dev"
+    run_mode = "none"
+    snapshot_mode = "gap"
     snapshot_gap = 5
 
-    if mode == 'ec2':
+    n_seeds = 3
+    mode = "ec2"
+    exp_prefix = "sdqlr-sweep-lr-2"
+
+    run_mode = 'grid'
+    use_gpu = True
+    if mode != "here":
         use_gpu = False
 
     dataset_path = args.replay_path
@@ -127,7 +140,7 @@ if __name__ == '__main__':
         dataset_path=str(dataset_path),
         algo_params=dict(
             num_epochs=101,
-            num_batches_per_epoch=1000,
+            num_batches_per_epoch=5000,
             use_soft_update=True,
             tau=1e-3,
             batch_size=1000,
@@ -137,6 +150,9 @@ if __name__ == '__main__':
             sample_goals_from='replay_buffer',
             # sample_goals_from='environment',
             sample_discount=False,
+        ),
+        qf_params=dict(
+            hidden_sizes=[400, 300],
         ),
         epoch_discount_schedule_class=RampUpSchedule,
         epoch_discount_schedule_params=dict(
@@ -149,8 +165,7 @@ if __name__ == '__main__':
         # env_class=FullStateVaryingWeightReacherEnv,
         env_params=dict(
             add_noop_action=False,
-            # reward_weights=[0, 0, 1, 1, 1, 1],
-            reward_weights=[1, 1, 1, 1, 0.01, 0.01],
+            # reward_weights=[1, 1, 1, 1, 0, 0],
         ),
         sampler_params=dict(
             min_num_steps_to_collect=20000,
@@ -159,16 +174,106 @@ if __name__ == '__main__':
         ),
         generate_data=args.replay_path is None,
     )
+    if run_mode == 'grid':
+        search_space = {
+            'algo_params.qf_learning_rate': [1e-2, 1e-3, 1e-4, 1e-5, 1e-6],
+            'qf_params.hidden_sizes': [
+                [100, 100],
+                [400, 300],
+                [400, 300, 200],
+            ],
+        }
+        sweeper = hyp.DeterministicHyperparameterSweeper(
+            search_space, default_parameters=variant,
+        )
+        for exp_id, variant in enumerate(sweeper.iterate_hyperparameters()):
+            for i in range(n_seeds):
+                seed = random.randint(0, 10000)
+                run_experiment(
+                    experiment,
+                    exp_prefix=exp_prefix,
+                    seed=seed,
+                    mode=mode,
+                    variant=variant,
+                    exp_id=exp_id,
+                    sync_s3_log=True,
+                    sync_s3_pkl=True,
+                    periodic_sync_interval=3600,
+                    snapshot_mode=snapshot_mode,
+                    snapshot_gap=snapshot_gap,
+                )
+    if run_mode == 'hyperopt':
+        search_space = {
+            'float_param': hp.uniform(
+                'float_param',
+                0.,
+                5,
+            ),
+            'float_param2': hp.loguniform(
+                'float_param2',
+                np.log(0.01),
+                np.log(1000),
+            ),
+            'seed': hp.randint('seed', 10000),
+        }
 
-    seed = random.randint(0, 10000)
-    run_experiment(
-        main,
-        exp_prefix=exp_prefix,
-        seed=seed,
-        mode=mode,
-        variant=variant,
-        exp_id=0,
-        use_gpu=use_gpu,
-        snapshot_mode=snapshot_mode,
-        snapshot_gap=snapshot_gap,
-    )
+        base_log_dir = create_log_dir(exp_prefix=exp_prefix)
+
+        optimize_and_save(
+            base_log_dir,
+            create_run_experiment_multiple_seeds(
+                n_seeds,
+                experiment,
+                exp_prefix=exp_prefix,
+            ),
+            search_space=search_space,
+            extra_function_kwargs=variant,
+            maximize=True,
+            verbose=True,
+            load_trials=True,
+            num_rounds=500,
+            num_evals_per_round=1,
+        )
+    if run_mode == 'random':
+        hyperparameters = [
+            hyp.LinearFloatParam('foo', 0, 1),
+            hyp.LogFloatParam('bar', 1e-5, 1e2),
+        ]
+        sweeper = hyp.RandomHyperparameterSweeper(
+            hyperparameters,
+            default_kwargs=variant,
+        )
+        for _ in range(num_configurations):
+            for exp_id in range(n_seeds):
+                seed = random.randint(0, 10000)
+                variant = sweeper.generate_random_hyperparameters()
+                run_experiment(
+                    experiment,
+                    exp_prefix=exp_prefix,
+                    seed=seed,
+                    mode=mode,
+                    variant=variant,
+                    exp_id=exp_id,
+                    sync_s3_log=True,
+                    sync_s3_pkl=True,
+                    periodic_sync_interval=3600,
+                    snapshot_mode=snapshot_mode,
+                    snapshot_gap=snapshot_gap,
+                )
+    else:
+        for _ in range(n_seeds):
+            seed = random.randint(0, 10000)
+            run_experiment(
+                experiment,
+                exp_prefix=exp_prefix,
+                seed=seed,
+                mode=mode,
+                variant=variant,
+                exp_id=0,
+                use_gpu=use_gpu,
+                sync_s3_log=True,
+                sync_s3_pkl=True,
+                periodic_sync_interval=3600,
+                snapshot_mode=snapshot_mode,
+                snapshot_gap=snapshot_gap,
+            )
