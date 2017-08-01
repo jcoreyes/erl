20b2b721901bc156894e93634af1ce88187ef2df
Vitchyr Pong <vitchyr@gmail.com>
2017-08-01 13:11:05 -0700

    use util for collecting data

--- experiments/state_distance/collect_data.py
+++ experiments/state_distance/collect_data.py
@@ -1,53 +1,12 @@
-from pathlib import Path
-
-from railrl.samplers.path_sampler import MultitaskPathSampler
-from railrl.data_management.env_replay_buffer import EnvReplayBuffer
-from railrl.data_management.split_buffer import SplitReplayBuffer
+from railrl.algos.state_distance.util import get_replay_buffer
 from railrl.envs.multitask.reacher_env import (
-    XyMultitaskReacherEnv,
     XyMultitaskSimpleStateReacherEnv,
 )
-from railrl.envs.wrappers import convert_gym_space
-from railrl.exploration_strategies.gaussian_strategy import GaussianStrategy
 from railrl.launchers.launcher_util import run_experiment
-from railrl.policies.zero_policy import ZeroPolicy
-from rllab.config import LOG_DIR
 
 
 def main(variant):
-    env = XyMultitaskSimpleStateReacherEnv()
-    action_space = convert_gym_space(env.action_space)
-    es = GaussianStrategy(
-        action_space=action_space,
-        max_sigma=0.2,
-        min_sigma=0.2,
-    )
-    exploration_policy = ZeroPolicy(
-        int(action_space.flat_dim),
-    )
-    replay_buffer_size = variant['replay_buffer_size']
-    replay_buffer = SplitReplayBuffer(
-        EnvReplayBuffer(
-            replay_buffer_size,
-            env,
-            flatten=True,
-        ),
-        EnvReplayBuffer(
-            replay_buffer_size,
-            env,
-            flatten=True,
-        ),
-        fraction_paths_in_train=0.8,
-    )
-    sampler = MultitaskPathSampler(
-        env,
-        exploration_strategy=es,
-        exploration_policy=exploration_policy,
-        replay_buffer=replay_buffer,
-        **variant['algo_params']
-    )
-    sampler.collect_data()
-    sampler.save_replay_buffer()
+    get_replay_buffer(variant, save_replay_buffer=True)
 
 
 if __name__ == '__main__':
@@ -55,24 +14,29 @@ if __name__ == '__main__':
     # out_dir /= '7-25--xy-multitask-simple-state--100k--add-no-op'
     # out_dir = str(out_dir)
     out_dir = None
-    min_num_steps_to_collect = 1000000
+    min_num_steps_to_collect = 10000
     max_path_length = 1000
     replay_buffer_size = min_num_steps_to_collect + max_path_length
 
     # noinspection PyTypeChecker
     variant = dict(
         out_dir=str(out_dir),
-        algo_params=dict(
+        sampler_params=dict(
             min_num_steps_to_collect=min_num_steps_to_collect,
             max_path_length=max_path_length,
-            render=False,
+            render=True,
         ),
+        env_class=XyMultitaskSimpleStateReacherEnv,
+        env_params=dict(
+            add_noop_action=False,
+        ),
+        generate_data=True,
         replay_buffer_size=replay_buffer_size,
     )
     # main(variant)
     run_experiment(
         main,
-        exp_prefix='gaussian-1m',
+        exp_prefix='dev-uniform-10k',
         seed=0,
         mode='here',
         variant=variant,
