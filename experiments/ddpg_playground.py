fdecc376c96c35c4306864dc082f04bbcb28ebf5
Vitchyr Pong <vitchyr@gmail.com>
2017-09-07 22:46:41 -0700

    can sample paths in parallel. needed to pass np arrays not torch arrays

--- experiments/ddpg_playground.py
+++ experiments/ddpg_playground.py
@@ -1,37 +1,54 @@
 """
 Run DDPG on things.
 """
+# from gym.envs.mujoco import HalfCheetahEnv
 
-from railrl.algos.ddpg import DDPG, TargetUpdateMode
+from railrl.envs.rollout_env import RemoteRolloutEnv
+from railrl.envs.wrappers import convert_gym_space
 from railrl.exploration_strategies.ou_strategy import OUStrategy
 from railrl.launchers.launcher_util import run_experiment
-from railrl.policies.tensorflow.nn_policy import FeedForwardPolicy
-from railrl.qfunctions.nn_qfunction import FeedForwardCritic
-from rllab.envs.box2d.cartpole_env import CartpoleEnv
+from railrl.policies.torch import FeedForwardPolicy
+from railrl.qfunctions.torch import FeedForwardQFunction
+from railrl.torch.ddpg import DDPG
+from rllab.envs.mujoco.half_cheetah_env import HalfCheetahEnv
 from rllab.envs.normalized_env import normalize
 
 
+
 def example(variant):
-    # env = HalfCheetahEnv()
-    # pointenv = PointEnv()
-    # env = ProxyEnv(PendulumEnv())
-    env = CartpoleEnv()
-    env = normalize(env)
-    # env = gym_env("Pendulum-v0")
-    es = OUStrategy(env_spec=env.spec)
-    qf = FeedForwardCritic(
-        name_or_scope="critic",
-        env_spec=env.spec,
+    env = HalfCheetahEnv()
+    # env = normalize(env)
+    obs_space = convert_gym_space(env.observation_space)
+    action_space = convert_gym_space(env.action_space)
+    es = OUStrategy(action_space=action_space)
+    qf = FeedForwardQFunction(
+        int(obs_space.flat_dim),
+        int(action_space.flat_dim),
+        400,
+        300,
     )
-    policy = FeedForwardPolicy(
-        name_or_scope="actor",
-        env_spec=env.spec,
+    policy_class = FeedForwardPolicy
+    policy_params = dict(
+        obs_dim=int(obs_space.flat_dim),
+        action_dim=int(action_space.flat_dim),
+        fc1_size=400,
+        fc2_size=300,
     )
+    policy = policy_class(**policy_params)
+    remote_env = RemoteRolloutEnv(
+        HalfCheetahEnv,
+        {},
+        policy_class,
+        policy_params,
+        100,
+    )
+    path = remote_env.rollout(policy)
+    import ipdb; ipdb.set_trace()
     algorithm = DDPG(
         env,
-        es,
-        policy,
-        qf,
+        exploration_strategy=es,
+        qf=qf,
+        policy=policy,
         **variant['algo_params']
     )
     algorithm.train()
@@ -40,21 +57,18 @@ def example(variant):
 if __name__ == "__main__":
     variant = dict(
         algo_params=dict(
-            n_epochs=10,
-            eval_samples=100,
-            epoch_length=1000,
+            num_epochs=2,
+            num_steps_per_epoch=100,
+            num_steps_per_eval=100,
+            max_path_length=100,
             batch_size=32,
-            # target_update_mode=TargetUpdateMode.SOFT,
-            # soft_target_tau=0.001,
-            target_update_mode=TargetUpdateMode.HARD,
-            hard_update_period=1000,
         ),
     )
     run_experiment(
         example,
-        exp_prefix="dev-ddpg-tf",
+        exp_prefix="dev-ddpg",
         seed=0,
         mode='here',
         variant=variant,
-        use_gpu=True,
+        use_gpu=False,
     )
---
+++ railrl/envs/rollout_env.py
@@ -0,0 +1,87 @@
+import abc
+import ray
+
+from railrl.samplers.util import rollout
+from rllab.core.serializable import Serializable
+from rllab.envs.proxy_env import ProxyEnv
+
+
+class RolloutEnv(object):
+    """ Environment that support only full rollouts. """
+
+    @abc.abstractmethod
+    def rollout(self, *args, **kwargs):
+        """
+        Non-blocking method for doing rollouts.
+
+        :param args:
+        :param kwargs:
+        :return: None if no complete paths has been collected.
+        Otherwise return a path.
+        """
+        pass
+
+
+@ray.remote
+class RayEnv(object):
+    def __init__(
+            self,
+            env_class,
+            env_params,
+            policy_class,
+            policy_params,
+            max_path_length,
+    ):
+        self._env = env_class(**env_params)
+        self._policy = policy_class(**policy_params)
+        self._max_path_length = max_path_length
+
+    def rollout(self, policy_params):
+        self._policy.set_param_values_np(policy_params)
+        return rollout(self._env, self._policy, self._max_path_length)
+
+
+class RemoteRolloutEnv(ProxyEnv, RolloutEnv, Serializable):
+    """
+    A synchronous interface for a remote rollout environment.
+
+    This "environment" basically just talkst o the remote environment.
+    The main difference is that rollout will return None if the a path is not
+    ready, rather than
+    """
+    def __init__(
+            self,
+            env_class,
+            env_params,
+            policy_class,
+            policy_params,
+            max_path_length
+    ):
+        Serializable.quick_init(self, locals())
+        super().__init__(env_class(**env_params))
+        ray.init()
+        self._ray_env_id = RayEnv.remote(
+            env_class,
+            env_params,
+            policy_class,
+            policy_params,
+            max_path_length,
+        )
+        self._rollout_promise = None
+
+    def rollout(self, policy):
+        if self._rollout_promise is None:
+            policy_params = policy.get_param_values_np()
+            self._rollout_promise = self._ray_env_id.rollout.remote(
+                policy_params
+            )
+            return None
+
+        # Check if remote path has been collected.
+        paths, _ = ray.wait([self._rollout_promise], timeout=0)
+
+        if len(paths):
+            self._rollout_promise = None
+            return ray.get(paths[0])
+
+        return None
