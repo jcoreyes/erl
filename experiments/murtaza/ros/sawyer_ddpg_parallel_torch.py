3ab801c20e019ca4c1003db37dd1fabe9458ef68
¨Murtaza <mdalal@berkeley.edu>
2017-09-13 21:56:39 -0700

    added running multiple experiments functionality

--- experiments/murtaza/ros/sawyer_ddpg_parallel_torch.py
+++ experiments/murtaza/ros/sawyer_ddpg_parallel_torch.py
@@ -12,50 +12,74 @@ from railrl.policies.torch import FeedForwardPolicy
 from railrl.qfunctions.torch import FeedForwardQFunction
 from railrl.torch.algos.parallel_ddpg import ParallelDDPG
 from rllab.envs.normalized_env import normalize
-
+import ray
 
 def example(variant):
-    env_class = variant['env_class']
-    env_params = variant['env_params']
-    env = env_class(**env_params)
-    obs_space = convert_gym_space(env.observation_space)
-    action_space = convert_gym_space(env.action_space)
-    es_class = variant['es_class']
-    es_params = dict(
-        action_space=action_space,
-        **variant['es_params']
-    )
-    policy_class = variant['policy_class']
-    use_gpu = variant['use_gpu']
+    # env_class = variant['env_class']
+    # env_params = variant['env_params']
+    # env = env_class(**env_params)
+    # obs_space = convert_gym_space(env.observation_space)
+    # action_space = convert_gym_space(env.action_space)
+    # es_class = variant['es_class']
+    # es_params = dict(
+    #     action_space=action_space,
+    #     **variant['es_params']
+    # )
+    # policy_class = variant['policy_class']
+    # use_gpu = variant['use_gpu']
+    #
+    # if variant['normalize_env']:
+    #     env = normalize(env)
+    #
+    # es = es_class(**es_params)
+    # qf = FeedForwardQFunction(
+    #     int(env.observation_space.flat_dim),
+    #     int(env.action_space.flat_dim),
+    #     100,
+    #     100,
+    # )
+    # policy_params = dict(
+    #     obs_dim=int(obs_space.flat_dim),
+    #     action_dim=int(action_space.flat_dim),
+    #     fc1_size=100,
+    #     fc2_size=100,
+    # )
+    # policy = policy_class(**policy_params)
+    # remote_env = RemoteRolloutEnv(
+    #     env_class,
+    #     env_params,
+    #     policy_class,
+    #     policy_params,
+    #     es_class,
+    #     es_params,
+    #     variant['max_path_length'],
+    #     variant['normalize_env'],
+    # )
 
-    if variant['normalize_env']:
-        env = normalize(env)
-
-    es = es_class(**es_params)
     qf = FeedForwardQFunction(
-        int(env.observation_space.flat_dim),
-        int(env.action_space.flat_dim),
+        int(remote_env.observation_space.flat_dim),
+        int(remote_env.action_space.flat_dim),
         100,
         100,
     )
+    policy_class = FeedForwardPolicy
     policy_params = dict(
-        obs_dim=int(obs_space.flat_dim),
-        action_dim=int(action_space.flat_dim),
+        obs_dim=int(remote_env.observation_space.flat_dim),
+        action_dim=int(remote_env.action_space.flat_dim),
         fc1_size=100,
         fc2_size=100,
     )
     policy = policy_class(**policy_params)
-    remote_env = RemoteRolloutEnv(
-        env_class,
-        env_params,
-        policy_class,
-        policy_params,
-        es_class,
-        es_params,
-        variant['max_path_length'],
-        variant['normalize_env'],
+    es_class = OUStrategy
+    es_params = dict(
+        action_space=remote_env.action_space,
+        **{
+            'max_sigma': .25,
+            'min_sigma': .25,
+        }
     )
-
+    es = es_class(**es_params)
+    use_gpu = variant['use_gpu']
     algorithm = ParallelDDPG(
         remote_env,
         exploration_strategy=es,
@@ -66,7 +90,6 @@ def example(variant):
     if use_gpu:
         algorithm.cuda()
     algorithm.train()
-    env.turn_off_robot()
 
 experiments=[
     'joint_angle|fixed_angle',
@@ -76,28 +99,15 @@ experiments=[
     'end_effector_position_orientation|fixed_ee',
     'end_effector_position_orientation|varying_ee'
 ]
+
 max_path_length = 100
 if __name__ == "__main__":
     try:
         exp_dir = sys.argv[1]
     except:
         exp_dir = None
-
-    if exp_dir == None:
-        run_experiment(
-            example,
-            exp_prefix="ddpg-parallel-sawyer-no-amplification-tuomas-params",
-            seed=3123,
-            mode='here',
-            variant={
-                'version': 'Original',
-                'max_path_length': max_path_length,
-                'use_gpu': True,
-                'es_class': OUStrategy,
-                'env_class': SawyerEnv,
-                'policy_class': FeedForwardPolicy,
-                'normalize_env': False,
-                'env_params': {
+    env_class = SawyerEnv
+    env_params = {
                     'arm_name': 'right',
                     'safety_box': True,
                     'loss': 'huber',
@@ -107,14 +117,53 @@ if __name__ == "__main__":
                     'remove_action': False,
                     'experiment': experiments[2],
                     'reward_magnitude': 10,
-                },
-                'es_params': {
-                    'max_sigma':.25,
-                    'min_sigma':.25,
-                },
+                }
+    env = env_class(**env_params)
+
+    policy_class = FeedForwardPolicy
+    obs_space = convert_gym_space(env.observation_space)
+    action_space = convert_gym_space(env.action_space)
+    policy_params = dict(
+        obs_dim=int(obs_space.flat_dim),
+        action_dim=int(action_space.flat_dim),
+        fc1_size=100,
+        fc2_size=100,
+    )
+    policy = policy_class(**policy_params)
+    es_class = OUStrategy
+    es_params = dict(
+        action_space=action_space,
+        **{
+            'max_sigma':.25,
+            'min_sigma':.25,
+        }
+    )
+    global remote_env
+    remote_env = RemoteRolloutEnv(
+        env_class,
+        env_params,
+        policy_class,
+        policy_params,
+        es_class,
+        es_params,
+        max_path_length,
+        False,
+    )
+
+    for i in range(0, 1):
+        print('SEED ', i)
+        run_experiment(
+            example,
+            exp_prefix="ddpg-parallel-sawyer-fixed-end-effector-REFACTOR-TEST",
+            seed=i,
+            mode='here',
+            variant={
+                'version': 'Original',
+                'max_path_length': max_path_length,
+                'use_gpu': True,
                 'algo_params': dict(
                     batch_size=64,
-                    num_epochs=1000,
+                    num_epochs=30,
                     number_of_gradient_steps=1,
                     num_steps_per_epoch=500,
                     max_path_length=max_path_length,
@@ -123,5 +172,45 @@ if __name__ == "__main__":
             },
             use_gpu=True,
         )
-    else:
-        continue_experiment(exp_dir, resume_torch_algorithm)
+    #  env.turn_off_robot()
+
+def run():
+    run_experiment(
+        example,
+        exp_prefix="ddpg-parallel-sawyer-fixed-end-effector-10-seeds",
+        seed=i,
+        mode='here',
+        variant={
+            'version': 'Original',
+            'max_path_length': max_path_length,
+            'use_gpu': True,
+            'es_class': OUStrategy,
+            'env_class': SawyerEnv,
+            'policy_class': FeedForwardPolicy,
+            'normalize_env': False,
+            'env_params': {
+                'arm_name': 'right',
+                'safety_box': True,
+                'loss': 'huber',
+                'huber_delta': 10,
+                'safety_force_magnitude': 5,
+                'temp': 1,
+                'remove_action': False,
+                'experiment': experiments[2],
+                'reward_magnitude': 10,
+            },
+            'es_params': {
+                'max_sigma': .25,
+                'min_sigma': .25,
+            },
+            'algo_params': dict(
+                batch_size=64,
+                num_epochs=30,
+                number_of_gradient_steps=1,
+                num_steps_per_epoch=500,
+                max_path_length=max_path_length,
+                num_steps_per_eval=500,
+            ),
+        },
+        use_gpu=True,
+    )
