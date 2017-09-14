f2743454bd43c93d1813e0ead926d47520b4a3ac
¨Murtaza <mdalal@berkeley.edu>
2017-09-13 22:37:23 -0700

    updated to work in parallel (NAF)

--- experiments/murtaza/ros/sawyer_naf_torch.py
+++ experiments/murtaza/ros/sawyer_naf_torch.py
@@ -1,57 +1,51 @@
+from railrl.envs.remote import RemoteRolloutEnv
+from railrl.envs.wrappers import convert_gym_space
 from railrl.exploration_strategies.ou_strategy import OUStrategy
 from railrl.launchers.launcher_util import run_experiment
+from railrl.torch.algos.parallel_naf import ParallelNAF
 from railrl.torch.naf import NAF, NafPolicy
 from railrl.torch import pytorch_util as ptu
 from os.path import exists
 import joblib
 from railrl.envs.ros.sawyer_env import SawyerEnv
 from railrl.torch import pytorch_util as ptu
-
+import random
 
 def example(variant):
-    arm_name = variant['arm_name']
-    experiment = variant['experiment']
-    loss = variant['loss']
-    huber_delta = variant['huber_delta']
-    safety_box = variant['safety_box']
-    remove_action = variant['remove_action']
-    safety_force_magnitude = variant['safety_force_magnitude']
-    temp = variant['temp']
-    es_min_sigma = variant['es_min_sigma']
-    es_max_sigma = variant['es_max_sigma']
-    num_epochs = variant['num_epochs']
-    batch_size = variant['batch_size']
+    env_class = variant['env_class']
+    env_params = variant['env_params']
+    env = env_class(**env_params)
+    obs_space = convert_gym_space(env.observation_space)
+    action_space = convert_gym_space(env.action_space)
+    es_class = variant['es_class']
+    es_params = dict(
+        action_space=action_space,
+        **variant['es_params']
+    )
     use_gpu = variant['use_gpu']
-
-    env = SawyerEnv(
-        experiment=experiment,
-        arm_name=arm_name,
-        loss=loss,
-        safety_box=safety_box,
-        remove_action=remove_action,
-        safety_force_magnitude=safety_force_magnitude,
-        temp=temp,
-        huber_delta=huber_delta,
+    es = es_class(**es_params)
+    policy_class = variant['policy_class']
+    policy_params = dict(
+        obs_dim=int(obs_space.flat_dim),
+        action_dim=int(action_space.flat_dim),
+        hidden_size=100,
     )
-    es = OUStrategy(
-        max_sigma=es_max_sigma,
-        min_sigma=es_min_sigma,
-        action_space=env.action_space,
+    naf_policy = NafPolicy(**policy_params)
+    remote_env = RemoteRolloutEnv(
+            env_class,
+            env_params,
+            policy_class,
+            policy_params,
+            es_class,
+            es_params,
+            variant['max_path_length'],
+            variant['normalize_env'],
     )
-    naf_policy = NafPolicy(
-        int(env.observation_space.flat_dim),
-        int(env.action_space.flat_dim),
-        100,
-    )
-    algorithm = NAF(
-        env,
-        naf_policy,
-        es,
-        num_epochs=num_epochs,
-        batch_size=batch_size,
-        max_path_length=100,
-        num_steps_per_epoch=500,
-        num_steps_per_eval=500,
+    algorithm = ParallelNAF(
+        remote_env,
+        naf_policy=naf_policy,
+        exploration_strategy=es,
+        **variant['algo_params'],
     )
     if use_gpu and ptu.gpu_enabled():
         algorithm.cuda()
@@ -65,28 +59,44 @@ experiments=[
     'end_effector_position_orientation|fixed_ee',
     'end_effector_position_orientation|varying_ee'
 ]
-
+max_path_length = 100
 if __name__ == "__main__":
-    run_experiment(
-        example,
-        exp_prefix="NAF-sawyer-fixed-end-effector-task-test-2",
-        seed=0,
-        mode='here',
-        variant={
-            'version': 'Original',
-            'arm_name': 'right',
-            'safety_box': False,
-            'loss': 'huber',
-            'huber_delta': 10,
-            'safety_force_magnitude': 10,
-            'temp': 1,
-            'remove_action': False,
-            'experiment': experiments[2],
-            'es_min_sigma': .25,
-            'es_max_sigma': .25,
-            'num_epochs': 70,
-            'batch_size': 64,
-            'use_gpu': True,
-        },
-        use_gpu=True,
-    )
+    for i in range(10):
+        run_experiment(
+            example,
+            exp_prefix="naf-parallel-sawyer-fixed-end-effector",
+            seed=random.randint(0, 666),
+            mode='here',
+            variant={
+                'version': 'Original',
+                'max_path_length': max_path_length,
+                'use_gpu': True,
+                'es_class': OUStrategy,
+                'env_class': SawyerEnv,
+                'policy_class': NafPolicy,
+                'normalize_env': False,
+                'env_params': {
+                    'arm_name': 'right',
+                    'safety_box': True,
+                    'loss': 'huber',
+                    'huber_delta': 10,
+                    'safety_force_magnitude': 5,
+                    'temp': 1,
+                    'remove_action': False,
+                    'experiment': experiments[2],
+                    'reward_magnitude': 10,
+                },
+                'es_params': {
+                    'max_sigma': .25,
+                    'min_sigma': .25,
+                },
+                'algo_params': dict(
+                    batch_size=64,
+                    num_epochs=30,
+                    num_steps_per_epoch=1000,
+                    max_path_length=max_path_length,
+                    num_steps_per_eval=300,
+                ),
+            },
+            use_gpu=True,
+        )
