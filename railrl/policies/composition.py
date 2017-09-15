143702f1475ca7097e6ee15c7d1d87804d7ab6cc
Vitchyr Pong <vitchyr@gmail.com>
2017-09-14 21:40:08 -0700

    fix typos

--- railrl/policies/composition.py
+++ railrl/policies/composition.py
@@ -21,7 +21,7 @@ class CombinedNafPolicy(SerializablePolicy, Serializable):
         inv = np.linalg.inv(P1 + P2)
         return inv * (P1 @ mu1 + P2 @ mu2), {}
 
-    def log_diagonstics(self, paths):
+    def log_diagnostics(self, paths):
         pass
 
 
@@ -36,5 +36,5 @@ class AveragerPolicy(Policy, Serializable):
         action2, info_dict2 = self.policy2.get_action(obs)
         return (action1 + action2) / 2, dict(info_dict1, **info_dict2)
 
-    def log_diagonstics(self, paths):
+    def log_diagnostics(self, paths):
         pass
