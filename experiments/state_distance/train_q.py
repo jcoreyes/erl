5fb423ed0c691bee96f92c4c396b704989a97262
Vitchyr Pong <vitchyr@gmail.com>
2017-09-19 16:59:26 -0700

    default to dev

--- experiments/state_distance/train_q.py
+++ experiments/state_distance/train_q.py
@@ -113,10 +113,10 @@ if __name__ == '__main__':
     exp_prefix = "dev-train-q"
     run_mode = "none"
 
-    n_seeds = 3
-    mode = "ec2"
-    exp_prefix = "train-oc-exploration-tau-always-zero"
-    run_mode = 'grid'
+    # n_seeds = 3
+    # mode = "ec2"
+    # exp_prefix = "train-oc-exploration-tau-always-zero"
+    # run_mode = 'grid'
 
     version = "Dev"
     num_configurations = 50  # for random mode
