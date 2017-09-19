ef488f89b83377501a4effc9beebbc2f7d9f3572
Vitchyr Pong <vitchyr@gmail.com>
2017-09-19 15:17:51 -0700

    do not use tf and supprot remote rollout env

--- scripts/sim_policy.py
+++ scripts/sim_policy.py
@@ -1,13 +1,54 @@
+from railrl.envs.remote import RemoteRolloutEnv
 from railrl.torch.core import PyTorchModule
 from railrl.torch.pytorch_util import set_gpu_mode
 from rllab.sampler.utils import rollout
 import argparse
 import joblib
 import uuid
-import tensorflow as tf
 from rllab.misc import logger
 
 filename = str(uuid.uuid4())
+
+
+def simulate_policy(args):
+    data = joblib.load(args.file)
+    if 'policy' in data:
+        policy = data['policy']
+    elif 'naf_policy' in data:
+        policy = data['naf_policy']
+    else:
+        qf = data['optimizable_qfunction']
+        policy = qf.implicit_policy
+    env = data['env']
+    if isinstance(env, RemoteRolloutEnv):
+        env = env._wrapped_env
+    print("Policy loaded")
+    if args.gpu:
+        set_gpu_mode(True)
+        policy.cuda()
+    if args.pause:
+        import ipdb; ipdb.set_trace()
+    if isinstance(policy, PyTorchModule):
+        policy.train(False)
+    while True:
+        try:
+            path = rollout(
+                env,
+                policy,
+                max_path_length=args.H,
+                animated=True,
+                speedup=args.speedup,
+                always_return_paths=True,
+            )
+            env.log_diagnostics([path])
+            policy.log_diagnostics([path])
+            logger.dump_tabular()
+        # Hack for now. Not sure why rollout assumes that close is an
+        # keyword argument
+        except TypeError as e:
+            if (str(e) != "render() got an unexpected keyword "
+                          "argument 'close'"):
+                raise e
 
 if __name__ == "__main__":
 
@@ -22,45 +63,9 @@ if __name__ == "__main__":
     parser.add_argument('--pause', action='store_true')
     args = parser.parse_args()
 
-    policy = None
-    env = None
+    simulate_policy(args)
 
-    with tf.Session() as sess:
-        import railrl.core.neuralnet
-        railrl.core.neuralnet.dropout_ph = tf.placeholder(tf.float32, name="dropout_keep_prob")
-        data = joblib.load(args.file)
-        if 'policy' in data:
-            policy = data['policy']
-        elif 'naf_policy' in data:
-            policy = data['naf_policy']
-        else:
-            qf = data['optimizable_qfunction']
-            policy = qf.implicit_policy
-        env = data['env']
-        print("Policy loaded")
-        if args.gpu:
-            set_gpu_mode(True)
-            policy.cuda()
-        if args.pause:
-            import ipdb; ipdb.set_trace()
-        if isinstance(policy, PyTorchModule):
-            policy.train(False)
-        while True:
-            try:
-                path = rollout(
-                    env,
-                    policy,
-                    max_path_length=args.H,
-                    animated=True,
-                    speedup=args.speedup,
-                    always_return_paths=True,
-                )
-                env.log_diagnostics([path])
-                policy.log_diagnostics([path])
-                logger.dump_tabular()
-            # Hack for now. Not sure why rollout assumes that close is an
-            # keyword argument
-            except TypeError as e:
-                if (str(e) != "render() got an unexpected keyword "
-                              "argument 'close'"):
-                    raise e
+    # TODO(vitchyr): maybe add this check back in with a try-except statement
+    # import tensorflow as tf
+    # with tf.Session() as sess:
+    #     simulate_policy(args)
