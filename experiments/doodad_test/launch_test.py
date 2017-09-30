759d7e3d642ea810686a42e3ab6ee64098515fa4
Vitchyr Pong <vitchyr@gmail.com>
2017-09-29 17:39:39 -0700

    make it easy to switch modes

--- experiments/doodad_test/launch_test.py
+++ experiments/doodad_test/launch_test.py
@@ -1,4 +1,5 @@
-from railrl.launchers.launcher_util import run_experiment_doodad
+import argparse
+from railrl.launchers.launcher_util import run_experiment
 
 
 def run_task(variant):
@@ -7,9 +8,13 @@ def run_task(variant):
     logger.log("Hello from script")
     logger.log("variant: " + str(variant))
 
-run_experiment_doodad(
+parser = argparse.ArgumentParser()
+parser.add_argument('--mode', type=str, default='local')
+args = parser.parse_args()
+
+run_experiment(
     run_task,
-    mode='local_docker',
+    mode=args.mode,
     exp_prefix='test-doodad',
     variant=dict(
         test=2
