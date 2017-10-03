89a2fc0f87dd68dab7e100f1199c20361b092f2e
Vitchyr Pong <vitchyr@gmail.com>
2017-10-03 16:16:46 -0700

    launch train-q-reacher2d-sweep-next-state-goal-prob-and-wd

--- experiments/state_distance/train_q.py
+++ experiments/state_distance/train_q.py
@@ -130,7 +130,7 @@ if __name__ == '__main__':
 
     n_seeds = 3
     mode = "ec2"
-    exp_prefix = "train-q-reacher2d-sweep-wd-and-dropout-next-state-goal-prob-2"
+    exp_prefix = "train-q-reacher2d-sweep-next-state-goal-prob-and-wd"
     run_mode = 'grid'
 
     version = "Dev"
@@ -143,7 +143,7 @@ if __name__ == '__main__':
 
     dataset_path = args.replay_path
 
-    max_path_length = 10
+    max_path_length = 300
     # noinspection PyTypeChecker
     variant = dict(
         dataset_path=str(dataset_path),
@@ -234,9 +234,9 @@ if __name__ == '__main__':
             # 'qf_class': [FlatUniversalQfunction, StructuredUniversalQfunction],
             # 'epoch_discount_schedule_params.value': [0, 1, 5],
             # 'algo_params.do_tau_correctly': [True, False],
-            'algo_params.prob_goal_state_is_next_state': [0, 0.5],
-            'qf_params.dropout_prob': [0.5, 0],
-            'algo_params.qf_weight_decay': [1, 1e-1, 1e-2, 1e-3, 1e-4, 0],
+            'algo_params.prob_goal_state_is_next_state': [0, 0.5, 0.99],
+            # 'qf_params.dropout_prob': [0.5, 0],
+            'algo_params.qf_weight_decay': [1e-3, 1e-4, 1e-5, 0],
             # 'algo_params.sample_goals_from': ['environment', 'replay_buffer'],
             # 'algo_params.num_steps_per_epoch': [1, 10],
             # 'algo_params.termination_threshold': [1e-4, 0]
