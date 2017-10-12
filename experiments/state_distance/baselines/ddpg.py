426265610e75ad584f0978dc92343f0c682c82ef
Vitchyr Pong <vitchyr@gmail.com>
2017-10-12 14:10:39 -0700

    launch 10-12-ddpg-reacher-nupo-sweep

--- experiments/state_distance/baselines/ddpg.py
+++ experiments/state_distance/baselines/ddpg.py
@@ -1,36 +1,44 @@
 import random
 
 from railrl.envs.env_utils import gym_env
+from railrl.envs.wrappers import normalize_box
+from railrl.exploration_strategies.base import \
+    PolicyWrappedWithExplorationStrategy
 from railrl.exploration_strategies.ou_strategy import OUStrategy
 from railrl.launchers.launcher_util import run_experiment
 from railrl.policies.torch import FeedForwardPolicy
 from railrl.qfunctions.torch import FeedForwardQFunction
 from railrl.torch.ddpg import DDPG
 
-from rllab.envs.normalized_env import normalize
-
 
 def experiment(variant):
     env = gym_env("Reacher-v1")
-    env = normalize(env)
+    env = normalize_box(
+        env,
+        **variant['normalize_params']
+    )
     es = OUStrategy(action_space=env.action_space)
     qf = FeedForwardQFunction(
         int(env.observation_space.flat_dim),
         int(env.action_space.flat_dim),
-        400,
-        300,
+        100,
+        100,
     )
     policy = FeedForwardPolicy(
         int(env.observation_space.flat_dim),
         int(env.action_space.flat_dim),
-        400,
-        300,
+        100,
+        100,
+    )
+    exploration_policy = PolicyWrappedWithExplorationStrategy(
+        exploration_strategy=es,
+        policy=policy,
     )
     algorithm = DDPG(
         env,
-        exploration_strategy=es,
-        qf=qf,
-        policy=policy,
+        qf,
+        policy,
+        exploration_policy,
         **variant['algo_params']
     )
     algorithm.train()
@@ -38,15 +46,15 @@ def experiment(variant):
 
 if __name__ == "__main__":
     n_seeds = 1
-    mode = "here"
-    exp_prefix = "7-8-dev-state-distance-ddpg-baseline"
+    mode = "local"
+    exp_prefix = "dev-state-distance-ddpg-baseline"
 
-    # n_seeds = 10
-    # mode = "ec2"
-    # exp_prefix = "7-8-state-distance-ddpg-baseline"
+    n_seeds = 3
+    mode = "ec2"
+    exp_prefix = "10-12-ddpg-reacher-nupo-sweep"
 
-    num_steps_per_iteration = 10000
-    H = 1000
+    num_steps_per_iteration = 900
+    H = 300
     num_iterations = 100
     # noinspection PyTypeChecker
     variant = dict(
@@ -61,19 +69,24 @@ if __name__ == "__main__":
             discount=0.99,
             qf_learning_rate=1e-3,
             policy_learning_rate=1e-4,
+            number_of_gradient_steps=1,
         ),
         version="DDPG",
+        normalize_params=dict(
+            obs_mean=None,
+            obs_std=[0.7, 0.7, 0.7, 0.6, 40, 5, 1, 1, 1, 1, 1],
+        ),
     )
-    for _ in range(n_seeds):
-        seed = random.randint(0, 999999)
-        run_experiment(
-            experiment,
-            exp_prefix=exp_prefix,
-            seed=seed,
-            mode=mode,
-            variant=variant,
-            use_gpu=False,
-            sync_s3_pkl=True,
-            sync_s3_log=True,
-            periodic_sync_interval=60*30,
-        )
+    for i, nupo in enumerate([1, 10, 50]):
+        variant['algo_params']['number_of_gradient_steps'] = nupo
+        for _ in range(n_seeds):
+            seed = random.randint(0, 999999)
+            run_experiment(
+                experiment,
+                exp_prefix=exp_prefix,
+                exp_id=i,
+                seed=seed,
+                mode=mode,
+                variant=variant,
+                use_gpu=False,
+            )
