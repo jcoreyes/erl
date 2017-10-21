9823772fd5fce87b8f0149f5880f3ef657486d3d
Vitchyr Pong <vitchyr@gmail.com>
2017-10-20 21:40:02 -0700

    can now eval exploration paths easily

--- railrl/algos/state_distance/state_distance_q_learning.py
+++ railrl/algos/state_distance/state_distance_q_learning.py
@@ -1,5 +1,4 @@
 import math
-import pickle
 import time
 import torch
 from collections import OrderedDict
@@ -15,7 +14,10 @@ from railrl.policies.state_distance import UniversalPolicy
 from railrl.samplers.util import rollout
 from railrl.torch.ddpg import DDPG
 from railrl.torch.algos.util import np_to_pytorch_batch
-from railrl.torch.algos.eval import get_difference_statistics
+from railrl.torch.algos.eval import (
+    get_difference_statistics,
+    get_generic_path_information,
+)
 from railrl.misc.tensorboard_logger import TensorboardLogger
 from railrl.torch.state_distance.exploration import UniversalExplorationPolicy
 from rllab.misc import logger
@@ -193,13 +195,7 @@ class StateDistanceQLearning(DDPG):
     def _sample_discount_for_rollout(self):
         return self._sample_discount(1)[0, 0]
 
-    def _paths_to_np_batch(self, paths):
-        np_batch = super()._paths_to_np_batch(paths)
-        goal_states = [path["goal_states"] for path in paths]
-        np_batch['goal_states'] = np.vstack(goal_states)
-        return np_batch
-
-    def evaluate(self, epoch, _):
+    def evaluate(self, epoch, exploration_paths):
         """
         Perform evaluation for this algorithm.
 
@@ -226,6 +222,11 @@ class StateDistanceQLearning(DDPG):
                 ],
             )
         )
+        statistics.update(get_generic_path_information(
+            exploration_paths, self.discount, stat_prefix="Exploration",
+        ))
+        statistics.update(self._statistics_from_paths(exploration_paths,
+                                                      "Exploration"))
 
         statistics['Discount Factor'] = self.discount
 
@@ -349,6 +350,7 @@ class StateDistanceQLearning(DDPG):
 
     @staticmethod
     def paths_to_batch(paths):
+        # import ipdb; ipdb.set_trace()
         rewards = [path["rewards"].reshape(-1, 1) for path in paths]
         terminals = [path["terminals"].reshape(-1, 1) for path in paths]
         actions = [path["actions"] for path in paths]
@@ -472,17 +474,6 @@ class HorizonFedStateDistanceQLearning(StateDistanceQLearning):
             agent_info,
             env_info,
     ):
-        if not self.cycle_taus_for_rollout:
-            return super()._handle_step(
-                num_paths_total,
-                observation,
-                action,
-                reward,
-                terminal,
-                agent_info=agent_info,
-                env_info=env_info,
-            )
-
         if num_paths_total % self.save_exploration_path_period == 0:
             self._current_path.add_all(
                 observations=self.obs_space.flatten(observation),
@@ -491,6 +482,7 @@ class HorizonFedStateDistanceQLearning(StateDistanceQLearning):
                 actions=self.action_space.flatten(action),
                 agent_infos=agent_info,
                 env_infos=env_info,
+                goal_states=self.goal_state,
                 taus=self._rollout_discount,
             )
 
@@ -503,10 +495,11 @@ class HorizonFedStateDistanceQLearning(StateDistanceQLearning):
             env_info=env_info,
         )
 
-        self._rollout_discount -= 1
-        if self._rollout_discount < 0:
-            self._rollout_discount = self.discount
-        self.exploration_policy.set_discount(self._rollout_discount)
+        if self.cycle_taus_for_rollout:
+            self._rollout_discount -= 1
+            if self._rollout_discount < 0:
+                self._rollout_discount = self.discount
+            self.exploration_policy.set_discount(self._rollout_discount)
 
     def _modify_batch_for_training(self, batch):
         obs = batch['observations']
