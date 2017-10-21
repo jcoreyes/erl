480e12bb5b7880e51de75aef862c65ba75a419d9
Vitchyr Pong <vitchyr@gmail.com>
2017-10-20 22:10:41 -0700

    refactor to have common mlp

---
+++ railrl/networks/base.py
@@ -0,0 +1,42 @@
+from torch import nn as nn
+from torch.nn import functional as F
+
+from railrl.pythonplusplus import identity
+from railrl.torch import pytorch_util as ptu
+from railrl.torch.core import PyTorchModule
+
+
+class Mlp(PyTorchModule):
+    def __init__(
+            self,
+            observation_dim,
+            action_dim,
+            goal_dim,
+            hidden_sizes,
+            output_size,
+            input_size,
+            init_w=3e-3,
+            hidden_activation=F.relu,
+            output_activation=identity,
+            hidden_init=ptu.fanin_init,
+            b_init_value=0.1,
+    ):
+        self.save_init_params(locals())
+        super().__init__()
+
+        self.hidden_activation = hidden_activation
+        self.output_activation = output_activation
+        self.fcs = []
+        in_size = input_size
+
+        for i, next_size in enumerate(hidden_sizes):
+            fc = nn.Linear(in_size, next_size)
+            in_size = next_size
+            hidden_init(fc.weight)
+            fc.bias.data.fill_(b_init_value)
+            self.__setattr__("fc{}".format(i), fc)
+            self.fcs.append(fc)
+
+        self.last_fc = nn.Linear(in_size, output_size)
+        self.last_fc.weight.data.uniform_(-init_w, init_w)
+        self.last_fc.bias.data.uniform_(-init_w, init_w)
