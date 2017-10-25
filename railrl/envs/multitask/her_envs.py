b1eebead74bd8bbe550b77db7f4dfbb6b03d4191
Vitchyr Pong <vitchyr@gmail.com>
2017-10-25 15:54:19 -0700

    added her tasks for Asvin

---
+++ railrl/envs/multitask/her_envs.py
@@ -0,0 +1,71 @@
+import numpy as np
+from gym import utils
+from gym.envs.mujoco import mujoco_env
+
+from railrl.envs.env_utils import get_asset_xml
+
+
+def obs_to_goal(obs):
+    return obs[14:17]
+
+
+def get_sparse_reward(obs):
+    """-1 if far, 0 if close"""
+    hand_pos = obs[14:17]
+    goal_pos = obs[17:20]
+    r = np.linalg.norm(hand_pos - goal_pos) < 0.1
+    return (r - 1).astype(float)
+
+
+class Reacher7Dof(
+    mujoco_env.MujocoEnv, utils.EzPickle
+
+):
+    def __init__(self):
+        utils.EzPickle.__init__(self)
+        mujoco_env.MujocoEnv.__init__(
+            self,
+            get_asset_xml('reacher_7dof.xml'),  # path to xml
+            5,  # frameskip
+        )
+        self._desired_xyz = np.zeros(3)
+        self.obs_to_goal = obs_to_goal
+        self.goal_idx = slice(17, 20)
+
+    def get_reward(self, obs):
+        return get_sparse_reward(obs)
+
+    def viewer_setup(self):
+        self.viewer.cam.trackbodyid = -1
+        self.viewer.cam.distance = 4.0
+
+    def reset_model(self):
+        qpos = self.init_qpos
+        qvel = self.init_qvel + self.np_random.uniform(low=-0.005,
+                                                       high=0.005, size=self.model.nv)
+        self._desired_xyz = np.random.uniform(
+            np.array([-0.75, -1.25, -0.24]),
+            np.array([0.75, 0.25, 0.6]),
+        )
+        qpos[-7:-4] = self._desired_xyz
+        qvel[-7:] = 0
+        self.set_state(qpos, qvel)
+        return self._get_obs()
+
+    def _step(self, a):
+        distance = np.linalg.norm(
+            self.get_body_com("tips_arm") - self.get_body_com("goal")
+        )
+        reward = - distance
+        self.do_simulation(a, self.frame_skip)
+        ob = self._get_obs()
+        done = False
+        return ob, reward, done, dict(distance=distance)
+
+    def _get_obs(self):
+        return np.concatenate([
+            self.model.data.qpos.flat[:7],
+            self.model.data.qvel.flat[:7],
+            self.get_body_com("tips_arm"),
+            self.get_body_com("goal"),
+        ])
---
+++ railrl/envs/multitask/her_pusher_env.py
@@ -0,0 +1,107 @@
+import numpy as np
+from gym import utils
+from gym.envs.mujoco import mujoco_env
+
+from railrl.envs.env_utils import get_asset_xml
+
+
+def obs_to_goal(obs):
+    return obs[6:8]
+
+
+def get_sparse_reward(obs):
+    """-1 if far, 0 if close"""
+    cylinder_pos = obs[8:10]
+    goal_pos = obs[10:12]
+    r = np.linalg.norm(cylinder_pos - goal_pos) < 0.1
+    return (r - 1).astype(float)
+
+
+class Pusher2DEnv(
+    mujoco_env.MujocoEnv, utils.EzPickle
+
+):
+    def __init__(self):
+        self._target_cylinder_position = np.zeros(2)
+        utils.EzPickle.__init__(self)
+        mujoco_env.MujocoEnv.__init__(
+            self,
+            get_asset_xml('3link_gripper_push_2d.xml'),  # path to xml
+            5,  # frameskip
+        )
+        self.obs_to_goal = obs_to_goal
+        self.goal_idx = slice(17, 20)
+
+    def get_reward(self, obs):
+        return get_sparse_reward(obs)
+
+    def viewer_setup(self):
+        self.viewer.cam.trackbodyid = 0
+        self.viewer.cam.distance = 4.0
+        rotation_angle = 90
+        cam_dist = 4
+        cam_pos = np.array([0, 0, 0, cam_dist, -45, rotation_angle])
+        for i in range(3):
+            self.viewer.cam.lookat[i] = cam_pos[i]
+        self.viewer.cam.distance = cam_pos[3]
+        self.viewer.cam.elevation = cam_pos[4]
+        self.viewer.cam.azimuth = cam_pos[5]
+        self.viewer.cam.trackbodyid = -1
+
+    def reset_model(self):
+        qpos = (
+            np.random.uniform(low=-0.1, high=0.1, size=self.model.nq)
+            + self.init_qpos.squeeze()
+        )
+        qpos[-3:] = self.init_qpos.squeeze()[-3:]
+        # Object position
+        obj_pos = np.random.uniform(
+            #         x      y
+            np.array([0.3, -0.8]),
+            np.array([0.8, -0.3]),
+        )
+        qpos[-6:-4] = obj_pos
+        self._target_cylinder_position = np.random.uniform(
+            np.array([-1, -1]),
+            np.array([1, 0]),
+            2
+        )
+        qpos[-4:-2] = self._target_cylinder_position
+        qpos[-2:] = np.zeros(2)   # ignore for now
+        qvel = self.init_qvel.copy().squeeze()
+        qvel[:] = 0
+
+        self.set_state(qpos, qvel)
+
+        return self._get_obs()
+
+    def _step(self, a):
+        hand_to_object_distance = np.linalg.norm(
+            self.model.data.site_xpos[0][:2] - self.get_body_com("object")[:2]
+        )
+        object_to_goal_distance = np.linalg.norm(
+            self.get_body_com("goal") - self.get_body_com("object")
+        )
+        hand_to_hand_goal_distance = np.linalg.norm(
+            self.model.data.site_xpos[0][:2] - self.get_body_com("hand_goal")[:2]
+        )
+        reward = - hand_to_object_distance - object_to_goal_distance
+
+        self.do_simulation(a, self.frame_skip)
+        ob = self._get_obs()
+        done = False
+        return ob, reward, done, dict(
+            hand_to_hand_goal_distance=hand_to_hand_goal_distance,
+            hand_to_object_distance=hand_to_object_distance,
+            object_to_goal_distance=object_to_goal_distance,
+        )
+
+    def _get_obs(self):
+        return np.concatenate([
+            self.model.data.qpos.flat[:3],
+            self.model.data.qvel.flat[:3],
+            self.model.data.site_xpos[0][:2],
+            self.get_body_com("object")[:2],
+            self._target_cylinder_position,
+        ])
+
