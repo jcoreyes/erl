551a7ad7d63d5bc54fe34a8e494232b53cbdc77e
Vitchyr Pong <vitchyr@gmail.com>
2017-10-27 07:28:25 -0700

    regen plots

--- experiments/state_distance/iclr2018/tau_sweep.py
+++ experiments/state_distance/iclr2018/tau_sweep.py
@@ -2,14 +2,21 @@ from railrl.misc.data_processing import Experiment
 import matplotlib.pyplot as plt
 import numpy as np
 
-path = "/home/vitchyr/git/rllab-rail/railrl/data/doodads3/10-25-get-results-take1/"
+# path = "/home/vitchyr/git/rllab-rail/railrl/data/doodads3/10-25-get-results-take1/"
+path = "/home/vitchyr/git/rllab-rail/railrl/data/doodads3/10-27-get-results-handxyxy-best-hp-no-oc-sampling-nspe1000/"
 exp = Experiment(path)
+# base_criteria = {
+#     'algo_params.goal_dim_weights': [1] * 17,
+#     'env_class.$class': "railrl.envs.multitask.reacher_7dof.Reacher7DofGoalStateEverything"
+# }
 base_criteria = {
-    'algo_params.goal_dim_weights': [1] * 17,
-    'env_class.$class': "railrl.envs.multitask.reacher_7dof.Reacher7DofGoalStateEverything"
+    'algo_params.num_updates_per_env_step': 5,
+    # 'algo_params.goal_dim_weights': [1] * 17,
+    # 'env_class.$class': "railrl.envs.multitask.reacher_7dof.Reacher7DofGoalStateEverything"
 }
 tau_to_criteria = {}
-taus = [1, 5, 10, 25]
+# taus = [1, 5, 10, 25]
+taus = [1, 5, 15, 50]
 for tau in taus:
     criteria = base_criteria.copy()
     criteria['epoch_discount_schedule_params.value'] = tau
@@ -20,7 +27,8 @@ tau_to_trials = {}
 for tau in taus:
     tau_to_trials[tau] = exp.get_trials(tau_to_criteria[tau])
 
-key = 'Final_Euclidean_distance_to_goal_Mean'
+# key = 'Final_Euclidean_distance_to_goal_Mean'
+key = 'test_Final_Euclidean_distance_to_goal_Mean'
 for tau, trials in tau_to_trials.items():
     all_values = []
     min_len = np.inf
