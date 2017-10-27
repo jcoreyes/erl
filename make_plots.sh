1545ffb90e56d1ee71c67f7e9c75648d4ad4b38e
Vitchyr Pong <vitchyr@gmail.com>
2017-10-26 22:38:56 -0700

    add new results

---
+++ make_plots.sh
@@ -0,0 +1,7 @@
+python /home/vitchyr/git/rllab-rail/railrl/experiments/state_distance/iclr2018/pusher_results.py
+python /home/vitchyr/git/rllab-rail/railrl/experiments/state_distance/iclr2018/reacher_results.py
+python /home/vitchyr/git/rllab-rail/railrl/experiments/state_distance/iclr2018/sawyer_results.py
+python /home/vitchyr/git/rllab-rail/railrl/experiments/state_distance/iclr2018/tau_sweep.py
+python /home/vitchyr/git/rllab-rail/railrl/experiments/state_distance/iclr2018/vectorized_vs_not_vectorized.py
+
+
