54b35ead21cb0f65adee9e524ab4799ca746a5c4
Vitchyr Pong <vitchyr@gmail.com>
2017-11-17 13:42:54 -0800

    simplify eval code and update target network

--- railrl/torch/dqn.py
+++ railrl/torch/dqn.py
@@ -1,9 +1,12 @@
 from collections import OrderedDict
+import numpy as np
 
 import torch
 import torch.optim as optim
 from torch import nn as nn
 
+import railrl.torch.pytorch_util as ptu
+from railrl.misc.data_processing import create_stats_ordered_dict
 from railrl.torch.algos.eval import get_statistics_from_pytorch_dict, \
     get_generic_path_information
 from railrl.torch.torch_rl_algorithm import TorchRLAlgorithm
@@ -17,12 +20,15 @@ class DQN(TorchRLAlgorithm):
             exploration_policy,
             qf,
             learning_rate=1e-3,
+            tau=0.001,
             **kwargs
     ):
         super().__init__(env, exploration_policy, **kwargs)
         self.qf = qf
         self.target_qf = self.qf.copy()
+        # self.target_qf = self.qf
         self.learning_rate = learning_rate
+        self.tau = tau
         self.qf_optimizer = optim.Adam(
             self.qf.parameters(),
             lr=self.learning_rate,
@@ -57,29 +63,22 @@ class DQN(TorchRLAlgorithm):
         qf_loss = self.qf_criterion(y_pred, y_target)
 
         """
-        Take the gradient step
+        Update networks
         """
         self.qf_optimizer.zero_grad()
         qf_loss.backward()
         self.qf_optimizer.step()
+        ptu.soft_update_from_to(self.target_qf, self.qf, self.tau)
 
         """
         Save some statistics for eval
         """
-        stats = {
-            'QF Loss': qf_loss,
-            'Y Predictions': qf_loss,
-        }
-        self.eval_statistics = get_statistics_from_pytorch_dict(
-            stats,
-            [
-                'QF Loss',
-            ],
-            [
-                'Y Predictions',
-            ],
-            '',
-        )
+        self.eval_statistics = OrderedDict()
+        self.eval_statistics['QF Loss'] = np.mean(ptu.get_numpy(qf_loss))
+        self.eval_statistics.update(create_stats_ordered_dict(
+            'Y Predictions',
+            ptu.get_numpy(y_pred),
+        ))
 
     def cuda(self):
         self.qf.cuda()
