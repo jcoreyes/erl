d95f0f1f5adde1b25b49e48ce6cfe95d12f09ad3
mdalal2020 <mdalal@berkeley.edu>
2017-11-19 12:36:59 -0800

    added parallel tester

---
+++ experiments/murtaza/ddpg_parallel_tester.py
@@ -0,0 +1,111 @@
+import random
+
+from railrl.envs.wrappers import convert_gym_space
+from railrl.exploration_strategies.base import PolicyWrappedWithExplorationStrategy
+from railrl.launchers.launcher_util import resume_torch_algorithm, continue_experiment, run_experiment
+from railrl.policies.torch import FeedForwardPolicy
+from railrl.qfunctions.torch import FeedForwardQFunction
+from railrl.torch.ddpg import DDPG
+from rllab.envs.mujoco.half_cheetah_env import HalfCheetahEnv
+from railrl.exploration_strategies.ou_strategy import OUStrategy
+from railrl.torch import pytorch_util as ptu
+import sys
+
+from rllab.envs.normalized_env import normalize
+
+
+def example(variant):
+    env_class = variant['env_class']
+    env_params = variant['env_params']
+    env = env_class(**env_params)
+    normalize(env)
+    obs_space = convert_gym_space(env.observation_space)
+    action_space = convert_gym_space(env.action_space)
+    es_class = variant['es_class']
+    es_params = dict(
+        action_space=action_space,
+        **variant['es_params']
+    )
+    use_gpu = variant['use_gpu']
+    es = es_class(**es_params)
+    qf = FeedForwardQFunction(
+        int(env.observation_space.flat_dim),
+        int(env.action_space.flat_dim),
+        100,
+        100,
+    )
+    policy_class = variant['policy_class']
+    policy_params = dict(
+        obs_dim=int(obs_space.flat_dim),
+        action_dim=int(action_space.flat_dim),
+        fc1_size=100,
+        fc2_size=100,
+    )
+    policy = policy_class(**policy_params)
+    exploration_policy = PolicyWrappedWithExplorationStrategy(
+        exploration_strategy=es,
+        policy=policy,
+    )
+    algorithm = DDPG(
+        env,
+        qf=qf,
+        policy=policy,
+        exploration_policy=exploration_policy,
+        **variant['algo_params'],
+    )
+    if use_gpu and ptu.gpu_enabled():
+        algorithm.cuda()
+    algorithm.train()
+
+experiments=[
+    'joint_angle|fixed_angle',
+    'joint_angle|varying_angle',
+    'end_effector_position|fixed_ee',
+    'end_effector_position|varying_ee',
+    'end_effector_position_orientation|fixed_ee',
+    'end_effector_position_orientation|varying_ee'
+]
+
+max_path_length = 100
+if __name__ == "__main__":
+    try:
+        exp_dir = sys.argv[1]
+    except:
+        exp_dir = None
+    if exp_dir:
+        continue_experiment(exp_dir, resume_function=resume_torch_algorithm)
+    else:
+        run_experiment(
+            example,
+            exp_prefix="ddpg-cheetah-parallel-TEST",
+            seed=random.randint(0, 666),
+            mode='local',
+            variant={
+                'version': 'Original',
+                'max_path_length': max_path_length,
+                'use_gpu': True,
+                'es_class': OUStrategy,
+                'env_class': HalfCheetahEnv,
+                'policy_class': FeedForwardPolicy,
+                'normalize_env': False,
+                'env_params': dict(),
+                'es_params':{
+                    'max_sigma': .25,
+                    'min_sigma': .25,
+                },
+                'algo_params':dict(
+                    num_epochs=100,
+                    num_steps_per_epoch=10000,
+                    num_steps_per_eval=1000,
+                    use_soft_update=True,
+                    tau=1e-2,
+                    batch_size=128,
+                    max_path_length=1000,
+                    discount=0.99,
+                    qf_learning_rate=1e-3,
+                    policy_learning_rate=1e-4,
+                    # collection_mode='parallel',
+                ),
+            },
+            use_gpu=True,
+        )
