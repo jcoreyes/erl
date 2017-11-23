5c391abdd18defb62ece80182741da9fc4dc0a18
Vitchyr Pong <vitchyr@gmail.com>
2017-11-23 13:19:27 -0800

    launch comparable experiment to tuomas's code

--- experiments/sac/cheetah.py
+++ experiments/sac/cheetah.py
@@ -4,17 +4,27 @@ Run PyTorch Soft Actor Critic on HalfCheetahEnv.
 import random
 
 import numpy as np
+import gym
 
 import railrl.torch.pytorch_util as ptu
+from railrl.envs.wrappers import normalize_box
 from railrl.launchers.launcher_util import run_experiment
 from railrl.sac.policies import TanhGaussianPolicy
 from railrl.sac.sac import SoftActorCritic
 from railrl.torch.networks import FlattenMlp
 from rllab.envs.mujoco.half_cheetah_env import HalfCheetahEnv
+from rllab.envs.normalized_env import normalize
+from rllab.envs.gym_env import GymEnv
 
 
 def experiment(variant):
-    env = HalfCheetahEnv()
+    # env = normalize(GymEnv(
+    #     'HalfCheetah-v1',
+    #     force_reset=True,
+    #     record_video=False,
+    #     record_log=False,
+    # ))
+    env = normalize_box(gym.make('HalfCheetah-v1'))
 
     obs_dim = int(np.prod(env.observation_space.shape))
     action_dim = int(np.prod(env.action_space.shape))
@@ -50,13 +60,17 @@ if __name__ == "__main__":
     # noinspection PyTypeChecker
     variant = dict(
         algo_params=dict(
-            num_epochs=100,
-            num_steps_per_epoch=100,
-            num_steps_per_eval=100,
+            num_epochs=1000,
+            num_steps_per_epoch=1000,
+            num_steps_per_eval=1000,
             batch_size=32,
-            max_path_length=10,
+            max_path_length=1000,
             discount=0.99,
-            soft_target_tau=0.001,
+
+            soft_target_tau=0.01,
+            policy_lr=3E-4,
+            qf_lr=3E-4,
+            vf_lr=3E-4,
         ),
     )
     for _ in range(1):
