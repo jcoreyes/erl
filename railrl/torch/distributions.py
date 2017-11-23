c3c0d884ce974a72453d556fa1c4a34b02fc38d5
Vitchyr Pong <vitchyr@gmail.com>
2017-11-22 16:50:34 -0800

    implement and test tanh normal

---
+++ railrl/torch/distributions.py
@@ -0,0 +1,50 @@
+"""
+Add custom distributions in addition to th existing ones
+"""
+import torch
+from torch.distributions import Distribution, Normal
+
+
+class TanhNormal(Distribution):
+    """
+    Represent distribution of X where
+        X ~ tanh(Z)
+        Z ~ N(mean, std)
+
+    Note: this is not very numerically stable.
+    """
+    def __init__(self, normal_mean, normal_std):
+        """
+        :param normal_mean: Mean of the normal distribution
+        :param normal_std: Std of the normal distribution
+        """
+        self.normal = Normal(normal_mean, normal_std)
+
+    def sample_n(self, n, return_pre_tanh_value=False):
+        z = self.normal.sample_n(n)
+        if return_pre_tanh_value:
+            return torch.tanh(z), z
+        else:
+            return torch.tanh(z)
+
+    def log_prob(self, value, pre_tanh_value=None):
+        """
+
+        :param value: some value, x
+        :param pre_tanh_value: arctanh(x)
+        :return:
+        """
+        if pre_tanh_value is None:
+            pre_tanh_value = torch.log(
+                (1+value) / (1-value)
+            ) / 2
+        return self.normal.log_prob(pre_tanh_value) - torch.log(
+            1 - value * value
+        )
+
+    def sample(self, return_pretanh_value=False):
+        z = self.normal.sample()
+        if return_pretanh_value:
+            return torch.tanh(z), z
+        else:
+            return torch.tanh(z)
