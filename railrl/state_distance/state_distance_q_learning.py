f37ee849ca1d4ac253d30a4c2759742798ba7751
Vitchyr Pong <vitchyr@gmail.com>
2017-11-24 22:18:18 -0800

    fix import

--- railrl/state_distance/state_distance_q_learning.py
+++ railrl/state_distance/state_distance_q_learning.py
@@ -16,7 +16,7 @@ from railrl.samplers.util import rollout, split_paths_to_dict
 from railrl.state_distance.exploration import UniversalExplorationPolicy
 from railrl.state_distance.networks import DuelingStructuredUniversalQfunction
 from railrl.torch.algos.util import np_to_pytorch_batch
-from railrl.torch.ddpg import DDPG
+from railrl.torch.algos.ddpg import DDPG
 from railrl.torch.eval_util import (
     get_difference_statistics,
     get_generic_path_information,
