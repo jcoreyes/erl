353f49e275429fea5b4eef91aaa86959a0e73faa
Vitchyr Pong <vitchyr@gmail.com>
2017-11-26 10:01:35 -0800

    fix min_idx bug and add todo

--- railrl/data_management/her_replay_buffer.py
+++ railrl/data_management/her_replay_buffer.py
@@ -5,7 +5,7 @@ from railrl.data_management.env_replay_buffer import EnvReplayBuffer
 
 class HerReplayBuffer(EnvReplayBuffer):
     """
-    Save goal states from the same trajectory into the replay buffer
+    Save goals from the same trajectory into the replay buffer
 
     Implementation details:
      - Ignore any trajectory that wraps around the buffer, so the final index
@@ -59,11 +59,13 @@ class HerReplayBuffer(EnvReplayBuffer):
         self._wrapped_buffer = False
         self._min_valid_idx = 0
         self._max_valid_idx = 0
+        # TODO(vitchyr): make this trajectory centric so that wrapping isn't
+        # an issue
 
     def add_sample(self, observation, action, reward, terminal,
-                    next_observation, goal_state=None, **kwargs):
-        assert goal_state is not None
-        self._goals[self._top] = goal_state
+                    next_observation, goal=None, **kwargs):
+        assert goal is not None
+        self._goals[self._top] = goal
         super().add_sample(observation, action, reward, terminal, next_observation)
 
     def _advance(self):
@@ -89,6 +91,8 @@ class HerReplayBuffer(EnvReplayBuffer):
         if not self._wrapped_buffer:
             self._max_valid_idx = max(self._last_top_index, self._max_valid_idx)
             self._add_goals_to_last_trajectory()
+        else:
+            self._min_valid_idx = self._top
         self._current_episode_start_index = self._top
         self._wrapped_buffer = False
 
@@ -101,14 +105,14 @@ class HerReplayBuffer(EnvReplayBuffer):
         for i in range(self._current_episode_start_index,
                        self._last_top_index + 1):
             self._index_to_goals_interval[i] = (i+1, self._last_top_index+1)
-            potential_goal_state_indices = list(range(i+1, self._last_top_index+1))
-            if len(potential_goal_state_indices) <= self.num_goals_to_sample:
+            potential_goal_indices = list(range(i+1, self._last_top_index+1))
+            if len(potential_goal_indices) <= self.num_goals_to_sample:
                 self._index_to_sampled_goals_idxs[i] = (
-                    potential_goal_state_indices
+                    potential_goal_indices
                 )
             else:
                 self._index_to_sampled_goals_idxs[i] = np.random.choice(
-                    potential_goal_state_indices,
+                    potential_goal_indices,
                     size=self.num_goals_to_sample,
                     replace=False,
                 )
@@ -123,19 +127,19 @@ class HerReplayBuffer(EnvReplayBuffer):
         )
         next_indices = (indices + 1) % self._size
         if self.goal_sample_strategy == 'store':
-            goal_state_indices = [
+            goal_indices = [
                 # This is generally faster than random.choice.
                 # random.choice is only fast if you give it a np array.
                 self._index_to_sampled_goals_idxs[np.random.randint(0, i)]
                 for i in indices
             ]
         else:
-            goal_state_indices = [
+            goal_indices = [
                 np.random.randint(*self._index_to_goals_interval[i])
                 for i in indices
             ]
         goals = self._env.convert_obs_to_goals(
-            self._observations[goal_state_indices]
+            self._observations[goal_indices]
         )
         num_goals_are_from_rollout = int(
             batch_size * self.fraction_goals_are_rollout_goals
@@ -154,13 +158,11 @@ class HerReplayBuffer(EnvReplayBuffer):
         )
 
     def random_batch_for_sl(self, batch_size, max_tau):
-        indices = np.random.choice(
-            self._valid_transition_indices,
-            batch_size,
-            replace=False
+        indices = np.random.randint(
+            self._min_valid_idx, self._max_valid_idx, batch_size
         )
         next_indices = (indices + 1) % self._size
-        goal_state_indices = []
+        goal_indices = []
         """
         This +1 -1 business is because tau is defined as "the number of time
         steps you have left AFTER taking the current action."
@@ -175,17 +177,15 @@ class HerReplayBuffer(EnvReplayBuffer):
             # +1 for funny tau stuff. See above
             max_i = min(max_i, i + max_tau + 1)
             # This +1 is because randint exclused the last element
-            goal_state_indices.append(np.random.randint(min_i, max_i+1))
-        goals = self._env.convert_obs_to_goals(
-            self._observations[goal_state_indices]
-        )
-        taus = np.array(goal_state_indices) - np.array(indices) - 1
+            goal_indices.append(np.random.randint(min_i, max_i+1))
+        obs_after_tau_steps = self._observations[goal_indices]
+        taus = np.array(goal_indices) - np.array(indices) - 1
         return dict(
             observations=self._observations[indices],
             actions=self._actions[indices],
             rewards=self._rewards[indices],
             terminals=self._terminals[indices],
             next_observations=self._observations[next_indices],
-            states_after_tau_steps=goals,
+            obs_after_tau_steps=obs_after_tau_steps,
             taus=np.expand_dims(taus, 1),
         )
