c621889ebacb7b74b4277c6f321c80dbe2fd70bc
Vitchyr Pong <vitchyr@gmail.com>
2017-11-28 11:59:11 -0800

    test tdm + dqn cartpole without vectorizaton

--- experiments/state_distance/discrete/vectorized_classic_control.py
+++ experiments/state_distance/discrete/vectorized_classic_control.py
@@ -8,6 +8,7 @@ from railrl.data_management.her_replay_buffer import HerReplayBuffer
 from railrl.envs.multitask.cartpole_env import CartPole, CartPoleAngleOnly
 from railrl.envs.multitask.mountain_car_env import MountainCar
 from railrl.launchers.launcher_util import run_experiment
+from railrl.policies.argmax import ArgmaxDiscretePolicy
 from railrl.state_distance.tdm_dqn import TdmDqn
 from railrl.state_distance.discrete_action_networks import \
     VectorizedDiscreteQFunction, ArgmaxDiscreteTdmPolicy
@@ -17,16 +18,24 @@ from railrl.torch.networks import FlattenMlp
 def experiment(variant):
     env = variant['env_class']()
 
-    qf = VectorizedDiscreteQFunction(
-        observation_dim=int(np.prod(env.observation_space.low.shape)),
-        action_dim=env.action_space.n,
-        goal_dim=env.goal_dim,
-        **variant['qf_params']
-    )
-    policy = ArgmaxDiscreteTdmPolicy(
-        qf,
-        **variant['policy_params']
-    )
+    if variant['algo_params']['tdm_kwargs']['vectorized']:
+        qf = VectorizedDiscreteQFunction(
+            observation_dim=int(np.prod(env.observation_space.low.shape)),
+            action_dim=env.action_space.n,
+            goal_dim=env.goal_dim,
+            **variant['qf_params']
+        )
+        policy = ArgmaxDiscreteTdmPolicy(
+            qf,
+            **variant['policy_params']
+        )
+    else:
+        qf = FlattenMlp(
+            hidden_sizes=[32, 32],
+            input_size=int(np.prod(env.observation_space.shape)) + env.goal_dim + 1,
+            output_size=env.action_space.n,
+        )
+        policy = ArgmaxDiscretePolicy(qf)
     replay_buffer = HerReplayBuffer(
         env=env,
         **variant['her_replay_buffer_params']
@@ -57,7 +66,7 @@ if __name__ == "__main__":
                 discount=0.99,
             ),
             tdm_kwargs=dict(
-                vectorized=True,
+                vectorized=False,
             ),
             dqn_kwargs=dict(
                 epsilon=0.2,
@@ -102,15 +111,13 @@ if __name__ == "__main__":
             seed = random.randint(0, 10000)
             run_experiment(
                 experiment,
-                # exp_prefix="dev-vectorized-discrete-tdm-classic-control",
-                # exp_prefix="vectorized-discrete-tdm-cartpole-weight-angle-only",
-                # exp_prefix="vectorized-discrete-tdm-cartpole-no-hack",
-                exp_prefix="tdm-dqn-cartpole-goal-weight-sweep",
                 seed=seed,
                 variant=variant,
                 exp_id=exp_id,
+                exp_prefix="tdm-dqn-cartpole-goal-weight-sweep",
                 mode='ec2',
                 use_gpu=False,
+                # exp_prefix="dev-vectorized-discrete-tdm-classic-control",
                 # mode='local',
                 # use_gpu=True,
             )
