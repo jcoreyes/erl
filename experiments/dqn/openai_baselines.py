23388db93c754f8533c2e28594ec237915d860a1
Vitchyr Pong <vitchyr@gmail.com>
2017-11-29 14:29:34 -0800

    launch openai-baselines-dqn-cartpole-sweep

--- experiments/dqn/openai_baselines.py
+++ experiments/dqn/openai_baselines.py
@@ -1,7 +1,8 @@
 from railrl.launchers.launcher_util import run_experiment
+import railrl.misc.hyperparameter as hyp
 
 
-def experiment(_):
+def experiment(variant):
     import gym
     from baselines import deepq
     from rllab.misc import logger
@@ -17,14 +18,9 @@ def experiment(_):
     act = deepq.learn(
         env,
         q_func=model,
-        lr=1e-3,
-        max_timesteps=1000000,
-        buffer_size=50000,
-        exploration_fraction=0.1,
-        exploration_final_eps=0.02,
-        print_freq=1000,
         callback=callback,
         logger=logger,
+        **variant['algo_kwargs']
     )
     print("Saving model to cartpole_model.pkl")
     act.save("cartpole_model.pkl")
@@ -34,10 +30,34 @@ def experiment(_):
 
 
 def main():
-    run_experiment(
-        experiment,
-        exp_prefix="openai-baselines-dqn-cartpole"
+    variant = dict(
+        algo_kwargs=dict(
+            lr=1e-3,
+            max_timesteps=1000000,
+            buffer_size=50000,
+            exploration_fraction=0.1,
+            exploration_final_eps=0.02,
+            print_freq=1000,
+        ),
     )
+    search_space = {
+        'algo_kwargs.prioritized_replay': [True, False],
+        'algo_kwargs.gamma': [1, 0.99],
+    }
+    sweeper = hyp.DeterministicHyperparameterSweeper(
+        search_space, default_parameters=variant,
+    )
+    for exp_id, variant in enumerate(sweeper.iterate_hyperparameters()):
+        for _ in range(3):
+            run_experiment(
+                experiment,
+                exp_id=exp_id,
+                variant=variant,
+                exp_prefix="openai-baselines-dqn-cartpole-sweep",
+                mode='ec2',
+                # exp_prefix="dev-openai-baselines-dqn-cartpole",
+                # mode='local',
+            )
 
 
 if __name__ == '__main__':
