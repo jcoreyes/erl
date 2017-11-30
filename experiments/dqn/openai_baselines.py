f7fb056725a3580b84d0e1a1697d13581413b18d
Vitchyr Pong <vitchyr@gmail.com>
2017-11-29 19:40:03 -0800

    launch openai-baselines-dqn-cartpole-sweep-2

--- experiments/dqn/openai_baselines.py
+++ experiments/dqn/openai_baselines.py
@@ -8,10 +8,8 @@ def experiment(variant):
     from rllab.misc import logger
 
     def callback(lcl, glb):
-        # stop training if reward exceeds 199
-        is_solved = lcl['t'] > 100 and sum(
-            lcl['episode_rewards'][-101:-1]) / 100 >= 199
-        return is_solved
+        # only stop training after number of time steps have been reached
+        return False
 
     env = gym.make("CartPole-v0")
     model = deepq.models.mlp([64])
@@ -24,38 +22,41 @@ def experiment(variant):
     )
     print("Saving model to cartpole_model.pkl")
     act.save("cartpole_model.pkl")
-    logger.save_itr_params(100, dict(
-        action_function=act
-    ))
+    # logger.save_itr_params(100, dict(
+    #     action_function=act
+    # ))
 
 
 def main():
     variant = dict(
         algo_kwargs=dict(
             lr=1e-3,
-            max_timesteps=1000000,
-            buffer_size=50000,
+            max_timesteps=int(1E7),
+            buffer_size=int(5E5),
             exploration_fraction=0.1,
             exploration_final_eps=0.02,
             print_freq=1000,
+            gamma=0.99,
         ),
     )
     search_space = {
         'algo_kwargs.prioritized_replay': [True, False],
-        'algo_kwargs.gamma': [1, 0.99],
+        'algo_kwargs.lr': [1e-2, 1e-3, 1e-4],
+        'algo_kwargs.exploration_fraction': [0.1, 0.5],
+        'algo_kwargs.exploration_final_eps': [0.2, 0.02],
     }
     sweeper = hyp.DeterministicHyperparameterSweeper(
         search_space, default_parameters=variant,
     )
     for exp_id, variant in enumerate(sweeper.iterate_hyperparameters()):
-        for _ in range(3):
+        for _ in range(1):
             run_experiment(
                 experiment,
                 exp_id=exp_id,
                 variant=variant,
-                exp_prefix="openai-baselines-dqn-cartpole-sweep",
+                exp_prefix="openai-baselines-dqn-cartpole-sweep-2",
                 mode='ec2',
-                # exp_prefix="dev-openai-baselines-dqn-cartpole",
+                # exp_prefix="dev-openai-baselines-dqn-cartpole-2",
                 # mode='local',
             )
 
