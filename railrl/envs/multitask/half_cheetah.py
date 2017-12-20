e7f8f0d7f7e8eb5f0b3abb83217b8e0216d297b0
Vitchyr Pong <vitchyr@gmail.com>
2017-12-19 16:34:28 -0800

    clean up code and add call to super log diagnostics

--- railrl/envs/multitask/half_cheetah.py
+++ railrl/envs/multitask/half_cheetah.py
@@ -20,26 +20,12 @@ class GoalXVelHalfCheetah(HalfCheetahEnv, MultitaskEnv):
         MultitaskEnv.__init__(self)
         self.set_goal(np.array([5]))
 
-    def sample_actions(self, batch_size):
-        return np.random.uniform(-0.5, -0.5, (batch_size, 6))
-
     @property
     def goal_dim(self) -> int:
         return 1
 
     def sample_goals(self, batch_size):
         return np.random.uniform(-MAX_SPEED, MAX_SPEED, (batch_size, 1))
-
-    def sample_irrelevant_goal_dimensions(self, goal, batch_size):
-        raise NotImplementedError()
-
-    def sample_dimensions_irrelevant_to_oc(self, goal, obs, batch_size):
-        # return np.random.uniform(-10, 10, batch_size)
-        return np.repeat(
-            np.expand_dims(goal, 0),
-            batch_size,
-            axis=0
-        )
 
     def convert_obs_to_goals(self, obs):
         return obs[:, 8:9]
@@ -58,49 +44,6 @@ class GoalXVelHalfCheetah(HalfCheetahEnv, MultitaskEnv):
         info_dict['desired_xvel'] = desired_xvel
         info_dict['xvel_error'] = xvel_error
         return ob, reward, done, info_dict
-
-    def sample_states(self, batch_size):
-        raise NotImplementedError()
-        # lows = np.array([
-        #     -0.59481074,
-        #     - 3.7495437,
-        #     - 0.71380705
-        #     - 0.96735003,
-        #     - 0.57593354,
-        #     - 1.15863039,
-        #     - 1.24097252,
-        #     - 0.6463361,
-        #     - 3.66419601,
-        #     - 4.39410921,
-        #     - 9.04578552,
-        #     - 27.18058883,
-        #     - 30.22956479,
-        #     - 26.8349202,
-        #     - 28.4277106,
-        #     - 30.47684186,
-        #     - 22.79845961,
-        #     ])
-        # highs = np.array([
-        #     0.55775534,
-        #     10.39850087,
-        #     1.0833258,
-        #     0.91681375,
-        #     0.89186029,
-        #     0.91657275,
-        #     1.13528496,
-        #     0.69514478,
-        #     3.98017764,
-        #     5.17706281,
-        #     8.30073489,
-        #     25.93850538,
-        #     27.8804229,
-        #     23.84783459,
-        #     30.58961975,
-        #     36.80954249,
-        #     24.14562621,
-        # ])
-        #
-        # return np.random.uniform(lows, highs, batch_size)
 
     def log_diagnostics(self, paths, logger=rllab_logger):
         super().log_diagnostics(paths)
@@ -133,33 +76,6 @@ class GoalXVelHalfCheetah(HalfCheetahEnv, MultitaskEnv):
             ))
         for key, value in statistics.items():
             logger.record_tabular(key, value)
-
-    def oc_reward(self, states, goals, current_states):
-        return self.oc_reward_on_goals(
-            self.convert_obs_to_goals(states),
-            goals,
-            current_states,
-        )
-
-    def oc_reward_on_goals(self, goals_predicted, goals, current_states):
-        return - torch.norm(goals_predicted - goals, dim=1, p=2, keepdim=True)
-
-    def compute_her_reward_pytorch(
-            self,
-            observations,
-            actions,
-            next_observations,
-            goal_states,
-    ):
-        xvels = observations[:, 8:9]
-        target_xvels = goal_states
-        costs = torch.norm(
-            xvels - target_xvels,
-            dim=1,
-            p=2,
-            keepdim=True,
-        )
-        return - costs
 
 
 # At a score of 5000, the cheetah is moving at "5 meters per dt" but dt = 0.05s,
@@ -210,6 +126,8 @@ class GoalXPosHalfCheetah(HalfCheetahEnv, MultitaskEnv, Serializable):
         return obs[:, 17:18]
 
     def log_diagnostics(self, paths, logger=rllab_logger):
+        super().log_diagnostics(paths)
+        MultitaskEnv.log_diagnostics(self, paths)
         distances_to_goal = get_stat_in_paths(
             paths, 'env_infos', 'distance_to_goal'
         )
