0dcd7ef36eaca59810d86385524a4e6993845297
Vitchyr Pong <vitchyr@gmail.com>
2017-12-24 22:09:57 -0800

    update reacher results

--- experiments/state_distance/iclr2018/reacher_results.py
+++ experiments/state_distance/iclr2018/reacher_results.py
@@ -21,18 +21,9 @@ ddpg_criteria = {
     'algo_params.tau': 0.001,
     'env_class.$class': "railrl.envs.multitask.her_reacher_7dof_env.Reacher7Dof"
 }
-# Example DDPG policy: /home/vitchyr/git/rllab-rail/railrl/data/doodads3/10-25-ddpg-reacher-pusher-baseline/10-25-ddpg-reacher-pusher-baseline-id4-s740143/
 
-mb_path = "/mnt/data-backup-12-02-2017/doodads3/10-25-abhishek" \
-          "-mb-baseline-pusher-reacher-300-300-net/"
-mb_criteria = {
-    'env_name_or_class.$class':
-        'railrl.envs.multitask.her_reacher_7dof_env.Reacher7Dof',
-}
-# our_path = "/home/vitchyr/git/rllab-rail/railrl/data/doodads3/10-24-sdql-reacher7everything-vtau/"
-# our_criteria = {
-#     'raw_explore_policy': 'oc',
-# }
+mb_path = "/home/vitchyr/git/railrl/data/doodads3/12-24-model-based-reacher-multitask-fixed-2/"
+mb_criteria = {'dagger_params.normalize': True, 'algorithm': 'Model-Based'}
 our_path = "/mnt/data-backup-12-02-2017/doodads3/10-27-sdql-reacher-get-long-results"
 our_criteria = {
     'epoch_discount_schedule_params.value': 15,
@@ -46,13 +37,11 @@ t1 = our_trials[0]
 
 ddpg_exp = Experiment(ddpg_path)
 mb_exp = Experiment(mb_path)
-# her_dense_exp = Experiment(her_dense_path)
 
 ddpg_trials = ddpg_exp.get_trials(ddpg_criteria)
 mb_trials = mb_exp.get_trials(mb_criteria)
-# her_dense_trials = her_dense_exp.get_trials(her_dense_criteria)
 
-MAX_ITERS = 1000
+MAX_ITERS = 100
 
 base_key = 'Final_Euclidean_distance_to_goal_Mean'
 plt.figure()
