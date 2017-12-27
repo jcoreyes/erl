9956a70fa9408c5df2a8e867e84342421fe69df2
Vitchyr Pong <vitchyr@gmail.com>
2017-12-27 10:20:47 -0800

    save ec2 instance id to variant

--- scripts/run_experiment_from_doodad.py
+++ scripts/run_experiment_from_doodad.py
@@ -5,9 +5,18 @@ args_dict = dd.get_args()
 method_call = args_dict['method_call']
 run_experiment_kwargs = args_dict['run_experiment_kwargs']
 output_dir = args_dict['output_dir']
-print("START from run_experiment_from_doodad:")
-print("output_dir", output_dir)
-print("END from run_experiment_from_doodad:")
+run_mode = args_dict.get('mode', None)
+if run_mode and run_mode == 'ec2':
+    try:
+        import urllib.request
+        instance_id = urllib.request.urlopen(
+            'http://169.254.169.254/latest/meta-data/instance-id'
+        ).read().decode()
+        run_experiment_kwargs['variant']['EC2_instance_id'] = instance_id
+    except Exception as e:
+        print("Could not get instance ID. Error was...")
+        print(e)
+
 run_experiment_here(
     method_call,
     log_dir=output_dir,
