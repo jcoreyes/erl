4817e75737564e9d89a0dc6c35b529c15f27b131
Vitchyr Pong <vitchyr@gmail.com>
2017-12-27 11:25:23 -0800

    timing c5 vs c4

--- experiments/state_distance/tdm/ddpg_tdm_launch.py
+++ experiments/state_distance/tdm/ddpg_tdm_launch.py
@@ -84,9 +84,9 @@ if __name__ == "__main__":
 
     n_seeds = 1
     mode = "ec2"
-    exp_prefix = "tdm-ddpg--ant-batch-size-structure-max-distance-sweep"
+    exp_prefix = "timeit-c5-vs-c4"
 
-    num_epochs = 200
+    num_epochs = 5
     num_steps_per_epoch = 1000
     num_steps_per_eval = 1000
     max_path_length = 50
@@ -152,11 +152,12 @@ if __name__ == "__main__":
             # MultitaskPusher3DEnv,
         ],
         'env_kwargs': [
-            dict(max_distance=2),
-            dict(max_distance=4),
-            dict(max_distance=6),
-            dict(max_distance=8),
-            dict(max_distance=10),
+            dict(),
+            # dict(max_distance=2),
+            # dict(max_distance=4),
+            # dict(max_distance=6),
+            # dict(max_distance=8),
+            # dict(max_distance=10),
         ],
         'qf_criterion_class': [
             nn.MSELoss,
@@ -186,7 +187,7 @@ if __name__ == "__main__":
             # "indicator",
         # ],
         'ddpg_tdm_kwargs.base_kwargs.reward_scale': [
-            10, 100, 1000
+            10,
         ],
         'ddpg_tdm_kwargs.base_kwargs.num_updates_per_env_step': [
             1,
@@ -195,7 +196,7 @@ if __name__ == "__main__":
             1,
         ],
         'ddpg_tdm_kwargs.base_kwargs.batch_size': [
-            128, 1024, 4096
+            128, 256, 512, 1024, 2048, 4096,
         ],
         'ddpg_tdm_kwargs.ddpg_kwargs.tau': [
             0.001,
@@ -204,13 +205,17 @@ if __name__ == "__main__":
             False,
         ],
         'instance_type': [
-            # 'c4.large',
-            # 'c4.xlarge',
-            # 'c4.2xlarge',
+            'c4.large',
+            'c4.xlarge',
+            'c4.2xlarge',
+            'c5.large',
+            'c5.xlarge',
+            'c5.2xlarge',
             # 'c4.4xlarge',
             # 'c4.8xlarge',
-            'g2.2xlarge',
-        ]
+            # 'g2.2xlarge',
+        ],
+        'add_one': [True, False],
     }
     sweeper = hyp.DeterministicHyperparameterSweeper(
         search_space, default_parameters=variant,
@@ -223,6 +228,12 @@ if __name__ == "__main__":
                 ] != 'fixed'
             )
             seed = random.randint(0, 10000)
+            bs = variant['ddpg_tdm_kwargs']['base_kwargs']['batch_size']
+            variant['original_batch_size'] = bs
+            if variant['add_one']:
+                bs += 1
+            variant['ddpg_tdm_kwargs']['base_kwargs']['batch_size'] = bs
+            instance_type = variant['instance_type']
             run_experiment(
                 experiment,
                 mode=mode,
@@ -230,7 +241,7 @@ if __name__ == "__main__":
                 seed=seed,
                 variant=variant,
                 exp_id=exp_id,
-                region='us-west-1',
-                instance_type=variant['instance_type'],
+                region='us-east-1',
+                instance_type=instance_type,
                 use_gpu=variant['instance_type'][0] == 'g',
             )
