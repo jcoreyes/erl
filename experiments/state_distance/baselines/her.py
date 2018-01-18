21905c67963407e0739e76e7177f50bfb4181088
Vitchyr Pong <vitchyr@gmail.com>
2018-01-17 22:06:27 -0800

    give working implementation of HER its own class, qf, and policy, at least nomially

--- experiments/state_distance/baselines/her.py
+++ experiments/state_distance/baselines/her.py
@@ -1,205 +1,218 @@
-import argparse
 import random
 
-from torch import nn
-
-from railrl.data_management.normalizer import IdentityNormalizer
-from railrl.exploration_strategies.gaussian_and_epislon import \
-    GaussianAndEpislonStrategy
-from railrl.state_distance.her import HER, HerQFunction, HerPolicy
-from torch.nn import functional as F
+import numpy as np
+import torch.nn as nn
 
 import railrl.misc.hyperparameter as hyp
 import railrl.torch.pytorch_util as ptu
 from railrl.data_management.her_replay_buffer import HerReplayBuffer
-from railrl.data_management.split_buffer import SplitReplayBuffer
-from railrl.envs.multitask.half_cheetah import GoalXVelHalfCheetah
+from railrl.envs.multitask.ant_env import GoalXYPosAnt, GoalXYPosAndVelAnt
+# from railrl.envs.multitask.half_cheetah import GoalXVelHalfCheetah
+from railrl.envs.multitask.half_cheetah import GoalXVelHalfCheetah, \
+    GoalXPosHalfCheetah
 from railrl.envs.multitask.pusher2d import CylinderXYPusher2DEnv
-from railrl.envs.multitask.reacher_7dof import Reacher7DofXyzGoalState
+from railrl.envs.multitask.pusher3d import MultitaskPusher3DEnv
+from railrl.envs.multitask.pusher3d_gym import GoalXYGymPusherEnv
+from railrl.envs.multitask.walker2d_env import Walker2DTargetXPos
+from railrl.envs.multitask.reacher_7dof import (
+    # Reacher7DofGoalStateEverything,
+    Reacher7DofXyzGoalState,
+)
+from railrl.envs.wrappers import NormalizedBoxEnv
+from railrl.exploration_strategies.base import \
+    PolicyWrappedWithExplorationStrategy
 from railrl.exploration_strategies.ou_strategy import OUStrategy
 from railrl.launchers.launcher_util import run_experiment
-from railrl.state_distance.exploration import \
-    UniversalPolicyWrappedWithExplorationStrategy
-from railrl.torch.data_management.normalizer import TorchNormalizer
+from railrl.state_distance.her import HER, HerQFunction, HerPolicy
+from railrl.state_distance.tdm_networks import TdmNormalizer
 from railrl.torch.modules import HuberLoss
+from railrl.torch.data_management.normalizer import TorchFixedNormalizer
 
 
 def experiment(variant):
-    env_class = variant['env_class']
-    env = env_class(**variant['env_params'])
-    # env = NormalizedBoxEnv(env)
-
-    action_space = env.action_space
-    obs_space = env.observation_space
-    obs_normalizer = variant['normalizer_class'](
-        obs_space.flat_dim, **variant['normalizers_params']
-    )
-    goal_normalizer = variant['normalizer_class'](
-        env.goal_dim, **variant['normalizers_params']
-    )
-
-    qf = variant['qf_class'](
+    env = NormalizedBoxEnv(variant['env_class'](**variant['env_kwargs']))
+    observation_dim = int(np.prod(env.observation_space.low.shape))
+    action_dim = int(np.prod(env.action_space.low.shape))
+    obs_normalizer = TorchFixedNormalizer(observation_dim)
+    goal_normalizer = TorchFixedNormalizer(env.goal_dim)
+    action_normalizer = TorchFixedNormalizer(action_dim)
+    distance_normalizer = TorchFixedNormalizer(env.goal_dim)
+    tdm_normalizer = TdmNormalizer(
         env,
         obs_normalizer=obs_normalizer,
         goal_normalizer=goal_normalizer,
-        **variant['qf_params']
+        action_normalizer=action_normalizer,
+        distance_normalizer=distance_normalizer,
+        max_tau=1,
+        **variant['tdm_normalizer_kwargs']
     )
-    policy = variant['policy_class'](
-        env,
-        obs_normalizer=obs_normalizer,
-        goal_normalizer=goal_normalizer,
-        **variant['policy_params']
+    qf = HerQFunction(
+        env=env,
+        **variant['qf_kwargs']
     )
-    qf_criterion = variant['qf_criterion_class'](
-        **variant['qf_criterion_params']
+    policy = HerPolicy(
+        env=env,
+        tdm_normalizer=tdm_normalizer,
+        **variant['policy_kwargs']
     )
-    es = variant['es_class'](
-        action_space=action_space,
-        **variant['es_params']
+    es = OUStrategy(
+        action_space=env.action_space,
+        **variant['es_kwargs']
     )
-    exploration_policy = UniversalPolicyWrappedWithExplorationStrategy(
+    exploration_policy = PolicyWrappedWithExplorationStrategy(
         exploration_strategy=es,
         policy=policy,
     )
     replay_buffer = HerReplayBuffer(
         env=env,
-        **variant['replay_buffer_params'],
+        **variant['her_replay_buffer_kwargs']
     )
-    algo = HER(
+    qf_criterion = variant['qf_criterion_class'](
+        **variant['qf_criterion_kwargs']
+    )
+    ddpg_tdm_kwargs = variant['ddpg_tdm_kwargs']
+    ddpg_tdm_kwargs['ddpg_kwargs']['qf_criterion'] = qf_criterion
+    algorithm = HER(
         env,
-        qf,
-        policy,
-        exploration_policy,
-        obs_normalizer=obs_normalizer,
-        goal_normalizer=goal_normalizer,
-        qf_criterion=qf_criterion,
+        qf=qf,
         replay_buffer=replay_buffer,
-        **variant['algo_params']
+        policy=policy,
+        exploration_policy=exploration_policy,
+        **variant['ddpg_tdm_kwargs']
     )
     if ptu.gpu_enabled():
-        algo.cuda()
-    algo.train()
+        algorithm.cuda()
+    algorithm.train()
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--render', action='store_true')
-    args = parser.parse_args()
-
+if __name__ == "__main__":
     n_seeds = 1
     mode = "local"
-    exp_prefix = "dev-her-baseline"
+    exp_prefix = "dev-her"
 
-    n_seeds = 2
-    mode = "ec2"
-    exp_prefix = "her-reacher-sweep-layernorm"
+    # n_seeds = 3
+    # mode = "ec2"
+    # exp_prefix = "final-ant-pos-and-vel"
 
-    max_path_length = 50
-    num_epochs = 200
-    num_steps_per_epoch = 16 * max_path_length
-    num_steps_per_eval = 16 * max_path_length
+    num_epochs = 500
+    num_steps_per_epoch = 1000
+    num_steps_per_eval = 1000
+    max_path_length = 100
 
     # noinspection PyTypeChecker
     variant = dict(
-        algo_params=dict(
-            # num_epochs=200*50,  # One epoch here = one cycle in HER paper
-            num_epochs=num_epochs,
-            num_steps_per_epoch=num_steps_per_epoch,
-            num_steps_per_eval=num_steps_per_eval,
-            num_updates_per_epoch=40,
-            use_soft_update=True,
-            tau=0.05,
-            batch_size=128,
-            discount=0.98,
-            qf_learning_rate=1e-3,
-            policy_learning_rate=1e-3,
-            qf_weight_decay=0.,
-            max_path_length=max_path_length,
-            render=args.render,
-            terminate_when_goal_reached=False,
-            collection_mode='batch',
-        ),
-        qf_class=HerQFunction,
-        qf_params=dict(
-            hidden_sizes=[300, 300],
-            hidden_activation=F.relu,
-            layer_norm=True,
-            layer_norm_kwargs=dict(
-                scale=True,
-                center=True,
+        ddpg_tdm_kwargs=dict(
+            base_kwargs=dict(
+                num_epochs=num_epochs,
+                num_steps_per_epoch=num_steps_per_epoch,
+                num_steps_per_eval=num_steps_per_eval,
+                max_path_length=max_path_length,
+                num_updates_per_env_step=25,
+                batch_size=256,
+                discount=1,
+            ),
+            tdm_kwargs=dict(),
+            ddpg_kwargs=dict(
+                tau=0.001,
+                qf_learning_rate=1e-3,
+                policy_learning_rate=1e-4,
             ),
         ),
-        policy_class=HerPolicy,
-        policy_params=dict(
-            hidden_sizes=[300, 300],
-            hidden_activation=F.relu,
-            layer_norm=True,
-            layer_norm_kwargs=dict(
-                scale=True,
-                center=True,
-            ),
-        ),
-        replay_buffer_params=dict(
-            max_size=int(1e6),
+        her_replay_buffer_kwargs=dict(
+            max_size=int(1E6),
             num_goals_to_sample=4,
         ),
-        env_params=dict(),
-        normalizers_params=dict(
-            eps=1e-1,
-            default_clip_range=5,
+        qf_kwargs=dict(
+            hidden_sizes=[300, 300],
         ),
-        normalizer_class=TorchNormalizer,
-        es_class=GaussianAndEpislonStrategy,
-        es_params=dict(
+        policy_kwargs=dict(
+            hidden_sizes=[300, 300],
+        ),
+        es_kwargs=dict(
+            theta=0.1,
             max_sigma=0.1,
             min_sigma=0.1,
-            epsilon=0.2,
         ),
-        # qf_criterion_class=HuberLoss,
-        qf_criterion_class=nn.MSELoss,
-        qf_criterion_params=dict(
-            # delta=1,
+        qf_criterion_class=HuberLoss,
+        qf_criterion_kwargs=dict(),
+        version="HER-Andrychowicz",
+        algorithm="HER-Andrychowicz",
+        env_kwargs=dict(),
+        tdm_normalizer_kwargs=dict(
+            normalize_tau=False,
+            log_tau=False,
         ),
-        exp_prefix=exp_prefix,
-        version="HER - Sparse",
-        algorithm="HER - DDPG",
     )
     search_space = {
-        # 'replay_buffer_params.goal_sample_strategy': [
-        #     'online',
-        #     'store',
-        # ],
         'env_class': [
-            Reacher7DofXyzGoalState,
-            # CylinderXYPusher2DEnv,
             # GoalXVelHalfCheetah,
+            # GoalXPosHalfCheetah,
+            # GoalXYPosAnt,
+            # GoalXYGymPusherEnv,
+            # CylinderXYPusher2DEnv,
+            # Reacher7DofXyzGoalState,
+            # MultitaskPusher3DEnv,
+            # Walker2DTargetXPos,
+            GoalXYPosAndVelAnt,
         ],
-        'algo_params.terminate_when_goal_reached': [
-            True,
+        'env_kwargs.speed_weight': [
+            None,
         ],
-        'algo_params.num_updates_per_epoch': [
-            400,
-        ],
-        'normalizer_class': [
-            IdentityNormalizer,
-            TorchNormalizer,
+        'env_kwargs.goal_dim_weights': [
+            (0.1, 0.1, 0.9, 0.9),
         ],
         'qf_criterion_class': [
             nn.MSELoss,
-            # HuberLoss,
         ],
-        'algo_params.batch_size': [
-            128,
+        'ddpg_tdm_kwargs.tdm_kwargs.sample_rollout_goals_from': [
+            'environment',
         ],
-        'algo_params.reward_scale': [
-            1,
+        'her_replay_buffer_kwargs.num_goals_to_sample': [
+            4,
+        ],
+        'ddpg_tdm_kwargs.base_kwargs.reward_scale': [
+            1, 10, 100,
+        ],
+        'ddpg_tdm_kwargs.base_kwargs.num_updates_per_env_step': [
+            5, 10
+        ],
+        'ddpg_tdm_kwargs.base_kwargs.discount': [
+            0.98,
+        ],
+        'ddpg_tdm_kwargs.ddpg_kwargs.tau': [
+            0.05,
+        ],
+        'ddpg_tdm_kwargs.ddpg_kwargs.policy_pre_activation_weight': [
+            0.,
+            0.01,
+        ],
+        'ddpg_tdm_kwargs.ddpg_kwargs.eval_with_target_policy': [
+            True,
+        ],
+        'ddpg_tdm_kwargs.tdm_kwargs.num_paths_for_normalization': [
+            20, 0
+        ],
+        'relabel': [
+            False,
+            True,
         ],
     }
     sweeper = hyp.DeterministicHyperparameterSweeper(
         search_space, default_parameters=variant,
     )
     for exp_id, variant in enumerate(sweeper.iterate_hyperparameters()):
+        relabel = variant['relabel']
+        if not relabel:
+            variant['ddpg_tdm_kwargs']['tdm_kwargs']['sample_train_goals_from'] = 'no_resampling'
+            variant['ddpg_tdm_kwargs']['tdm_kwargs']['tau_sample_strategy'] = 'no_resampling'
+            variant['version'] = "DDPG-Sparse"
+            variant['algorithm'] = "DDPG-Sparse"
         for i in range(n_seeds):
+            variant['multitask'] = (
+                    variant['ddpg_tdm_kwargs']['tdm_kwargs'][
+                        'sample_rollout_goals_from'
+                    ] != 'fixed'
+            )
             seed = random.randint(0, 10000)
             run_experiment(
                 experiment,
