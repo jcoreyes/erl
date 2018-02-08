d83159790eff01f975b75dfeea9e98026c707db4
Vitchyr Pong <vitchyr@gmail.com>
2018-02-07 17:58:32 -0800

    visualize tdm. simple one does well at least one mtau = 0 and 2d pointmass

--- experiments/state_distance/tdm/visualize_sac_tdm_errors.py
+++ experiments/state_distance/tdm/visualize_sac_tdm_errors.py
@@ -1,13 +1,19 @@
 import argparse
 import numpy as np
 import joblib
+import json
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 from railrl.state_distance.util import merge_into_flat_obs
 
 
 def main(args):
+
     data = joblib.load(args.file)
+    variant_path = Path(args.file).parents[0] / 'variant.json'
+    variant = json.load(variant_path.open())
+    reward_scale = variant['sac_tdm_kwargs']['base_kwargs']['reward_scale']
     if args.pause:
         import ipdb; ipdb.set_trace()
     horizon = args.H
@@ -22,38 +28,48 @@ def main(args):
 
     states = []
 
-    qf_outputs = []
+    q_vals = []
+    v_vals = []
+    a_vals = []
     distances = []
-    entropies = []
+    log_probs = []
 
     for _ in range(horizon):
         states.append(state.copy())
         action, _ = policy.get_action(state)
         next_state, *_ = env.step(action)
         flat_ob = merge_into_flat_obs(state, goal, np.array([0]))
-        qf_outputs.append(
-            qf.eval_np(flat_ob[None], action[None])[0]
-            - vf.eval_np(flat_ob[None])[0]
-        )
-        distances.append(
-            env.convert_ob_to_goal(state)
-            - env.convert_ob_to_goal(next_state)
-        )
+        q_val = qf.eval_np(flat_ob[None], action[None])[0]
+        v_val = vf.eval_np(flat_ob[None])[0]
+        q_vals.append(q_val)
+        v_vals.append(v_val)
+        a_vals.append(q_val - v_val)
+        distances.append(np.abs(
+            env.convert_ob_to_goal(next_state)
+            - env.convert_ob_to_goal(goal)
+        ))
         sample_log_probs = []
+        next_flat_ob = merge_into_flat_obs(next_state, goal, np.array([0]))
         for _ in range(10):
             sample_log_probs.append(
-                policy.eval_np(flat_ob[None], return_log_prob=True)[3]
+                policy.eval_np(next_flat_ob[None], return_log_prob=True)[3]
             )
-        entropies.append(-np.mean(np.vstack(sample_log_probs), axis=0))
+        log_probs.append(np.mean(sample_log_probs))
 
-    qf_outputs = np.array(qf_outputs).sum(1).flatten()
+        state = next_state
+
+    q_vals = np.array(q_vals).sum(1).flatten() / reward_scale
+    v_vals = np.array(v_vals).sum(1).flatten() / reward_scale
+    a_vals = np.array(a_vals).sum(1).flatten() / reward_scale
     distances = np.array(distances).sum(1).flatten()
-    entropies = np.array(entropies).flatten()
-    target_qf_outputs = distances + entropies
-    plt.plot(qf_outputs, label='qf')
-    plt.plot(distances, label='distances')
-    plt.plot(entropies, label='entropies')
-    plt.plot(target_qf_outputs, label='target qf')
+    log_probs = np.array(log_probs).flatten()
+    target_vf_outputs = - distances - log_probs
+    plt.plot(q_vals, label='q values')
+    plt.plot(v_vals, label='v values')
+    plt.plot(a_vals, label='a values')
+    plt.plot(-distances, label='negative distances')
+    plt.plot(log_probs, label='log_probs')
+    plt.plot(target_vf_outputs, label='target qf')
     plt.legend()
     plt.show()
 
