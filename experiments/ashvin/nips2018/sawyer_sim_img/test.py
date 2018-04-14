67b99e5f53891f75809ad38fbf57f77fb9213a48
Ashvin Nair <nair.ashvin@gmail.com>
2018-04-14 10:54:38 -0700

    Better initial Sawyer env position

--- experiments/ashvin/nips2018/sawyer_sim_img/test.py
+++ experiments/ashvin/nips2018/sawyer_sim_img/test.py
@@ -6,18 +6,21 @@ import cv2
 print("making env")
 env = SawyerBlockEnv()
 # env = SawyerEnv()
-env = ImageEnv(env, imsize=400)
+env = ImageEnv(env, imsize=4)
 
 print("starting rollout")
 while True:
+    # import pdb; pdb.set_trace()
     obs = env.reset()
     last_reward_t = 0
     returns = 0
+    x = np.random.rand((4))
     for t in range(100):
         print(t)
         # action = env.action_space.sample()
         action = np.random.rand((4))
-        action = np.zeros((4))
+        # action = np.zeros((4))
+        # action = x
         obs, reward, done, info = env.step(action)
         env.render()
         # print(obs)
