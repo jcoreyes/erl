69c88e559ede23189aa9fe132affb5677eed5d81
mdalal2020 <mdalal@berkeley.edu>
2018-05-27 14:36:33 -0700

    removed unnecessary files

--- experiments/murtaza/vae/torque_control/image_to_state.py
+++ experiments/murtaza/vae/torque_control/image_to_state.py
@@ -1,97 +1,129 @@
-from keras.models import Sequential
-from keras.layers import Dense, Flatten, BatchNormalization, Dropout
-from keras.layers import Convolution2D, MaxPooling2D
-import tensorflow as tf
-from keras.backend.tensorflow_backend import set_session
+from experiments.murtaza.vae.torque_control.ConvTrainer import ConvTrainer
+from railrl.launchers.launcher_util import run_experiment
+import railrl.misc.hyperparameter as hyp
+from railrl.torch.networks import CNN
 import numpy as np
-from sklearn.cross_validation import train_test_split
-import matplotlib.pyplot as plt
 
 
-config = tf.ConfigProto()
-config.gpu_options.allow_growth=True
-set_session(tf.Session(config=config))
+def experiment(variant):
+    from railrl.core import logger
+    import railrl.torch.pytorch_util as ptu
+    ptu.set_gpu_mode(True)
+    info = dict()
+    logger.save_extra_data(info)
+    logger.get_snapshot_dir()
+    net = CNN(**variant['cnn_kwargs'])
+    net.cuda()
+    num_divisions = variant['num_divisions']
+    images = np.zeros((num_divisions * 10000, 21168))
+    states = np.zeros((num_divisions*10000, 7))
+    for i in range(num_divisions):
+        imgs = np.load('/home/murtaza/vae_data/sawyer_torque_control_images100000_' + str(i + 1) + '.npy')
+        state = np.load('/home/murtaza/vae_data/sawyer_torque_control_states100000_' + str(i + 1) + '.npy')[:,:7] % (2 * np.pi)
+        images[i * 10000:(i + 1) * 10000] = imgs
+        states[i * 10000:(i + 1) * 10000] = state
+        print(i)
+    if variant['normalize']:
+        std = np.std(states, axis=0)
+        mu = np.mean(states, axis=0)
+        states = np.divide((states - mu), std)
+    mid = int(num_divisions * 10000 * .9)
+    train_images, test_images = images[:mid], images[mid:]
+    train_labels, test_labels = states[:mid], states[mid:]
 
-X = np.load('/home/murtaza/vae_data/sawyer_torque_control_ou_imgs_zoomed_out10000.npy')
-Y = np.load('/home/murtaza/vae_data/sawyer_torque_control_ou_states_zoomed_out10000.npy')
-# Y = np.concatenate((Y[:, :7], Y[:, 14:]), axis=1)
-Y = Y[:, :7] % (2*np.pi) #joint angle regression only
-X = np.reshape(X, (X.shape[0], 3, 84, 84))
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.1)
-def create_feedforward_network(model, hidden_sizes, input_shape=None):
-    count = 0
-    for size in hidden_sizes:
-        if count == 0 and input_shape is not None:
-            model.add(Dense(size, activation='relu', input_shape=input_shape))
-            count+=1
-        else:
-            model.add(Dense(size, activation='relu'))
 
-def create_convolutional_network(model, conv_sizes, fc_sizes, input_shape):
-    #structure: conv, pool, conv, pool and so on
-    count = 0
-    for size in conv_sizes:
-        num_filters, kernel_size, pool_size = size
-        stride = 1
-        if count == 0:
-            model.add(Convolution2D(num_filters, strides=(stride, stride), kernel_size=(kernel_size, kernel_size), padding='same', activation='relu', input_shape=input_shape))
-            count+=1
-        else:
-            model.add(Convolution2D(num_filters, strides=(stride, stride), kernel_size=(kernel_size, kernel_size), padding='same', activation='relu'))
-        # model.add(BatchNormalization())
-        model.add(MaxPooling2D(pool_size=(pool_size, pool_size)))
-        # model.add(Dropout(.1))
+    algo = ConvTrainer(
+        train_images,
+        test_images,
+        train_labels,
+        test_labels,
+        net,
+        batch_size=variant['batch_size'],
+        lr=variant['lr'],
+        weight_decay=variant['weight_decay']
+    )
+    for epoch in range(variant['num_epochs']):
+        algo.train_epoch(epoch)
+        algo.test_epoch(epoch)
 
-    model.add(Flatten())
-    create_feedforward_network(model, fc_sizes)
+if __name__ == "__main__":
+    n_seeds = 1
+    mode = 'local'
+    exp_prefix = 'conv_net_sweep'
+    use_gpu = True
 
-def create_network(hidden_sizes, num_outputs, do_regression, use_fc=True, conv_sizes=None, input_shape=None, optimizer='adam'):
-    model = Sequential()
-    if use_fc:
-        create_feedforward_network(model, hidden_sizes, input_shape)
+    variant = dict(
+        cnn_kwargs=dict(
+        input_width=84,
+        input_height=84,
+        input_channels=3,
+        output_size=7,
+        kernel_sizes=[3, 3, 3, 3],
+        n_channels=[16, 16, 16, 16],
+        strides=[1, 1, 1, 1],
+        pool_sizes=[2, 2, 2,2],
+        paddings=[0, 0, 0, 0],
+        hidden_sizes=[400, 300, 300],
+        use_batch_norm=True,
+        ),
+        batch_size = 128,
+        lr = 3e-4,
+        normalize=False,
+        num_epochs=100,
+        weight_decay=0,
+        num_divisions=5,
+    )
+
+    search_space = {
+        'batch_size':[128, 256],
+        'cnn_kwargs.hidden_sizes':[[100], [100, 100]],
+        'weight_decay':[0, .0001, .0005, .001, .01],
+        'lr':[1e-2, 1e-3, 1e-4],
+        'normalize':[True, False],
+    }
+    sweeper = hyp.DeterministicHyperparameterSweeper(
+        search_space, default_parameters=variant,
+    )
+    for _ in range(n_seeds):
+        for exp_id, variant in enumerate(sweeper.iterate_hyperparameters()):
+            run_experiment(
+                experiment,
+                exp_prefix=exp_prefix,
+                mode=mode,
+                variant=variant,
+                use_gpu=use_gpu,
+                snapshot_mode='gap',
+                snapshot_gap=20,
+            )
+
+def compute_conv_output_size(h_in, w_in, kernel_size, stride,padding=0):
+    h_out = (h_in + 2 * padding - (kernel_size-1) - 1)/stride + 1
+    w_out = (w_in + 2 * padding - (kernel_size-1) - 1)/stride + 1
+    return int(np.floor(h_out)), int(np.floor(w_out))
+
+def compute_deconv_output_size(h_in, w_in, kernel_size, stride, padding=0):
+    h_out = (h_in -1)*stride - 2*padding + kernel_size
+    w_out = (w_in -1)*stride - 2*padding + kernel_size
+    return int(np.floor(h_out)), int(np.floor(w_out))
+
+def compute_conv_layer_sizes(h_in, w_in, kernel_sizes, strides, pool_sizes, paddings=None):
+    if paddings==None:
+        for kernel, stride, pool in zip(kernel_sizes, strides, pool_sizes):
+            h_in, w_in = compute_conv_output_size(h_in, w_in, kernel, stride)
+            # h_in, w_in = compute_conv_output_size(h_in, w_in, pool, pool)
+            print('Output Size:', (h_in, w_in))
     else:
-        create_convolutional_network(model, conv_sizes, hidden_sizes, input_shape)
-    if do_regression:
-        model.add(Dense(num_outputs))
-        model.compile(loss='mean_squared_error', optimizer=optimizer)
+        for kernel, stride, pool, padding in zip(kernel_sizes, strides, pool_sizes, paddings):
+            h_in, w_in = compute_conv_output_size(h_in, w_in, kernel, stride, padding=padding)
+            h_in, w_in = compute_conv_output_size(h_in, w_in, pool, pool)
+            print('Output Size:', (h_in, w_in))
+
+def compute_deconv_layer_sizes(h_in, w_in, kernel_sizes, strides, paddings=None):
+    if paddings==None:
+        for kernel, stride in zip(kernel_sizes, strides):
+            h_in, w_in = compute_deconv_output_size(h_in, w_in, kernel, stride)
+            print('Output Size:', (h_in, w_in))
     else:
-        model.add(Dense(num_outputs, activation='softmax'))
-        model.compile(loss='categorical_crossentropy',
-                  optimizer=optimizer,
-                  metrics=['accuracy'])
-
-    return model
-
-conv_architectures = [
-    [[16, 5, 1]],
-    # [[16, 5, 3, 2], [32, 3, 1, 1]],
-]
-fc_architectures = [
-    # [256, 128, 64, 32, 16],
-    [32],
-]
-
-batch_size = 32
-num_epochs = 100
-histories = []
-for conv_architecture in conv_architectures:
-    for fc_architecture in fc_architectures:
-        model = create_network(fc_architecture, Y_train.shape[1], do_regression=True, conv_sizes = conv_architecture, use_fc=False, input_shape=[3, 84, 84])
-        hist = model.fit(X_train, Y_train, batch_size=batch_size, shuffle=True, steps_per_epoch=None, epochs=num_epochs, validation_split=0.1)
-        print('Test Loss:', model.evaluate(X_test, Y_test))
-        histories.append(hist)
-train_losses = [history.history['loss'] for history in histories]
-labels = ['Training']
-val_labels = ['Validation']
-# plt.xlabel('Epochs')
-# plt.ylabel('Loss')
-# plt.plot(train_losses[0], label='Training')
-
-# plt.legend()
-# plt.show()
-
-X = np.load('/home/murtaza/vae_data/sawyer_torque_control_ou_imgs_zoomed_out_210000.npy')
-Y = np.load('/home/murtaza/vae_data/sawyer_torque_control_ou_states_zoomed_out_210000.npy')
-X = np.reshape(X, (X.shape[0], 84, 84, 3))
-Y = Y[:,:7]
-print('Different Data Set Test Loss: ', model.evaluate(X, Y))
+        for kernel, stride, padding in zip(kernel_sizes, strides, paddings):
+            h_in, w_in = compute_deconv_output_size(h_in, w_in, kernel, stride, padding=padding)
+            print('Output Size:', (h_in, w_in))
