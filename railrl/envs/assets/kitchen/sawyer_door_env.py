a37fb320ec02a12b3b03c02ad5d8783bbad46268
mdalal2020 <mdalal@berkeley.edu>
2018-05-27 14:33:23 -0700

    made new sawyer door env

--- railrl/envs/assets/kitchen/sawyer_door_env.py
+++
@@ -1,9 +0,0 @@
-import mujoco_py
-import matplotlib.pyplot as plt
-model = mujoco_py.load_model_from_path('sawyer_door.xml')
-sim = mujoco_py.MjSim(model)
-viewer = mujoco_py.MjViewer(sim)
-# plt.imshow(sim.render(500, 500))
-# plt.show()
-while True:
-    viewer.render()
--- railrl/envs/mujoco/sawyer_door_env.py
+++ railrl/envs/mujoco/sawyer_door_env.py
@@ -1,4 +1,9 @@
 import mujoco_py
-model = mujoco_py.load_model_from_path('sawyer_kitchen.xml')
+import matplotlib.pyplot as plt
+model = mujoco_py.load_model_from_path('sawyer_door.xml')
 sim = mujoco_py.MjSim(model)
-sim.render()
+viewer = mujoco_py.MjViewer(sim)
+# plt.imshow(sim.render(500, 500))
+# plt.show()
+while True:
+    viewer.render()
