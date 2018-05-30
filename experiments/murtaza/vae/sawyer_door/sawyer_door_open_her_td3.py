a2c4e73035070b570903fc03b05e0b1fd480784e
mdalal2020 <mdalal@berkeley.edu>
2018-05-30 14:58:19 -0700

    added new sawyer experiment files

--- experiments/murtaza/vae/sawyer_door/sawyer_door_open_her_td3.py
+++ experiments/murtaza/vae/sawyer_door/sawyer_door_open_her_td3.py
@@ -1,6 +1,6 @@
 import railrl.misc.hyperparameter as hyp
 from railrl.data_management.her_replay_buffer import RelabelingReplayBuffer
-from railrl.envs.mujoco.sawyer_reach_torque_env import SawyerReachTorqueEnv
+from railrl.envs.mujoco.sawyer_door_env import SawyerDoorPushOpenEnv
 from railrl.launchers.experiments.vitchyr.multitask import her_td3_experiment
 from railrl.launchers.launcher_util import run_experiment
 
@@ -8,23 +8,23 @@ if __name__ == "__main__":
     variant = dict(
         algo_kwargs=dict(
             num_epochs=500,
-            num_steps_per_epoch=50,
+            num_steps_per_epoch=1000,
             num_steps_per_eval=1000,
-            max_path_length=50,
+            max_path_length=100,
             num_updates_per_env_step=1,
             batch_size=128,
             discount=0.99,
             min_num_steps_before_training=128,
+            render=True
         ),
-        env_class=SawyerReachTorqueEnv,
+        env_class=SawyerDoorPushOpenEnv,
         env_kwargs=dict(
-            keep_vel_in_obs=False,
         ),
         replay_buffer_class=RelabelingReplayBuffer,
         replay_buffer_kwargs=dict(
             max_size=int(1E6),
             fraction_goals_are_rollout_goals=0.1,
-            fraction_goals_are_env_goals=0.5,
+            fraction_resampled_goals_are_env_goals=0.5,
         ),
         qf_kwargs=dict(
             hidden_sizes=[400, 300],
@@ -47,21 +47,18 @@ if __name__ == "__main__":
             5,
             10,
         ],
-        'replay_buffer_kwargs.fraction_goals_are_env_goals': [
+        'replay_buffer_kwargs.fraction_resampled_goals_are_env_goals': [
             0.5,
             1.0,
         ],
         'replay_buffer_kwargs.fraction_goals_are_rollout_goals': [
             0.2,
         ],
-        'env_kwargs.reward_info.type': [
-            'euclidean',
-        ],
         'exploration_type': [
             'ou',
         ],
         'history_len': [
-            2,
+            1,
         ],
     }
     sweeper = hyp.DeterministicHyperparameterSweeper(
@@ -70,7 +67,7 @@ if __name__ == "__main__":
     for exp_id, variant in enumerate(sweeper.iterate_hyperparameters()):
         if (
                 variant['replay_buffer_kwargs']['fraction_goals_are_rollout_goals'] == 1.0
-                and variant['replay_buffer_kwargs']['fraction_goals_are_env_goals'] != 0.0
+                and variant['replay_buffer_kwargs']['fraction_resampled_goals_are_env_goals'] != 0.0
         ):  # redundant setting
             continue
         for _ in range(n_seeds):
