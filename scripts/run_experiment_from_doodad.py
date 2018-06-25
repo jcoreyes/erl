18e0fd1cc47e032a52240cbc4a53fca5e4931466
Steven Lin <stevenlin1111@gmail.com>
2018-06-25 14:14:52 -0700

    Multi-exp support for ec2

--- scripts/run_experiment_from_doodad.py
+++ scripts/run_experiment_from_doodad.py
@@ -1,5 +1,8 @@
 import doodad as dd
+import uuid
 from railrl.launchers.launcher_util import run_experiment_here
+from railrl.launchers import config
+import os
 
 args_dict = dd.get_args()
 method_call = args_dict['method_call']
@@ -7,6 +10,13 @@ run_experiment_kwargs = args_dict['run_experiment_kwargs']
 output_dir = args_dict['output_dir']
 run_mode = args_dict.get('mode', None)
 if run_mode and run_mode == 'ec2':
+    # Hacky. Turning the output dir from
+    # OUTPUT_DIR_FOR_DOODAD_TARGET to OUTPUT_DIR_FOR_DOODAD_TARGET/exp-name
+    subdir_prefix = args_dict['s3_log_name']
+    output_dir = config.OUTPUT_DIR_FOR_DOODAD_TARGET
+    # hopefully unique enough
+    unique_sub_id = str(uuid.uuid4()).split('-')[0]
+    output_dir = os.path.join(output_dir, subdir_prefix + "-" + unique_sub_id)
     try:
         import urllib.request
         instance_id = urllib.request.urlopen(
