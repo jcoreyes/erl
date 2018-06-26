9c9c4d52b33a5605af4da22dc8bfe69682c3634f
Vitchyr Pong <vitchyr@gmail.com>
2018-06-26 11:11:00 -0700

    trying old settings

--- experiments/vitchyr/nips2018/full_tdm_grill.py
+++ experiments/vitchyr/nips2018/full_tdm_grill.py
@@ -1,6 +1,6 @@
 import railrl.misc.hyperparameter as hyp
 from multiworld.envs.mujoco.cameras import init_sawyer_camera_v1, \
-    init_sawyer_camera_v2, init_sawyer_camera_v3
+    init_sawyer_camera_v2, init_sawyer_camera_v3, init_sawyer_camera_v4
 from multiworld.envs.mujoco.sawyer_xyz.sawyer_pick_and_place import \
     SawyerPickAndPlaceEnv
 from multiworld.envs.pygame.point2d import Point2DEnv
@@ -20,28 +20,31 @@ from multiworld.envs.mujoco.sawyer_xyz.sawyer_push_and_reach_env import (
 from railrl.launchers.launcher_util import run_experiment
 from railrl.torch.grill.launcher import grill_her_td3_full_experiment, \
     grill_tdm_td3_full_experiment
+from railrl.torch.modules import HuberLoss
 from railrl.torch.vae.sawyer2d_push_variable_data import generate_vae_dataset
 
 if __name__ == "__main__":
     variant = dict(
-        env_class=SawyerReachXYEnv,
-        # env_class=SawyerPushAndReachXYEnv,
+        # env_class=SawyerReachXYEnv,
+        env_class=SawyerPushAndReachXYEnv,
         # env_class=SawyerPickAndPlaceEnv,
         # env_class=Point2DEnv,
         env_kwargs=dict(
             hide_goal_markers=True,
-            # puck_low=(-0.05, 0.6),
-            # puck_high=(0.05, 0.7),
+            puck_low=(-0.2, 0.5),
+            puck_high=(0.2, 0.7),
+            hand_low=(-0.1, 0.5, 0.),
+            hand_high=(0.1, 0.7, 0.5),
         ),
-        init_camera=init_sawyer_camera_v1,
+        init_camera=init_sawyer_camera_v4,
         grill_variant=dict(
             algo_kwargs=dict(
                 base_kwargs=dict(
                     num_epochs=100,
                     num_steps_per_epoch=1000,
                     num_steps_per_eval=1000,
-                    max_path_length=100,
-                    num_updates_per_env_step=4,
+                    max_path_length=16,
+                    num_updates_per_env_step=1,
                     batch_size=128,
                     discount=1,
                 ),
@@ -49,6 +52,7 @@ if __name__ == "__main__":
                     max_tau=15,
                 ),
                 td3_kwargs=dict(
+                    tau=1,
                 ),
             ),
             replay_kwargs=dict(
@@ -74,14 +78,16 @@ if __name__ == "__main__":
             policy_kwargs=dict(
                 hidden_sizes=[400, 300],
             ),
+            qf_criterion_class=HuberLoss,
             # vae_path='06-25-pusher-state-puck-reward-cached-goals-hard-2/06-25-pusher-state-puck-reward-cached-goals-hard-2-id0-s48265/vae.pkl',
+            # vae_path="05-23-vae-sawyer-variable-fixed-2/05-23-vae-sawyer-variable-fixed-2_2018_05_23_16_19_33_0000--s-293-nImg-1000--cam-sawyer_init_camera_zoomed_in_fixed/params.pkl",
         ),
         train_vae_variant=dict(
             representation_size=16,
             beta=1.0,
             num_epochs=1000,
             generate_vae_dataset_kwargs=dict(
-                N=100,
+                N=1000,
                 oracle_dataset=True,
                 num_channels=3,
                 # show=True,
@@ -103,9 +109,10 @@ if __name__ == "__main__":
     )
 
     search_space = {
-        'grill_variant.algo_kwargs.tdm_kwargs.max_tau': [15, 30, 50],
+        # 'grill_variant.algo_kwargs.tdm_kwargs.max_tau': [15, 30, 50],
         'grill_variant.algo_kwargs.base_kwargs.reward_scale': [
-            0.01, 1, 100, 10000
+            # 0.0001,
+            1,
         ],
         # 'grill_variant.observation_key': ['latent_observation'],
         # 'grill_variant.desired_goal_key': ['state_desired_goal'],
@@ -128,7 +135,7 @@ if __name__ == "__main__":
     exp_prefix = 'dev'
 
     mode = 'ec2'
-    exp_prefix = 'mw-full-grill-tdm-vitchyr-test-reacher'
+    exp_prefix = 'mw-full-grill-tdm-vitchyr-old-settings-1-seed'
     for exp_id, variant in enumerate(sweeper.iterate_hyperparameters()):
         run_experiment(
             grill_tdm_td3_full_experiment,
@@ -140,5 +147,5 @@ if __name__ == "__main__":
             snapshot_gap=50,
             snapshot_mode='gap_and_last',
             exp_id=exp_id,
-            num_exps_per_instance=3,
+            num_exps_per_instance=1,
         )
