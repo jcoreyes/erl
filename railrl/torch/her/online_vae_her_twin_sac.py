9e190d8ee1596e6124761d5ae8c56893479b912c
Steven Lin <stevenlin1111@gmail.com>
2018-07-21 15:02:00 -0700

    add experiments

---
+++ railrl/torch/her/online_vae_her_twin_sac.py
@@ -0,0 +1,25 @@
+from railrl.torch.her.her_twin_sac import HerTwinSac
+from railrl.torch.vae.online_vae_algorithm import OnlineVaeAlgorithm
+import railrl.torch.vae.vae_schedules as vae_schedules
+from railrl.data_management.online_vae_replay_buffer \
+        import OnlineVaeRelabelingBuffer
+
+class OnlineVaeHerTwinSac(OnlineVaeAlgorithm, HerTwinSac):
+
+    def __init__(
+        self,
+        online_vae_algo_kwargs,
+        algo_kwargs,
+    ):
+        OnlineVaeAlgorithm.__init__(
+            self,
+            **online_vae_algo_kwargs,
+        )
+        HerTwinSac.__init__(self, **algo_kwargs)
+
+        assert isinstance(self.replay_buffer, OnlineVaeRelabelingBuffer)
+
+    @property
+    def networks(self):
+        return HerTwinSac.networks.fget(self) + \
+               OnlineVaeAlgorithm.networks.fget(self)
