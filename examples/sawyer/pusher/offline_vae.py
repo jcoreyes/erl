ae6ac3aca0aaf1a84d90b2917d0a66f3d8f49129
Murtaza Dalal <mdalal@berkeley.edu>
2018-10-31 13:28:17 -0700

    pusher offline vae exp

--- examples/sawyer/pusher/offline_vae.py
+++ examples/sawyer/pusher/offline_vae.py
@@ -1,13 +1,10 @@
 import railrl.misc.hyperparameter as hyp
 from multiworld.envs.mujoco.cameras import sawyer_pusher_camera_upright_v2
-from multiworld.envs.mujoco.sawyer_xyz.sawyer_push_and_reach_env import (
-    SawyerPushAndReachXYEnv
-)
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_push_and_reach_env import SawyerPushAndReachXYEnv
 from railrl.launchers.launcher_util import run_experiment
 from railrl.torch.grill.launcher import grill_her_td3_full_experiment
 
 if __name__ == "__main__":
-    # noinspection PyTypeChecker
     variant = dict(
         imsize=84,
         init_camera=sawyer_pusher_camera_upright_v2,
@@ -17,8 +14,7 @@ if __name__ == "__main__":
         ),
         grill_variant=dict(
             save_video=True,
-            save_video_period=500,
-            do_state_exp=True,
+            save_video_period=50,
             qf_kwargs=dict(
                 hidden_sizes=[400, 300],
             ),
@@ -35,11 +31,11 @@ if __name__ == "__main__":
                     max_path_length=100,
                     discount=0.99,
                     num_updates_per_env_step=1,
-                    collection_mode='online-parallel',
+                    collection_mode='online',
                     parallel_env_params=dict(
                         num_workers=1,
                     ),
-                    reward_scale=100,
+                    reward_scale=1,
                 ),
                 her_kwargs=dict(),
                 td3_kwargs=dict(
@@ -48,29 +44,35 @@ if __name__ == "__main__":
             ),
             replay_buffer_kwargs=dict(
                 max_size=int(1e6),
-                fraction_goals_are_rollout_goals=0.5,
+                fraction_goals_are_rollout_goals=0,
                 fraction_resampled_goals_are_env_goals=0.5,
             ),
-            algorithm='STATE-HER-TD3',
+            algorithm='OFFLINE-VAE-HER-TD3',
             normalize=False,
             render=False,
             exploration_noise=0.8,
             exploration_type='ou',
             training_mode='train',
             testing_mode='test',
-            observation_key='state_observation',
-            desired_goal_key='state_desired_goal',
+            reward_params=dict(
+                type='latent_distance',
+            ),
+            observation_key='latent_observation',
+            desired_goal_key='latent_desired_goal',
+            vae_wrapped_env_kwargs=dict(
+                sample_from_true_prior=True,
+            )
         ),
         train_vae_variant=dict(
             vae_path=None,
             representation_size=16,
             beta=.5,
-            num_epochs=1000,
+            num_epochs=2500,
             dump_skew_debug_plots=False,
             generate_vae_dataset_kwargs=dict(
                 test_p=.9,
                 N=5000,
-                oracle_dataset=True,
+                oracle_dataset_using_set_to_goal=True,
                 use_cached=True,
                 vae_dataset_specific_kwargs=dict(),
                 show=False,
@@ -85,38 +87,34 @@ if __name__ == "__main__":
                 batch_size=64,
                 lr=1e-3,
             ),
-            save_period=10,
+            save_period=50,
         ),
     )
 
     search_space = {
-        'env_kwargs.reward_type':['puck_distance', 'state_distance'],
-        'env_kwargs.reset_free':[True, False],
-        'grill_variant.algo_kwargs.base_kwargs.max_path_length':[100, 250],
+        'train_vae_variant.beta':[1],
+        # 'grill_variant.replay_buffer_kwargs.fraction_goals_are_rollout_goals':[0, .5],
+        # 'grill_variant.algo_kwargs.base_kwargs.max_path_length': [100, 250],
     }
-
     sweeper = hyp.DeterministicHyperparameterSweeper(
         search_space, default_parameters=variant,
     )
 
-
-    # n_seeds = 1
-    # mode = 'local'
-    # exp_prefix = 'test'
-
     n_seeds = 1
-    mode = 'ec2'
-    exp_prefix = 'sawyer_pusher_state_her_td3_finalized_sweep'
+    mode = 'local'
+    exp_prefix = 'test'
+
+    # n_seeds = 3
+    # mode = 'ec2'
+    # exp_prefix = 'sawyer_pusher_offline_vae_final'
 
     for exp_id, variant in enumerate(sweeper.iterate_hyperparameters()):
-        for i in range(n_seeds):
+        for _ in range(n_seeds):
             run_experiment(
                 grill_her_td3_full_experiment,
                 exp_prefix=exp_prefix,
                 mode=mode,
-                snapshot_mode='gap_and_last',
-                snapshot_gap=500,
                 variant=variant,
                 use_gpu=True,
-                num_exps_per_instance=5,
-            )
+                num_exps_per_instance=2,
+          )
