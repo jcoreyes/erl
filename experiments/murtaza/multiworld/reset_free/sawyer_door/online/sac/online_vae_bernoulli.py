1d5e46cb1fda862c8a9de410328850c114104955
Murtaza Dalal <mdalal@berkeley.edu>
2018-11-01 20:37:31 -0700

    fixed import

--- experiments/murtaza/multiworld/reset_free/sawyer_door/online/sac/online_vae_bernoulli.py
+++ experiments/murtaza/multiworld/reset_free/sawyer_door/online/sac/online_vae_bernoulli.py
@@ -1,9 +1,9 @@
 import railrl.misc.hyperparameter as hyp
-from railrl.torch.vae.generate_goal_dataset import generate_goal_dataset_using_policy
 from multiworld.envs.mujoco.cameras import sawyer_door_env_camera_v0
 from railrl.launchers.launcher_util import run_experiment
 from railrl.torch.grill.launcher import grill_her_twin_sac_online_vae_full_experiment
 import railrl.torch.vae.vae_schedules as vae_schedules
+from railrl.torch.vae.dataset.generate_goal_dataset import generate_goal_dataset_using_policy
 
 if __name__ == "__main__":
     variant = dict(
