c134cfd9b491fefb6639e0f12be8efd8638915c3
Vitchyr Pong <vitchyr@gmail.com>
2018-11-06 22:32:11 -0800

    update generate report script to make gif

--- scripts/fitskew/generate_report.py
+++ scripts/fitskew/generate_report.py
@@ -1,15 +1,19 @@
-import argparse
 import glob
 import json
-from os.path import join
 import pickle
+from os.path import join
+
+import argparse
+import numpy as np
 import re
+from skvideo.io import vwrite
 
 from railrl.misc.html_report import HTMLReport
+from railrl.misc.visualization_util import gif
+from railrl.torch.vae.skew.datasets import project_square_border_np_4x4
 from railrl.torch.vae.skew.skewed_vae_with_histogram import (
     visualize_vae_samples, visualize_vae,
 )
-from railrl.torch.vae.skew.datasets import project_square_border_np_4x4
 
 
 def append_itr(paths):
@@ -40,23 +44,22 @@ def main():
                         default='report_retroactive.html')
 
     args = parser.parse_args()
-    dir = args.dir
+    directory = args.dir
     report_name = args.report_name
-    # dir = '/home/vitchyr/git/railrl/data/doodads3/10-30-point2d-debug-why-alpha0-crashes/10-30-point2d-debug-why-alpha0-crashes_2018_10_30_19_57_53_id000--s54973/'
 
-    with open(join(dir, 'variant.json')) as variant_file:
+    with open(join(directory, 'variant.json')) as variant_file:
         variant = json.load(variant_file)
     skew_config = get_key_recursive(variant, 'skew_config')
-    pkl_paths = glob.glob(dir + '/*.pkl')
+    pkl_paths = glob.glob(directory + '/*.pkl')
     numbered_paths = append_itr(pkl_paths)
     ordered_numbered_paths = sorted(numbered_paths, key=lambda x: x[1])
 
-    report = HTMLReport(join(dir, report_name), images_per_row=5)
+    report = HTMLReport(join(directory, report_name), images_per_row=5)
 
+    vae_heatmap_imgs = []
+    sample_imgs = []
     for path, itr in ordered_numbered_paths:
-        if 30 > itr or itr > 35:
-            continue
-        print("Processing itration {}".format(itr))
+        print("Processing iteration {}".format(itr))
         snapshot = pickle.load(open(path, "rb"))
         if 'vae' in snapshot:
             vae = snapshot['vae']
@@ -66,19 +69,35 @@ def main():
         vae_train_data = snapshot['train_data']
         dynamics = snapshot.get('dynamics', project_square_border_np_4x4)
         report.add_header("Iteration {}".format(itr))
-        visualize_vae_samples(
+        vae_heatmap_img = visualize_vae_samples(
             itr,
             vae_train_data,
             vae,
             report,
             dynamics=dynamics,
         )
-        visualize_vae(
+        sample_img = visualize_vae(
             vae,
             skew_config,
             report,
             title="Post-skew",
         )
+        vae_heatmap_imgs.append(vae_heatmap_img)
+        sample_imgs.append(sample_img)
+
+    report.add_header("Summary GIFs")
+    for filename, imgs in [
+        ("vae_heatmaps", vae_heatmap_imgs),
+        ("samples", sample_imgs),
+    ]:
+        video = np.stack(imgs)
+        vwrite(
+            '{}/{}.mp4'.format(directory, filename),
+            video,
+        )
+        gif_file_path = '{}/{}.gif'.format(directory, filename)
+        gif(gif_file_path, video)
+        report.add_image(gif_file_path, txt=filename, is_url=True)
 
     report.save()
     print("Report saved to")
