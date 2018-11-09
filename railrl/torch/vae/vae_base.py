a9b6313dc1c880e47e1cde28d1c46716846b8a67
Murtaza Dalal <mdalal@berkeley.edu>
2018-11-08 22:47:37 -0800

    changed to accomodate fit skew

--- railrl/torch/vae/vae_base.py
+++ railrl/torch/vae/vae_base.py
@@ -24,7 +24,7 @@ class VAEBase(PyTorchModule,  metaclass=abc.ABCMeta):
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def rsample(self, latent_distribution_params, num_latents_to_sample=1):
+    def rsample(self, latent_distribution_params):
         """
 
         :param latent_distribution_params:
@@ -87,12 +87,21 @@ class GaussianLatentVAE(VAEBase):
         self.dist_mu = np.zeros(self.representation_size)
         self.dist_std = np.ones(self.representation_size)
 
-    def rsample(self, latent_distribution_params, num_latents_to_sample=1):
+    def rsample(self, latent_distribution_params):
         mu, logvar = latent_distribution_params
         mu = mu.view((mu.size()[0], mu.size()[1]))
         stds = (0.5 * logvar).exp()
         stds = stds.view(stds.size()[0], stds.size()[1])
         epsilon = ptu.randn((mu.size()[0], mu.size()[1]))
+        latents = epsilon * stds + mu
+        return latents
+
+    def rsample_multiple_latents(self, latent_distribution_params, num_latents_to_sample=1):
+        mu, logvar = latent_distribution_params
+        mu = mu.view((mu.size()[0], 1, mu.size()[1]))
+        stds = (0.5 * logvar).exp()
+        stds = stds.view(stds.size()[0], 1, stds.size()[1])
+        epsilon = ptu.randn((mu.size()[0], num_latents_to_sample, mu.size()[1]))
         latents = epsilon * stds + mu
         return latents
 
