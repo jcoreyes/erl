KL fix

railrl/torch/vae/vae_base.py
@@ -120,7 +120,7 @@ class GaussianLatentVAE(VAEBase):
 
     def vectorized_kl_divergence(self, latent_distribution_params):
         mu, logvar = latent_distribution_params
-        return - 0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp(), dim=1)
+        return  -1*torch.sum(1 + logvar - mu.pow(2) - logvar.exp(), dim=1)
 
     def kl_divergence(self, latent_distribution_params):
         return self.vectorized_kl_divergence(latent_distribution_params).mean()