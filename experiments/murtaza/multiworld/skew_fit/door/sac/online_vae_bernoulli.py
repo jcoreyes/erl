c53e73630cc6ff550854c9ee1991d0894ec98669
Murtaza Dalal <mdalal@berkeley.edu>
2019-01-20 14:59:00 -0800

    added vae dist mu std to online vae

--- experiments/murtaza/multiworld/skew_fit/door/sac/online_vae_bernoulli.py
+++ experiments/murtaza/multiworld/skew_fit/door/sac/online_vae_bernoulli.py
@@ -146,13 +146,13 @@ if __name__ == "__main__":
         search_space, default_parameters=variant,
     )
 
-    n_seeds = 1
-    mode = 'local'
-    exp_prefix = 'test'
+    # n_seeds = 1
+    # mode = 'local'
+    # exp_prefix = 'test'
 
-    # n_seeds = 4
-    # mode = 'gcp'
-    # exp_prefix = 'door-sample-from-true-prior-sweep'
+    n_seeds = 4
+    mode = 'gcp'
+    exp_prefix = 'door-sample-from-true-prior-sweep-fixed'
 
     for exp_id, variant in enumerate(sweeper.iterate_hyperparameters()):
         for _ in range(n_seeds):
