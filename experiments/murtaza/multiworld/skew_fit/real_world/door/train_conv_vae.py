ffb2c5dd7c4782f426cd05829b51c979eb59b174
Murtaza Dalal <mdalal@berkeley.edu>
2019-02-07 17:31:56 -0800

    exp update

--- experiments/murtaza/multiworld/skew_fit/real_world/door/train_conv_vae.py
+++ experiments/murtaza/multiworld/skew_fit/real_world/door/train_conv_vae.py
@@ -3,12 +3,11 @@ from torch import nn
 import railrl.misc.hyperparameter as hyp
 from railrl.launchers.launcher_util import run_experiment
 from railrl.misc.ml_util import PiecewiseLinearSchedule
-from railrl.torch.vae.conv_vae import ConvVAE
+from railrl.torch.vae.conv_vae import ConvVAE, ConvVAEDouble
 from railrl.torch.vae.vae_trainer import ConvVAETrainer
 from railrl.torch.grill.launcher import generate_vae_dataset
-from railrl.torch.vae.conv_vae import imsize84_default_architecture
+from railrl.torch.vae.conv_vae import imsize48_default_architecture
 from railrl.misc.asset_loader import load_local_or_remote_file
-import pickle
 
 def experiment(variant):
     from railrl.core import logger
@@ -18,8 +17,8 @@ def experiment(variant):
     train_data, test_data, info = variant['generate_vae_dataset_fn'](
         variant['generate_vae_dataset_kwargs']
     )
-    # uniform_dataset = load_local_or_remote_file(variant['uniform_dataset_path']).item()
-    # uniform_dataset = unormalize_image(uniform_dataset['image_desired_goal'])
+    uniform_dataset = load_local_or_remote_file(variant['uniform_dataset_path']).item()
+    uniform_dataset = unormalize_image(uniform_dataset['image_desired_goal'])
     logger.save_extra_data(info)
     logger.get_snapshot_dir()
     if 'beta_schedule_kwargs' in variant:
@@ -39,7 +38,7 @@ def experiment(variant):
     for epoch in range(variant['num_epochs']):
         should_save_imgs = (epoch % save_period == 0)
         t.train_epoch(epoch)
-        # t.log_loss_under_uniform(m, uniform_dataset)
+        t.log_loss_under_uniform(m, uniform_dataset, variant['algo_kwargs']['priority_function_kwargs'])
         t.test_epoch(epoch, save_reconstruction=should_save_imgs,
                      save_scatterplot=should_save_imgs)
         if should_save_imgs:
@@ -48,18 +47,18 @@ def experiment(variant):
                 t.dump_best_reconstruction(epoch)
                 t.dump_worst_reconstruction(epoch)
                 t.dump_sampling_histogram(epoch)
-                # t.dump_uniform_imgs_and_reconstructions(dataset=uniform_dataset, epoch=epoch)
+                t.dump_uniform_imgs_and_reconstructions(dataset=uniform_dataset, epoch=epoch)
         t.update_train_weights()
 
 
 if __name__ == "__main__":
-    # n_seeds = 1
-    # mode = 'local'
-    # exp_prefix = 'test'
-
     n_seeds = 1
-    mode = 'gcp'
-    exp_prefix = 'skew-fit-real-world-random-policy-data-sweep-v7'
+    mode = 'local'
+    exp_prefix = 'offline-vae-real-world-random-policy-data-gaussian-decoder'
+
+    # n_seeds = 1
+    # mode = 'gcp'
+    # exp_prefix = 'skew-fit-real-world-random-policy-data-sweep-v7'
 
     use_gpu = True
 
@@ -70,17 +69,18 @@ if __name__ == "__main__":
             batch_size=64,
             lr=1e-3,
             skew_config=dict(
-                method='inv_bernoulli_p_x',
-                power=2,
+                method='vae_prob',
+                power=-1/100,
             ),
-            skew_dataset=True,
+            skew_dataset=False,
             priority_function_kwargs=dict(
                 num_latents_to_sample=10,
-                sampling_method='correct',
+                sampling_method='true_prior_sampling',
+                decoder_distribution='gaussian_identity_variance'
             ),
             use_parallel_dataloading=False,
         ),
-        vae=ConvVAE,
+        vae=ConvVAEDouble,
         dump_skew_debug_plots=True,
         generate_vae_dataset_fn=generate_vae_dataset,
         generate_vae_dataset_kwargs=dict(
@@ -88,31 +88,27 @@ if __name__ == "__main__":
             random_and_oracle_policy_data=True,
             random_and_oracle_policy_data_split=1,
             use_cached=True,
-            imsize=84,
+            imsize=48,
             non_presampled_goal_img_is_garbage=True,
             vae_dataset_specific_kwargs=dict(),
             n_random_steps=1,
             show=False,
-            dataset_path='datasets/SawyerDoorEnv_N1000__imsize84_random_oracle_split_1.npy',
+            dataset_path='datasets/SawyerDoorEnv_N1000__imsize48_random_oracle_split_1.npy',
         ),
         vae_kwargs=dict(
             input_channels=3,
-            imsize=84,
-            decoder_distribution='bernoulli',
-            architecture=imsize84_default_architecture,
+            imsize=48,
+            decoder_distribution='gaussian',
+            architecture=imsize48_default_architecture,
         ),
         save_period=50,
         beta=5,
         representation_size=16,
-        # uniform_dataset_path='goals/SawyerDoorEnv_N100_imsize48goals_twin_sac.npy'
+        uniform_dataset_path='goals/SawyerDoorEnv_N100_imsize48goals_twin_sac.npy'
     )
 
     search_space = {
-        'beta':[5],
-        'algo_kwargs.priority_function_kwargs.num_latents_to_sample':[10],
-        'algo_kwargs.skew_config.power':[0],
-        'algo_kwargs.priority_function_kwargs.sampling_method':['correct'],
-        # 'generate_vae_dataset_kwargs.test_p':[.1, .3, .5, .9],
+        'beta':[.01, .1, 1, 2.5, 5, 10],
     }
     sweeper = hyp.DeterministicHyperparameterSweeper(
         search_space, default_parameters=variant,
@@ -135,4 +131,5 @@ if __name__ == "__main__":
                         num_gpu=1,
                     )
                 ),
+                skip_wait=True,
             )
