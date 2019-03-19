77ecf0b06c9c3da1deca3011c177a6fb9eddeec9
Vitchyr Pong <vitchyr@gmail.com>
2019-03-19 15:06:08 -0700

    update sac to match tf sac

--- experiments/references/twin_sac.py
+++ experiments/references/twin_sac.py
@@ -6,6 +6,8 @@ from gym.envs.mujoco import (
     AntEnv,
     Walker2dEnv,
     InvertedDoublePendulumEnv,
+    HopperEnv,
+    HumanoidEnv,
 )
 from gym.envs.classic_control import PendulumEnv
 
@@ -14,40 +16,43 @@ from railrl.launchers.launcher_util import run_experiment
 import railrl.torch.pytorch_util as ptu
 from railrl.torch.networks import FlattenMlp
 from railrl.torch.sac.policies import TanhGaussianPolicy
-from railrl.torch.sac.twin_sac import TwinSAC
+from railrl.torch.sac.sac import SAC
 import railrl.misc.hyperparameter as hyp
 
 
 ENV_PARAMS = {
     'half-cheetah': {  # 6 DoF
         'env_class': HalfCheetahEnv,
+        'num_epochs': 3000,
+    },
+    'hopper': {  # 6 DoF
+        'env_class': HopperEnv,
         'num_epochs': 1000,
-        'train_policy_with_reparameterization': True,
+    },
+    'humanoid': {  # 6 DoF
+        'env_class': HumanoidEnv,
+        'num_epochs': 3000,
     },
     'inv-double-pendulum': {  # 2 DoF
         'env_class': InvertedDoublePendulumEnv,
-        'num_epochs': 50,
-        'train_policy_with_reparameterization': True,
+        'num_epochs': 100,
     },
     'pendulum': {  # 2 DoF
         'env_class': PendulumEnv,
-        'num_epochs': 50,
+        'num_epochs': 200,
         'num_steps_per_epoch': 200,
         'num_steps_per_eval': 200,
         'max_path_length': 200,
-        'min_num_steps_before_training': 200,
+        'min_num_steps_before_training': 2000,
         'target_update_period': 200,
-        'train_policy_with_reparameterization': False,
     },
     'ant': {  # 6 DoF
         'env_class': AntEnv,
         'num_epochs': 3000,
-        'train_policy_with_reparameterization': True,
     },
     'walker': {  # 6 DoF
         'env_class': Walker2dEnv,
         'num_epochs': 3000,
-        'train_policy_with_reparameterization': True,
     },
 }
 
@@ -70,12 +75,9 @@ def experiment(variant):
         discount=variant['discount'],
         replay_buffer_size=variant['replay_buffer_size'],
         soft_target_tau=variant['soft_target_tau'],
-        policy_update_period=variant['policy_update_period'],
         target_update_period=variant['target_update_period'],
-        train_policy_with_reparameterization=variant['train_policy_with_reparameterization'],
         policy_lr=variant['policy_lr'],
         qf_lr=variant['qf_lr'],
-        vf_lr=variant['vf_lr'],
         reward_scale=1,
         use_automatic_entropy_tuning=True,
     )
@@ -91,28 +93,16 @@ def experiment(variant):
         output_size=1,
         hidden_sizes=[M, M],
     )
-    vf = FlattenMlp(
-        input_size=obs_dim,
-        output_size=1,
-        hidden_sizes=[M, M],
-    )
-    target_vf = FlattenMlp(
-        input_size=obs_dim,
-        output_size=1,
-        hidden_sizes=[M, M],
-    )
     policy = TanhGaussianPolicy(
         obs_dim=obs_dim,
         action_dim=action_dim,
         hidden_sizes=[M, M],
     )
-    algorithm = TwinSAC(
+    algorithm = SAC(
         env,
         policy=policy,
         qf1=qf1,
         qf2=qf2,
-        vf=vf,
-        target_vf=target_vf,
         **variant['algo_kwargs']
     )
     algorithm.to(ptu.device)
@@ -123,19 +113,16 @@ if __name__ == "__main__":
     variant = dict(
         num_epochs=3000,
         num_steps_per_epoch=1000,
-        num_steps_per_eval=1000, #check
-        max_path_length=1000, #check
-        min_num_steps_before_training=1000, #check
+        num_steps_per_eval=1000,
+        max_path_length=1000,
+        min_num_steps_before_training=1000,
         batch_size=256,
         discount=0.99,
         replay_buffer_size=int(1E6),
-        soft_target_tau=1.0,
-        policy_update_period=1, #check
-        target_update_period=1000,  #check
-        train_policy_with_reparameterization=False,
+        soft_target_tau=5e-3,
+        target_update_period=1,
         policy_lr=3E-4,
         qf_lr=3E-4,
-        vf_lr=3E-4,
         layer_size=256, # [256, 512]
         algorithm="Twin-SAC",
         version="normal",
@@ -145,17 +132,19 @@ if __name__ == "__main__":
     mode = 'local'
     exp_prefix = 'dev'
 
-    n_seeds = 3
-    mode = 'ec2'
-    exp_prefix = 'reference-twin-sac-sweep'
+    n_seeds = 2
+    mode = 'sss'
+    exp_prefix = 'reference-new-tsac-pre-refactor'
 
     search_space = {
         'env': [
-            'half-cheetah',
+            # 'half-cheetah',
             'inv-double-pendulum',
             'pendulum',
             'ant',
             'walker',
+            'hopper',
+            'humanoid',
         ],
     }
     sweeper = hyp.DeterministicHyperparameterSweeper(
