38ea24a19ec89f08c847fb1fa47d82889ef47367
Murtaza Dalal <mdalal@berkeley.edu>
2019-10-16 12:14:59 -0700

    added basic door experiment on td3 bc

---
+++ experiments/murtaza/off_policy_ssl/door/state.py
@@ -0,0 +1,103 @@
+import gym
+
+import railrl.torch.pytorch_util as ptu
+from railrl.data_management.obs_dict_replay_buffer import ObsDictRelabelingBuffer
+from railrl.exploration_strategies.base import \
+    PolicyWrappedWithExplorationStrategy
+from railrl.exploration_strategies.gaussian_and_epislon import \
+    GaussianAndEpislonStrategy
+from railrl.launchers.launcher_util import setup_logger, run_experiment
+from railrl.samplers.data_collector import GoalConditionedPathCollector
+from railrl.torch.her.her import HERTrainer
+from railrl.torch.networks import FlattenMlp, TanhMlpPolicy
+# from railrl.torch.td3.td3 import TD3
+from railrl.demos.td3_bc import TD3BCTrainer
+from railrl.torch.torch_rl_algorithm import TorchBatchRLAlgorithm
+# from multiworld.envs.mujoco.sawyer_xyz.sawyer_push_multiobj_subset import SawyerMultiobjectEnv
+# from multiworld.envs.mujoco.sawyer_xyz.sawyer_reach import SawyerReachXYZEnv
+
+from multiworld.core.image_env import ImageEnv
+# from sawyer_control.envs.sawyer_reaching import SawyerReachXYZEnv
+
+from railrl.launchers.arglauncher import run_variants
+import railrl.misc.hyperparameter as hyp
+
+# from railrl.launchers.experiments.ashvin.rfeatures.rfeatures_trainer import TimePredictionTrainer
+from railrl.launchers.experiments.ashvin.rfeatures.rfeatures_rl import encoder_wrapped_td3bc_experiment
+
+if __name__ == "__main__":
+    # demo_path = ["/home/anair/ros_ws/src/railrl-private/demos/door_demos_v3/processed_demos_%s_jitter2.pkl" % color for color in ["grey", "beige", "green", "brownhatch"]]
+    # demo_off_policy_path = ["/home/anair/data/s3doodad/ashvin/rfeatures/sawyer/door2/bc-v3-varied1/run%s/id0/video_0_env.p" % str(i) for i in [0, 1]]
+    # print(demo_off_policy_path)
+    demo_path = None
+    demo_off_policy_path = None
+    variant = dict(
+        env_id='SawyerDoorHookResetFreeEnv-v1',
+        algo_kwargs=dict(
+            num_epochs=200,
+            max_path_length=100,
+            batch_size=128,
+            num_eval_steps_per_epoch=1000,
+            num_expl_steps_per_train_loop=500,
+            num_trains_per_train_loop=1000,
+            min_num_steps_before_training=1000,
+        ),
+        trainer_kwargs=dict(
+            discount=0.99,
+            demo_path=demo_path,
+            demo_off_policy_path=demo_off_policy_path,
+            add_demo_latents=False, # already done
+            bc_num_pretrain_steps=10000,
+            q_num_pretrain_steps=10000,
+            rl_weight=1.0,
+            bc_weight=0,
+            # reward_scale=0.0001,
+            # weight_decay=0.001,
+        ),
+        replay_buffer_kwargs=dict(
+            max_size=1000000,
+            fraction_goals_rollout_goals=0.2,
+            fraction_goals_env_goals=0.5,
+        ),
+        qf_kwargs=dict(
+            hidden_sizes=[400, 300],
+        ),
+        policy_kwargs=dict(
+            hidden_sizes=[400, 300],
+        ),
+        save_video=False,
+    )
+
+    search_space = {
+    }
+    sweeper = hyp.DeterministicHyperparameterSweeper(
+        search_space, default_parameters=variant,
+    )
+
+    n_seeds = 1
+    mode = 'local'
+    exp_prefix = 'dev-{}'.format(
+        __file__.replace('/', '-').replace('_', '-').split('.')[0]
+    )
+
+    # n_seeds = 3
+    # mode = 'gcp'
+    # exp_prefix = 'skew-fit-door-reference-post-refactor'
+
+    for exp_id, variant in enumerate(sweeper.iterate_hyperparameters()):
+        for _ in range(n_seeds):
+            run_experiment(
+                encoder_wrapped_td3bc_experiment,
+                exp_prefix=exp_prefix,
+                mode=mode,
+                variant=variant,
+                use_gpu=True,
+                num_exps_per_instance=3,
+                gcp_kwargs=dict(
+                    zone='us-east1-c',
+                    gpu_kwargs=dict(
+                        gpu_model='nvidia-tesla-k80',
+                        num_gpu=1,
+                    )
+                ),
+            )
--- railrl/launchers/experiments/ashvin/rfeatures/rfeatures_rl.py
+++ railrl/launchers/experiments/ashvin/rfeatures/rfeatures_rl.py
@@ -225,3 +225,130 @@ def encoder_wrapped_td3bc_experiment(variant):
     td3bc_trainer.pretrain_q_with_bc_data()
 
     algorithm.train()
+
+def state_td3bc_experiment(variant):
+    if variant.get('env_id', None):
+        env = gym.make(variant['env_id'])
+    else:
+        env = variant['env_class'](**variant['env_kwargs'])
+
+    expl_env = env # variant['env_class'](**variant['env_kwargs'])
+    eval_env = env # variant['env_class'](**variant['env_kwargs'])
+
+    observation_key = 'state_observation'
+    desired_goal_key = 'state_desired_goal'
+    achieved_goal_key = desired_goal_key.replace("desired", "achieved")
+    es = GaussianAndEpislonStrategy(
+        action_space=expl_env.action_space,
+        max_sigma=.2,
+        min_sigma=.2,  # constant sigma
+        epsilon=.3,
+    )
+    obs_dim = expl_env.observation_space.spaces['observation'].low.size
+    goal_dim = expl_env.observation_space.spaces['desired_goal'].low.size
+    action_dim = expl_env.action_space.low.size
+    qf1 = FlattenMlp(
+        input_size=obs_dim + goal_dim + action_dim,
+        output_size=1,
+        **variant['qf_kwargs']
+    )
+    qf2 = FlattenMlp(
+        input_size=obs_dim + goal_dim + action_dim,
+        output_size=1,
+        **variant['qf_kwargs']
+    )
+    target_qf1 = FlattenMlp(
+        input_size=obs_dim + goal_dim + action_dim,
+        output_size=1,
+        **variant['qf_kwargs']
+    )
+    target_qf2 = FlattenMlp(
+        input_size=obs_dim + goal_dim + action_dim,
+        output_size=1,
+        **variant['qf_kwargs']
+    )
+    policy = TanhMlpPolicy(
+        input_size=obs_dim + goal_dim,
+        output_size=action_dim,
+        **variant['policy_kwargs']
+    )
+    target_policy = TanhMlpPolicy(
+        input_size=obs_dim + goal_dim,
+        output_size=action_dim,
+        **variant['policy_kwargs']
+    )
+    expl_policy = PolicyWrappedWithExplorationStrategy(
+        exploration_strategy=es,
+        policy=policy,
+    )
+    replay_buffer = ObsDictRelabelingBuffer(
+        env=eval_env,
+        observation_key=observation_key,
+        desired_goal_key=desired_goal_key,
+        achieved_goal_key=achieved_goal_key,
+        **variant['replay_buffer_kwargs']
+    )
+    demo_train_buffer = ObsDictRelabelingBuffer(
+        env=env,
+        observation_key=observation_key,
+        desired_goal_key=desired_goal_key,
+        achieved_goal_key=achieved_goal_key,
+        **variant['replay_buffer_kwargs']
+    )
+    demo_test_buffer = ObsDictRelabelingBuffer(
+        env=env,
+        observation_key=observation_key,
+        desired_goal_key=desired_goal_key,
+        achieved_goal_key=achieved_goal_key,
+        **variant['replay_buffer_kwargs']
+    )
+    td3bc_trainer = TD3BCTrainer(
+        env=env,
+        policy=policy,
+        qf1=qf1,
+        qf2=qf2,
+        replay_buffer=replay_buffer,
+        demo_train_buffer=demo_train_buffer,
+        demo_test_buffer=demo_test_buffer,
+        target_qf1=target_qf1,
+        target_qf2=target_qf2,
+        target_policy=target_policy,
+        **variant['trainer_kwargs']
+    )
+    trainer = HERTrainer(td3bc_trainer)
+    eval_path_collector = GoalConditionedPathCollector(
+        eval_env,
+        policy,
+        observation_key=observation_key,
+        desired_goal_key=desired_goal_key,
+    )
+    expl_path_collector = GoalConditionedPathCollector(
+        expl_env,
+        expl_policy,
+        observation_key=observation_key,
+        desired_goal_key=desired_goal_key,
+    )
+    algorithm = TorchBatchRLAlgorithm(
+        trainer=trainer,
+        exploration_env=expl_env,
+        evaluation_env=eval_env,
+        exploration_data_collector=expl_path_collector,
+        evaluation_data_collector=eval_path_collector,
+        replay_buffer=replay_buffer,
+        **variant['algo_kwargs']
+    )
+
+    if variant.get("save_video", True):
+        video_func = VideoSaveFunction(
+            env,
+            **variant["dump_video_kwargs"],
+        )
+        algorithm.post_train_funcs.append(video_func)
+
+    algorithm.to(ptu.device)
+
+    td3bc_trainer.load_demos()
+    # td3bc_trainer.pretrain_policy_with_bc()
+    # td3bc_trainer.pretrain_q_with_bc_data()
+
+    algorithm.train()
