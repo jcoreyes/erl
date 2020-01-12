db89d92cfaa3596097d202a426e31a6421769be8
Murtaza Dalal <mdalal@berkeley.edu>
2020-01-12 13:55:41 -0800

    added all demo files

--- experiments/murtaza/off_policy_ssl/gym/demos.py
+++ experiments/murtaza/off_policy_ssl/gym/demos.py
@@ -7,4 +7,26 @@ if __name__ == '__main__':
     data = load_local_or_remote_file('/home/murtaza/research/railrl/data/doodads3/12-05-sac-mujoco-envs-v1/12-05-sac_mujoco_envs_v1_2019_12_06_02_08_27_id000--s22176/params.pkl')
     env = data['evaluation/env']
     policy = data['exploration/policy']
-    collect_demos_fixed(env, policy, "data/local/demos/gym_1000.npy", N=1000, horizon=1000, threshold=9000, add_action_noise=False, render=False, noise_sigma=0.0)
+    collect_demos_fixed(env, policy, "data/local/demos/hc_action_noise_1000.npy", N=1000, horizon=1000, threshold=9000, add_action_noise=False, render=False, noise_sigma=0.0)
+
+    data = load_local_or_remote_file(
+        '/home/murtaza/research/railrl/data/doodads3/12-05-sac-mujoco-envs-v1/12-05-sac_mujoco_envs_v1_2019_12_06_02_08_21_id005--s6655/params.pkl')
+    env = data['evaluation/env']
+    policy = data['exploration/policy']
+    collect_demos_fixed(env, policy, "data/local/demos/hopper_action_noise_1000.npy", N=1000, horizon=1000, threshold=2500,
+                        add_action_noise=False, render=False, noise_sigma=0.0)
+
+    data = load_local_or_remote_file(
+        '/home/murtaza/research/railrl/data/doodads3/12-05-sac-mujoco-envs-v1/12-05-sac_mujoco_envs_v1_2019_12_06_02_08_26_id003--s3090/params.pkl')
+    env = data['evaluation/env']
+    policy = data['exploration/policy']
+    collect_demos_fixed(env, policy, "data/local/demos/ant_action_noise_1000.npy", N=1000, horizon=1000, threshold=3500,
+                        add_action_noise=False, render=False, noise_sigma=0.0)
+
+    data = load_local_or_remote_file(
+        '/home/murtaza/research/railrl/data/doodads3/12-05-sac-mujoco-envs-v1/12-05-sac_mujoco_envs_v1_2019_12_06_02_08_24_id004--s76342/params.pkl')
+    env = data['evaluation/env']
+    policy = data['exploration/policy']
+    collect_demos_fixed(env, policy, "data/local/demos/walker_action_noise_1000.npy", N=1000, horizon=1000, threshold=4000,
+                        add_action_noise=False, render=False, noise_sigma=0.0)
+
