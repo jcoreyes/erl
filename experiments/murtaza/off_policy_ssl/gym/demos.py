f6cad233d2b024dd3900b85d9254fa5f6f149031
Murtaza Dalal <mdalal@berkeley.edu>
2020-01-12 14:05:57 -0800

    updated ant demo file

--- experiments/murtaza/off_policy_ssl/gym/demos.py
+++ experiments/murtaza/off_policy_ssl/gym/demos.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
                         add_action_noise=False, render=False, noise_sigma=0.0)
 
     data = load_local_or_remote_file(
-        '/home/murtaza/research/railrl/data/doodads3/12-05-sac-mujoco-envs-v1/12-05-sac_mujoco_envs_v1_2019_12_06_02_08_26_id003--s3090/params.pkl')
+        '/home/murtaza/research/railrl/data/doodads3/12-05-sac-mujoco-envs-v1/12-05-sac_mujoco_envs_v1_2019_12_06_02_08_26_id003--s3017/params.pkl')
     env = data['evaluation/env']
     policy = data['exploration/policy']
     collect_demos_fixed(env, policy, "data/local/demos/ant_action_noise_1000.npy", N=1000, horizon=1000, threshold=3500,
