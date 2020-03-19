09a933d850b32b0fedcf7d69c6a42716115f67a3
Ashvin Nair <anair17@berkeley.edu>
2020-03-19 11:45:27 -0400

    Use callable class instead of anonymous fn for experiment AutoSetup (#258)
    * Use callable class instead of anonymous fn
    
    * re-add docstring

--- railrl/launchers/launcher_util.py
+++ railrl/launchers/launcher_util.py
@@ -21,6 +21,40 @@ GitInfo = NamedTuple(
     ],
 )
 
+class AutoSetup:
+    """
+    Automatically set up:
+    1. the logger
+    2. the GPU mode
+    3. the seed
+    :param exp_function: some function that should not depend on `logger_config`
+    nor `seed`.
+    :param unpack_variant: do you call exp_function with `**variant`?
+    :return: function output
+    """
+    def __init__(self, exp_function, unpack_variant=True):
+        self.exp_function = exp_function
+        self.unpack_variant = unpack_variant
+
+    def __call__(self, doodad_config, variant):
+        if doodad_config:
+            variant_to_save = variant.copy()
+            variant_to_save['doodad_info'] = doodad_config.extra_launch_info
+            setup_experiment(
+                variant=variant_to_save,
+                exp_name=doodad_config.exp_name,
+                base_log_dir=doodad_config.base_log_dir,
+                git_infos=doodad_config.git_infos,
+                script_name=doodad_config.script_name,
+                use_gpu=doodad_config.use_gpu,
+                gpu_id=doodad_config.gpu_id,
+            )
+        variant.pop('logger_config', None)
+        variant.pop('seed', None)
+        if self.unpack_variant:
+            self.exp_function(**variant)
+        else:
+            self.exp_function(variant)
 
 def run_experiment(
         method_call,
@@ -37,7 +71,7 @@ def run_experiment(
     if base_log_dir is None:
         base_log_dir=config.LOCAL_LOG_DIR
     if wrap_fn_with_auto_setup:
-        method_call = auto_setup(method_call, unpack_variant=unpack_variant)
+        method_call = AutoSetup(method_call, unpack_variant=unpack_variant)
     if mode == 'here_no_doodad':
         setup_experiment(
             variant=variant,
@@ -62,44 +96,6 @@ def run_experiment(
             gpu_id=gpu_id,
             **kwargs
         )
-
-
-def auto_setup(exp_function, unpack_variant=True):
-    """
-    Automatically set up:
-    1. the logger
-    2. the GPU mode
-    3. the seed
-
-    :param exp_function: some function that should not depend on `logger_config`
-    nor `seed`.
-    :param unpack_variant: do you call exp_function with `**variant`?
-    nor `seed`.
-    :return: function output
-    """
-    def run_experiment_compatible_function(doodad_config, variant):
-        # See doodad.easy_launch.python_function.DoodadConfig
-        if doodad_config:
-            variant_to_save = variant.copy()
-            variant_to_save['doodad_info'] = doodad_config.extra_launch_info
-            setup_experiment(
-                variant=variant_to_save,
-                exp_name=doodad_config.exp_name,
-                base_log_dir=doodad_config.base_log_dir,
-                git_infos=doodad_config.git_infos,
-                script_name=doodad_config.script_name,
-                use_gpu=doodad_config.use_gpu,
-                gpu_id=doodad_config.gpu_id,
-            )
-        variant.pop('logger_config', None)
-        variant.pop('seed', None)
-        if unpack_variant:
-            exp_function(**variant)
-        else:
-            exp_function(variant)
-
-    return run_experiment_compatible_function
-
 
 def setup_experiment(
         variant,
