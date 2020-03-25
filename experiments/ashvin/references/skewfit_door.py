0fa4ccfdbd12ab7ff97ab282453c4a576b360760
Ashvin Nair <nair.ashvin@gmail.com>
2020-03-25 13:31:54 -0700

    Update my reference exps to use arglauncher

--- experiments/ashvin/references/skewfit_door.py
+++ experiments/ashvin/references/skewfit_door.py
@@ -7,6 +7,7 @@ from railrl.torch.grill.launcher import grill_her_twin_sac_online_vae_full_exper
 import railrl.torch.vae.vae_schedules as vae_schedules
 from railrl.torch.vae.conv_vae import imsize48_default_architecture
 
+from railrl.launchers.arglauncher import run_variants
 
 if __name__ == "__main__":
     variant = dict(
@@ -118,41 +119,14 @@ if __name__ == "__main__":
     )
 
     search_space = {
+        'seedid': range(5),
     }
     sweeper = hyp.DeterministicHyperparameterSweeper(
         search_space, default_parameters=variant,
     )
 
-    # n_seeds = 1
-    # mode = 'local'
-    # exp_prefix = 'dev-{}'.format(
-    #     __file__.replace('/', '-').replace('_', '-').split('.')[0]
-    # )
+    variants = []
+    for variant in sweeper.iterate_hyperparameters():
+        variants.append(variant)
 
-    # n_seeds = 3
-    # mode = 'gcp'
-    # exp_prefix = 'skew-fit-door-reference-post-refactor'
-
-    n_seeds = 5
-    mode = 'ec2'
-    exp_prefix = 'skewfit-door5'
-
-    for exp_id, variant in enumerate(sweeper.iterate_hyperparameters()):
-        for _ in range(n_seeds):
-            run_experiment(
-                grill_her_twin_sac_online_vae_full_experiment,
-                exp_prefix=exp_prefix,
-                mode=mode,
-                variant=variant,
-                use_gpu=True,
-                region='us-west-2'
-                # num_exps_per_instance=3,
-                # gcp_kwargs=dict(
-                #     zone='us-east1-c',
-                #     gpu_kwargs=dict(
-                #         gpu_model='nvidia-tesla-k80',
-                #         num_gpu=1,
-                #     )
-                # ),
-                # time_in_mins=int(2.5*24*60),
-          )
+    run_variants(grill_her_twin_sac_online_vae_full_experiment, variants)
