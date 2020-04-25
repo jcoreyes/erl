fc24ad9765f8c1118afa997daae5207e0e059cd1
Vitchyr Pong <vitchyr@gmail.com>
2020-04-25 14:58:49 -0700

    make it easy to insert multiple images at once (#289)
    * refactor path samplers and support many keys in ContextualPathSampler
    
    * set exp_id and seed correctly in example script and shorten length
    
    * refactor contextual diagnostics
    
    * remove unnecessary code
    
    * refactor contextual launcher code into its own module
    
    * specify that context keys are for policy only
    
    * add descriptions to interface
    
    * switch to more general achieved_goal_from_observation
    
    * update contextual encoder distance launcher with latest updates:
    
    * add example for image-obs, state-reward and make accompanying changes
    
    * do not flatten images unnecessarily
    
    * update contextual encoder distance experiment
    
    * set exp for actual launch
    
    * always concatenate along the first non-batch dimension as default behaviorgs
    
    * update example pointmass script
    
    * fix script key
    
    * make it easy to insert multiple images at once
    
    * consolidate renderer interface
    
    * nit
    
    * fix typo

--- railrl/envs/images/insert_image_env.py
+++ railrl/envs/images/insert_image_env.py
@@ -1,3 +1,5 @@
+import typing
+
 import gym
 import numpy as np
 from gym.spaces import Box, Dict
@@ -5,25 +7,39 @@ from gym.spaces import Box, Dict
 from railrl.envs.images import Renderer
 
 
-class InsertImageEnv(gym.Wrapper):
+class InsertImagesEnv(gym.Wrapper):
+    """
+    Add an image to the observation. Usage:
+
+    ```
+    obs = env.reset()
+    print(obs.keys())  # ['observations']
+
+    new_env = InsertImageEnv(
+        env,
+        {
+            'image_observation': renderer_one,
+            'debugging_img': renderer_two,
+        },
+    )
+    obs = new_env.reset()
+    print(obs.keys())  # ['observations', 'image_observation', 'debugging_img']
+    ```
+    """
     def __init__(
             self,
             wrapped_env: gym.Env,
-            renderer: Renderer,
-            image_key='image_observation',
+            renderers: typing.Dict[str, Renderer],
     ):
-        """
-        Appends images to the observation.
-        """
         super().__init__(wrapped_env)
-        self._renderer = renderer
-        if self._renderer.normalize:
-            img_space = Box(0, 1, renderer.image_shape, dtype=np.float32)
-        else:
-            img_space = Box(0, 255, renderer.image_shape, dtype=np.uint8)
         spaces = self.env.observation_space.spaces.copy()
-        spaces[image_key] = img_space
-        self._image_key = image_key
+        for image_key, renderer in renderers.items():
+            if renderer.image_is_normalized:
+                img_space = Box(0, 1, renderer.image_shape, dtype=np.float32)
+            else:
+                img_space = Box(0, 255, renderer.image_shape, dtype=np.uint8)
+            spaces[image_key] = img_space
+        self.renderers = renderers
         self.observation_space = Dict(spaces)
         self.action_space = self.env.action_space
 
@@ -38,8 +54,27 @@ class InsertImageEnv(gym.Wrapper):
         return obs
 
     def _update_obs(self, obs):
-        img_obs = self._get_image()
-        obs[self._image_key] = img_obs
+        for image_key, renderer in self.renderers.items():
+            obs[image_key] = renderer.create_image(self.env)
 
-    def _get_image(self):
-        return self._renderer.create_image(self.env)
+
+class InsertImageEnv(InsertImagesEnv):
+    """
+    Add an image to the observation. Usage:
+
+    ```
+    obs = env.reset()
+    print(obs.keys())  # ['observations']
+
+    new_env = InsertImageEnv(env, renderer, image_key='pretty_picture')
+    obs = new_env.reset()
+    print(obs.keys())  # ['observations', 'pretty_picture']
+    ```
+    """
+    def __init__(
+            self,
+            wrapped_env: gym.Env,
+            renderer: Renderer,
+            image_key='image_observation',
+    ):
+        super().__init__(wrapped_env, {image_key: renderer})
--- railrl/envs/images/renderer.py
+++ railrl/envs/images/renderer.py
@@ -7,65 +7,68 @@ class Renderer(object):
             self,
             img_width=48,
             img_height=48,
+            num_channels=3,
             init_camera=None,
-            grayscale=False,
-            normalize=True,
-            flatten=False,
-            input_image_format='HWC',
-            output_image_format='HWC',
+            normalize_img=True,
+            flatten_img=False,
+            input_img_format='HWC',
+            output_img_format='HWC',
     ):
         """Render an image."""
-        if input_image_format not in {'HWC', 'CWH'}:
+        if input_img_format not in {'HWC', 'CWH'}:
             raise ValueError(
                 "Only support input format of WHC or CWH, not {}".format(
-                    input_image_format
+                    input_img_format
                 )
             )
-        if output_image_format not in {'HWC', 'CWH'}:
+        if output_img_format not in {'HWC', 'CWH'}:
             raise ValueError(
                 "Only support output format of WHC or CWH, not {}".format(
-                    output_image_format
+                    output_img_format
                 )
             )
-        self.img_width = img_width
-        self.img_height = img_height
-        self.init_camera = init_camera
-        self.grayscale = grayscale
-        self.normalize = normalize
-        self.flatten = flatten
-        self.channels = 1 if grayscale else 3
-        self.input_image_format = input_image_format
-        self.output_image_format = output_image_format
-
+        self._img_width = img_width
+        self._img_height = img_height
+        self._init_camera = init_camera
+        self._grayscale = num_channels == 1
+        self._normalize_imgs = normalize_img
+        self._flatten = flatten_img
+        self._num_channels = num_channels
+        self.input_image_format = input_img_format
+        self.output_image_format = output_img_format
         self._camera_is_initialized = False
 
     def create_image(self, env):
-        if not self._camera_is_initialized and self.init_camera is not None:
-            env.initialize_camera(self.init_camera)
+        if not self._camera_is_initialized and self._init_camera is not None:
+            env.initialize_camera(self._init_camera)
             self._camera_is_initialized = True
 
         image_obs = env.get_image(
-            width=self.img_width,
-            height=self.img_height,
+            width=self._img_width,
+            height=self._img_height,
         )
-        if self.grayscale:
+        if self._grayscale:
             image_obs = Image.fromarray(image_obs).convert('L')
             image_obs = np.array(image_obs)
-        if self.normalize:
+        if self._normalize_imgs:
             image_obs = image_obs / 255.0
         if self.input_image_format != self.output_image_format:
             image_obs = image_obs.transpose()
         assert image_obs.shape == self.image_shape
-        if self.flatten:
+        if self._flatten:
             return image_obs.flatten()
         else:
             return image_obs
 
     @property
+    def image_is_normalized(self):
+        return self._normalize_imgs
+
+    @property
     def image_shape(self):
         if self.output_image_format == 'HWC':
-            return self.img_height, self.img_width, self.channels
+            return self._img_height, self._img_width, self._num_channels
         elif self.output_image_format == 'CWH':
-            return self.channels, self.img_width, self.img_height
+            return self._num_channels, self._img_width, self._img_height
         else:
             raise ValueError(self.output_image_format)
