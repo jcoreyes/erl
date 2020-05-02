53bc0da85f75863ce83c9d6652895b45aeba1cf3
Vitchyr Pong <vitchyr@gmail.com>
2020-05-01 19:34:12 -0700

    Make image module and allow grid images (#293)
    * move image code to visualization/image.py
    
    * undo hardcoded chang to combined_images_into_grid
    
    * only use grid function

--- railrl/visualization/video.py
+++ railrl/visualization/video.py
@@ -2,8 +2,6 @@ import os
 import os.path as osp
 import uuid
 
-from railrl.envs.vae_wrappers import VAEWrappedEnv, ConditionalVAEWrappedEnv
-
 filename = str(uuid.uuid4())
 
 import skvideo.io
@@ -14,6 +12,9 @@ import scipy.misc
 
 from multiworld.core.image_env import ImageEnv
 from railrl.core import logger
+from railrl.envs.vae_wrappers import VAEWrappedEnv, ConditionalVAEWrappedEnv
+from railrl.visualization.image import combine_images_into_grid
+
 import pickle
 
 
@@ -128,89 +129,6 @@ class RIGVideoSaveFunction:
         for i_in_path, d in enumerate(path['full_observations']):
             d[self.goal_image_key] = decoded_img
 
-def add_border(img, border_thickness, border_color):
-    imheight, imwidth = img.shape[:2]
-    framed_img = np.ones(
-        (
-            imheight + 2 * border_thickness,
-            imwidth + 2 * border_thickness,
-            img.shape[2]
-        ),
-        dtype=np.uint8
-    ) * border_color
-    framed_img[
-        border_thickness:-border_thickness,
-        border_thickness:-border_thickness,
-        :
-    ] = img
-    return framed_img
-
-
-def make_image_fit_into_hwc_format(
-        img, output_imwidth, output_imheight, input_image_format
-):
-    if len(img.shape) == 1:
-        if input_image_format == 'HWC':
-            hwc_img = img.reshape(output_imheight, output_imwidth, -1)
-        elif input_image_format == 'CWH':
-            cwh_img = img.reshape(-1, output_imwidth, output_imheight)
-            hwc_img = cwh_img.transpose()
-        else:
-            raise ValueError(input_image_format)
-    else:
-        a, b, c = img.shape
-        # TODO: remove hack
-        if a == b and a != c:
-            input_image_format = 'HWC'
-        elif a != b and b == c:
-            input_image_format = 'CWH'
-        if input_image_format == 'HWC':
-            hwc_img = img
-        elif input_image_format == 'CWH':
-            hwc_img = img.transpose()
-        else:
-            raise ValueError(input_image_format)
-
-    if hwc_img.shape == (output_imheight, output_imwidth, 3):
-        image_that_fits = hwc_img
-    else:
-        try:
-            import cv2
-            image_that_fits = cv2.resize(
-                hwc_img,
-                dsize=(output_imwidth, output_imheight),
-            )
-        except ImportError:
-            image_that_fits = np.zeros((output_imheight, output_imwidth, 3))
-            h, w = hwc_img.shape[:2]
-            image_that_fits[:h, :w, :] = hwc_img
-    return image_that_fits
-
-
-def get_image(
-        imgs, imwidth, imheight,
-        subpad_length=1, subpad_color=255,
-        pad_length=1, pad_color=255,
-        unnormalize=True,
-        image_format='CWH',
-):
-    hwc_imgs = [
-        make_image_fit_into_hwc_format(img, imwidth, imheight, image_format)
-        for img in imgs
-    ]
-
-    new_imgs = []
-    for img in hwc_imgs:
-        if unnormalize:
-            img = np.uint8(255 * img)
-        if subpad_length > 0:
-            img = add_border(img, subpad_length, subpad_color)
-        new_imgs.append(img)
-    final_image = np.concatenate(new_imgs, axis=0)
-    if pad_length > 0:
-        final_image = add_border(final_image, pad_length, pad_color)
-    return final_image
-
 
 def dump_video(
         env,
@@ -232,6 +150,7 @@ def dump_video(
         get_extra_imgs=None,
         grayscale=False,
         keys_to_show=None,
+        num_columns_per_rollout=1,
 ):
     """
 
@@ -280,8 +199,9 @@ def dump_video(
             imgs_to_stack = [d[k] for k in keys_to_show]
             imgs_to_stack += get_extra_imgs(path, i_in_path, env)
             l.append(
-                get_image(
+                combine_images_into_grid(
                     imgs_to_stack,
+                    max_num_cols=num_columns_per_rollout,
                     imwidth=imsize,
                     imheight=imsize,
                     pad_length=pad_length,
@@ -375,6 +295,7 @@ def dump_paths(
         unnormalize=True,
         grayscale=False,
         get_extra_imgs=None,
+        num_columns_per_rollout=1,
 ):
     if get_extra_imgs is None:
         get_extra_imgs = get_generic_env_imgs
@@ -403,7 +324,7 @@ def dump_paths(
             imgs = imgs + get_extra_imgs(path, i_in_path, env)
             imgs = imgs[:num_imgs]
             l.append(
-                get_image(
+                combine_images_into_grid(
                     imgs,
                     imwidth,
                     imheight,
@@ -412,6 +333,7 @@ def dump_paths(
                     subpad_length=subpad_length,
                     subpad_color=subpad_color,
                     unnormalize=unnormalize,
+                    max_num_cols=num_columns_per_rollout,
                 )
             )
         frames += l
