e883197ed70c18c5bdc0a3a9cc5eaa5824be620a
stevenlin1111 <stevenlin1111@gmail.com>
2020-05-21 16:41:01 -0700

    Add vae visualizations to vae trainer (#331)
    * Add vae vis
    
    * Fix default None as dict
    
    * Option for no visualizations

--- railrl/torch/vae/vae_torch_trainer.py
+++ railrl/torch/vae/vae_torch_trainer.py
@@ -1,17 +1,22 @@
 from collections import OrderedDict, namedtuple
+import os.path as osp
 from typing import Tuple
 
+import cv2
 import numpy as np
 import torch
 import torch.optim as optim
-from railrl.core.loss import LossFunction
 from torch import nn as nn
 
-import railrl.torch.pytorch_util as ptu
+from railrl.core import logger
+from railrl.core.logging import add_prefix
+from railrl.core.loss import LossFunction
 from railrl.misc.eval_util import create_stats_ordered_dict
+import railrl.torch.pytorch_util as ptu
 from railrl.torch.torch_rl_algorithm import TorchTrainer
 from railrl.core.logging import add_prefix
 from railrl.core.timer import timer
+from railrl.visualization.image import combine_images_into_grid
 
 VAELosses = namedtuple(
     'VAELoss',
@@ -27,6 +32,7 @@ class VAETrainer(TorchTrainer, LossFunction):
             vae_lr=1e-3,
             beta=1,
             loss_scale=1.0,
+            vae_visualization_config=None,
             optimizer_class=optim.Adam,
     ):
         super().__init__()
@@ -40,6 +46,10 @@ class VAETrainer(TorchTrainer, LossFunction):
         self.loss_scale = loss_scale
         self.eval_statistics = OrderedDict()
 
+        self.vae_visualization_config = vae_visualization_config
+        if not self.vae_visualization_config:
+            self.vae_visualization_config = {}
+
     def train_from_torch(self, batch):
         losses, stats = self.compute_loss(
             batch,
@@ -52,6 +62,7 @@ class VAETrainer(TorchTrainer, LossFunction):
         if self._need_to_update_eval_statistics:
             self.eval_statistics = stats
             self._need_to_update_eval_statistics = False
+            self.example_obs_batch = batch['raw_next_observations']
 
     def kl_divergence(self, z_mu, logvar):
         return - 0.5 * torch.sum(
@@ -105,6 +116,49 @@ class VAETrainer(TorchTrainer, LossFunction):
 
     def end_epoch(self, epoch):
         self._need_to_update_eval_statistics = True
+        self.dump_debug_images(epoch, **self.vae_visualization_config)
+
+    def dump_debug_images(
+        self,
+        epoch,
+        dump_images=True,
+        num_recons=10,
+        num_samples=25
+    ):
+        if not dump_images:
+            return
+        example_obs_batch_np = ptu.get_numpy(self.example_obs_batch)
+        recon_examples_np = ptu.get_numpy(
+            self.vae.reconstruct(self.example_obs_batch, use_mean=True))
+
+        top_row_example = example_obs_batch_np[:num_recons]
+        bottom_row_recon = np.clip(recon_examples_np, 0, 1)[:num_recons]
+
+        recon_vis = combine_images_into_grid(
+            imgs=list(top_row_example) + list(bottom_row_recon),
+            imwidth=example_obs_batch_np.shape[2],
+            imheight=example_obs_batch_np.shape[3],
+            max_num_cols=len(top_row_example),
+            image_format='CWH',
+        )
+
+        logdir = logger.get_snapshot_dir()
+        cv2.imwrite(
+            osp.join(logdir, '{}_recons.png'.format(epoch)),
+            recon_vis,
+        )
+
+        vae_samples = np.clip(self.vae.sample_np(num_samples), 0, 1)
+        vae_sample_vis = combine_images_into_grid(
+            imgs=list(vae_samples),
+            imwidth=example_obs_batch_np.shape[2],
+            imheight=example_obs_batch_np.shape[3],
+            image_format='CWH',
+        )
+        cv2.imwrite(
+            osp.join(logdir, '{}_vae_samples.png'.format(epoch)),
+            vae_sample_vis,
+        )
 
     @property
     def networks(self):
