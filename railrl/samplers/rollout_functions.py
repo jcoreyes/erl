f040020458f8c07c65271c90e8ea0b1dedf3a7fc
Ashvin Nair <nair.ashvin@gmail.com>
2020-07-19 13:02:26 -0700

    rollout fn

--- railrl/samplers/rollout_functions.py
+++ railrl/samplers/rollout_functions.py
@@ -109,7 +109,7 @@ def rollout(
         if full_o_postprocess_func:
             full_o_postprocess_func(env, agent, o)
 
-        next_o, r, d, env_info = env.step(a)
+        next_o, r, d, env_info = env.step(a.copy())
         if render:
             env.render(**render_kwargs)
         observations.append(o)
